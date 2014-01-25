ec6f8812c9ac1a18c66aff8658649a76cc973638
Vasco Lavos <vasco.men@hotmail.com>
2014-01-25 21:37:50 +0000

    Fixed some models.

--- MODSRC/vazkii/botania/client/model/ModelPool.java
+++ MODSRC/vazkii/botania/client/model/ModelPool.java
@@ -15,7 +15,7 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 
 public class ModelPool extends ModelBase {
-	
+
 	ModelRenderer base;
 	ModelRenderer side1;
 	ModelRenderer side2;
@@ -28,18 +28,23 @@ public class ModelPool extends ModelBase {
 
 		base = new ModelRenderer(this, 0, 0);
 		base.addBox(0F, 0F, 0F, 16, 1, 16);
+		base.setRotationPoint(-8F, 23F, -8F);
 		base.setTextureSize(64, 32);
 		side1 = new ModelRenderer(this, 0, 0);
 		side1.addBox(0F, 0F, 0F, 16, 7, 1);
+		side1.setRotationPoint(-8F, 16F, 7F);
 		side1.setTextureSize(64, 32);
 		side2 = new ModelRenderer(this, 0, 0);
 		side2.addBox(0F, 0F, 0F, 16, 7, 1);
+		side2.setRotationPoint(-8F, 16F, -8F);
 		side2.setTextureSize(64, 32);
 		side3 = new ModelRenderer(this, 0, 0);
 		side3.addBox(0F, 0F, 0F, 1, 7, 14);
+		side3.setRotationPoint(-8F, 16F, -7F);
 		side3.setTextureSize(64, 32);
 		side4 = new ModelRenderer(this, 0, 0);
 		side4.addBox(0F, 0F, 0F, 1, 7, 14);
+		side4.setRotationPoint(7F, 16F, -7F);
 		side4.setTextureSize(64, 32);
 	}
 
