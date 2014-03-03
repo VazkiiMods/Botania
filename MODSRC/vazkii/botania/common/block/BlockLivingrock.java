38e3a502b80ef484d6c7e3892dcfc7623437d1c8
Vasco Lavos <vasco.men@hotmail.com>
2014-03-03 02:38:39 +0000

    Added a bunch of decorative blocks.

--- MODSRC/vazkii/botania/common/block/BlockLivingrock.java
+++ MODSRC/vazkii/botania/common/block/BlockLivingrock.java
@@ -11,11 +11,24 @@
  */
 package vazkii.botania.common.block;
 
+import java.util.List;
+
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
+import vazkii.botania.client.core.helper.IconHelper;
+import vazkii.botania.common.item.block.ItemBlockWithMetadataAndName;
 import vazkii.botania.common.lib.LibBlockIDs;
 import vazkii.botania.common.lib.LibBlockNames;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class BlockLivingrock extends BlockMod {
+
+	private static final int TYPES = 5; 
+	Icon[] icons;
 
 	public BlockLivingrock() {
 		super(LibBlockIDs.idLivingrock, Material.rock);
@@ -23,6 +36,35 @@ public class BlockLivingrock extends BlockMod {
 		setResistance(10.0F);
 		setStepSound(soundStoneFootstep);
 		setUnlocalizedName(LibBlockNames.LIVING_ROCK);
+	}
+
+	@Override
+	protected boolean shouldRegisterInNameSet() {
+		return false;
+	}
+
+	@Override
+	public Block setUnlocalizedName(String par1Str) {
+		GameRegistry.registerBlock(this, ItemBlockWithMetadataAndName.class, par1Str);
+		return super.setUnlocalizedName(par1Str);
+	}
+	
+	@Override
+	public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List) {
+		for(int i = 0; i < TYPES; i++)
+			par3List.add(new ItemStack(par1, 0, i));
+	}
+
+	@Override
+	public void registerIcons(IconRegister par1IconRegister) {
+		icons = new Icon[TYPES];
+		for(int i = 0; i < TYPES; i++)
+			icons[i] = IconHelper.forBlock(par1IconRegister, this, i);
+	}
+
+	@Override
+	public Icon getIcon(int par1, int par2) {
+		return icons[Math.min(TYPES - 1, par2)];
 	}
 
 }
--- MODSRC/vazkii/botania/common/block/BlockLivingwood.java
+++ MODSRC/vazkii/botania/common/block/BlockLivingwood.java
@@ -11,17 +11,59 @@
  */
 package vazkii.botania.common.block;
 
+import java.util.List;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
+import vazkii.botania.client.core.helper.IconHelper;
+import vazkii.botania.common.item.block.ItemBlockWithMetadataAndName;
 import vazkii.botania.common.lib.LibBlockIDs;
 import vazkii.botania.common.lib.LibBlockNames;
 
 public class BlockLivingwood extends BlockMod {
 
+	private static final int TYPES = 5; 
+	Icon[] icons;
+	
 	public BlockLivingwood() {
 		super(LibBlockIDs.idLivingwood, Material.wood);
 		setHardness(2.0F);
 		setStepSound(soundWoodFootstep);
 		setUnlocalizedName(LibBlockNames.LIVING_WOOD);
+	}
+	
+	@Override
+	protected boolean shouldRegisterInNameSet() {
+		return false;
+	}
+
+	@Override
+	public Block setUnlocalizedName(String par1Str) {
+		GameRegistry.registerBlock(this, ItemBlockWithMetadataAndName.class, par1Str);
+		return super.setUnlocalizedName(par1Str);
+	}
+	
+	@Override
+	public void getSubBlocks(int par1, CreativeTabs par2CreativeTabs, List par3List) {
+		for(int i = 0; i < TYPES; i++)
+			par3List.add(new ItemStack(par1, 0, i));
+	}
+
+	@Override
+	public void registerIcons(IconRegister par1IconRegister) {
+		icons = new Icon[TYPES];
+		for(int i = 0; i < TYPES; i++)
+			icons[i] = IconHelper.forBlock(par1IconRegister, this, i);
+	}
+
+	@Override
+	public Icon getIcon(int par1, int par2) {
+		return icons[Math.min(TYPES - 1, par2)];
 	}
 
 }
