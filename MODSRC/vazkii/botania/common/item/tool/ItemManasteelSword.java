60796cd026cc0dcd27f5d460d48db467a677b5ac
Vasco Lavos <vasco.men@hotmail.com>
2014-04-14 02:50:40 +0100

    A little easter egg never hurt anybody.

--- MODSRC/vazkii/botania/common/item/tool/ItemManasteelSword.java
+++ MODSRC/vazkii/botania/common/item/tool/ItemManasteelSword.java
@@ -17,6 +17,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.client.core.helper.IconHelper;
@@ -28,8 +29,10 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemManasteelSword extends ItemSword {
 
-private static final int MANA_PER_DAMAGE = 50;
-	
+	private static final int MANA_PER_DAMAGE = 50;
+
+	IIcon elucidatorIcon;
+
 	public ItemManasteelSword() {
 		super(BotaniaAPI.manasteelToolMaterial);
 		setCreativeTab(BotaniaCreativeTab.INSTANCE);
@@ -46,12 +49,23 @@ private static final int MANA_PER_DAMAGE = 50;
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IIconRegister par1IconRegister) {
 		itemIcon = IconHelper.forItem(par1IconRegister, this);
+		elucidatorIcon = IconHelper.forName(par1IconRegister, "elucidator");
 	}
-	
+
 	@Override
 	public boolean hitEntity(ItemStack par1ItemStack, EntityLivingBase par2EntityLivingBase, EntityLivingBase par3EntityLivingBase) {
 		ManasteelToolCommons.damageItem(par1ItemStack, 1, par3EntityLivingBase, MANA_PER_DAMAGE);
 		return true;
+	}
+
+	@Override
+	public IIcon getIconIndex(ItemStack par1ItemStack) {
+		return par1ItemStack.getDisplayName().equals("The Elucidator") ? elucidatorIcon : super.getIconIndex(par1ItemStack);
+	}
+	
+	@Override
+	public IIcon getIcon(ItemStack stack, int pass) {
+		return getIconIndex(stack);
 	}
 
 	@Override
@@ -61,5 +75,5 @@ private static final int MANA_PER_DAMAGE = 50;
 
 		return true;
 	}
-	
+
 }
