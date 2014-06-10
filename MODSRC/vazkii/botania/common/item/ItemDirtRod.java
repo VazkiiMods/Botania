624784d258818803c8b9b45e81ba0e725a21faa2
Vasco Lavos <vasco.men@hotmail.com>
2014-06-10 23:22:37 +0100

    Added detection to the Rod of the Lands for entities, you can't suffocate with it anymore.

--- MODSRC/vazkii/botania/common/item/ItemDirtRod.java
+++ MODSRC/vazkii/botania/common/item/ItemDirtRod.java
@@ -11,9 +11,11 @@
  */
 package vazkii.botania.common.item;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 import vazkii.botania.api.mana.IManaUsingItem;
@@ -34,14 +36,18 @@ public class ItemDirtRod extends ItemMod implements IManaUsingItem {
 	@Override
 	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
 		if(ManaItemHandler.requestManaExact(par1ItemStack, par2EntityPlayer, COST, false)) {
-			ItemStack stackToPlace = new ItemStack(Blocks.dirt);
-			stackToPlace.tryPlaceItemIntoWorld(par2EntityPlayer, par3World, par4, par5, par6, par7, par8, par9, par10);
-
-			if(stackToPlace.stackSize == 0) {
-				ManaItemHandler.requestManaExact(par1ItemStack, par2EntityPlayer, COST, true);
-				ForgeDirection dir = ForgeDirection.getOrientation(par7);
-				for(int i = 0; i < 6; i++)
-					Botania.proxy.sparkleFX(par3World, par4 + dir.offsetX + Math.random(), par5 + dir.offsetY + Math.random(), par6 + dir.offsetZ + Math.random(), 0.35F, 0.2F, 0.05F, 1F, 5);
+			ForgeDirection dir = ForgeDirection.getOrientation(par7);
+			int entities = par3World.getEntitiesWithinAABB(EntityLivingBase.class, AxisAlignedBB.getBoundingBox(par4 + dir.offsetX, par5 + dir.offsetY, par6 + dir.offsetZ, par4 + dir.offsetX + 1, par5 + dir.offsetY + 1, par6 + dir.offsetZ + 1)).size();
+			
+			if(entities == 0) {
+				ItemStack stackToPlace = new ItemStack(Blocks.dirt);
+				stackToPlace.tryPlaceItemIntoWorld(par2EntityPlayer, par3World, par4, par5, par6, par7, par8, par9, par10);
+					
+				if(stackToPlace.stackSize == 0) {
+					ManaItemHandler.requestManaExact(par1ItemStack, par2EntityPlayer, COST, true);
+					for(int i = 0; i < 6; i++)
+						Botania.proxy.sparkleFX(par3World, par4 + dir.offsetX + Math.random(), par5 + dir.offsetY + Math.random(), par6 + dir.offsetZ + Math.random(), 0.35F, 0.2F, 0.05F, 1F, 5);
+				}
 			}
 		}
 		return true;
@@ -59,3 +65,4 @@ public class ItemDirtRod extends ItemMod implements IManaUsingItem {
 
 
 }
+
