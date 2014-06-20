37dad90e5308bcd1ba7fe5eeeac4faf9f7199e6b
Vasco Lavos <vasco.men@hotmail.com>
2014-06-20 16:49:03 +0100

    lalala I'm taking the 1000th commit.

---
+++ MODSRC/vazkii/botania/common/item/ItemSpawnerMover.java
@@ -0,0 +1,16 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the Botania Mod. Get the Source Code in github:
+ * https://github.com/Vazkii/Botania
+ * 
+ * Botania is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * File Created @ [Jun 20, 2014, 4:47:21 PM (GMT)]
+ */
+package vazkii.botania.common.item;
+
+public class ItemSpawnerMover {
+
+}
