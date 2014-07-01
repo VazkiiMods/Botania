49109f34be1b8d2347fe2c03ba450979d902cd6b
Vasco Lavos <vasco.men@hotmail.com>
2014-07-01 03:30:07 +0100

    Floral Powder now dyes wool.

--- MODSRC/vazkii/botania/common/item/ItemDye.java
+++ MODSRC/vazkii/botania/common/item/ItemDye.java
@@ -11,12 +11,29 @@
  */
 package vazkii.botania.common.item;
 
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import vazkii.botania.common.lib.LibItemNames;
 
 public class ItemDye extends Item16Colors {
 
 	public ItemDye() {
 		super(LibItemNames.DYE);
+	}
+	
+	@Override
+	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
+		Block block = par3World.getBlock(par4, par5, par6);
+		int meta = par1ItemStack.getItemDamage();
+		if(meta != par3World.getBlockMetadata(par4, par5, par6) && (block == Blocks.wool || block == Blocks.carpet)) {
+			par3World.setBlockMetadataWithNotify(par4, par5, par6, meta, 1 | 2);
+			par1ItemStack.stackSize--;
+			return true;
+		}
+		return false;
 	}
 
 }
