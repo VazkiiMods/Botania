a8ce3e908e7ceed4c33a04e97eca38dad755becb
Vasco Lavos <vasco.men@hotmail.com>
2014-07-22 13:16:32 +0100

    Credits...

--- web/credits.php
+++ web/credits.php
@@ -20,6 +20,7 @@
 				<b>cpw and Player</b> for help with some internals.<br>
 				<b>dylan4ever</b> for a bunch of texture work.<br>
 				<b>Flaxbeard</b> for texture work when dylan isn't available.<br>
+				<b>Futureazoo</b> for the Terra Shatterer texture and the alternate textures in the Unity texture pack style that are available in the config.<br>
 				<b>Haighyorkie</b> for being a goldfish and always being supportive with making videos to show off the mod.<br>
 				<b>Le_Alchemist</b> for providing many hours of shameless fun.<br>
 				<b>MCPBot</b> for always being there for my obfuscation woes.<br>
