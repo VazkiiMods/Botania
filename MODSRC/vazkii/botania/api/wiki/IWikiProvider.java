8d33d0408fef85ffff2ef07db172d67db99e5263
Lizbeth Rika <vazkii@hotmail.com>
2014-09-02 22:48:25 +0100

    API documentation...

--- MODSRC/vazkii/botania/api/wiki/IWikiProvider.java
+++ MODSRC/vazkii/botania/api/wiki/IWikiProvider.java
@@ -14,12 +14,26 @@ package vazkii.botania.api.wiki;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
+/**
+ * An interface for a Wiki Provider, these are registered to allow a mod to provide a wiki
+ * for all the blocks in them, used for the world interaction with the Lexica Botania.
+ * A simple, mostly all-inclusive implementation can be found on SimpleWikiProvider.
+ */
 public interface IWikiProvider {
 
+	/**
+	 * Gets the name of the block being looked at for display.
+	 */
 	public String getBlockName(World world, MovingObjectPosition pos);
 	
+	/**
+	 * Gets the URL to open when the block is clicked.
+	 */
 	public String getWikiURL(World world, MovingObjectPosition pos);
 	
+	/**
+	 * Gets the name of the wiki for display.
+	 */
 	public String getWikiName(World world, MovingObjectPosition pos);
 	
 }
