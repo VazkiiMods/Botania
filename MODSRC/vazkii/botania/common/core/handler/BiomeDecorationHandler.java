632e7c6c1855b2c9a840f5e11794da6a81b100ea
Lizbeth Rika <vazkii@hotmail.com>
2014-10-06 17:16:18 +0100

    Changed the flower generation to be patch-based rather than fully random.

--- MODSRC/vazkii/botania/common/core/handler/BiomeDecorationHandler.java
+++ MODSRC/vazkii/botania/common/core/handler/BiomeDecorationHandler.java
@@ -28,13 +28,14 @@ public class BiomeDecorationHandler {
 				int z = event.chunkZ + event.rand.nextInt(16) + 8;
 				int y = event.world.getTopSolidOrLiquidBlock(x, z);
 
+				int color = event.rand.nextInt(16);
 				for(int j = 0; j < ConfigHandler.flowerDensity; j++) {
 					int x1 = x + event.rand.nextInt(8) - event.rand.nextInt(8);
 					int y1 = y + event.rand.nextInt(4) - event.rand.nextInt(4);
 					int z1 = z + event.rand.nextInt(8) - event.rand.nextInt(8);
 
 					if(event.world.isAirBlock(x1, y1, z1) && (!event.world.provider.hasNoSky || y1 < 127) && ModBlocks.flower.canBlockStay(event.world, x1, y1, z1))
-						event.world.setBlock(x1, y1, z1, ModBlocks.flower, event.rand.nextInt(16), 2);
+						event.world.setBlock(x1, y1, z1, ModBlocks.flower, color, 2);
 				}
 			}
 	}
