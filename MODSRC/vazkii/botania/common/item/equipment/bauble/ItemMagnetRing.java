3a96d2c2bc86ed23075a1de1ce640ad8e309ce19
Lizbeth Rika <vazkii@hotmail.com>
2014-10-09 23:11:06 +0100

    Smater magnet ring behaviour, it now goes into a 5 second cooldown whenever an item is tossed.

--- MODSRC/vazkii/botania/common/item/equipment/bauble/ItemMagnetRing.java
+++ MODSRC/vazkii/botania/common/item/equipment/bauble/ItemMagnetRing.java
@@ -13,43 +13,94 @@ package vazkii.botania.common.item.equipment.bauble;
 
 import java.util.List;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.IIcon;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
+import vazkii.botania.client.core.helper.IconHelper;
 import vazkii.botania.common.Botania;
+import vazkii.botania.common.core.helper.ItemNBTHelper;
 import vazkii.botania.common.core.helper.MathHelper;
 import vazkii.botania.common.core.helper.Vector3;
 import vazkii.botania.common.lib.LibItemNames;
 import baubles.api.BaubleType;
+import baubles.common.container.InventoryBaubles;
+import baubles.common.lib.PlayerHandler;
 
 public class ItemMagnetRing extends ItemBauble {
 
+	IIcon iconOff;
+	
+	private static final String TAG_COOLDOWN = "cooldown";
+	
 	public ItemMagnetRing() {
 		super(LibItemNames.MAGNET_RING);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IIconRegister par1IconRegister) {
+		itemIcon = IconHelper.forItem(par1IconRegister, this, 0);
+		iconOff = IconHelper.forItem(par1IconRegister, this, 1);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIconIndex(ItemStack stack) {
+		return getCooldown(stack) <= 0 ? itemIcon : iconOff;
+	}
+	
+	@SubscribeEvent
+	public void onTossItem(ItemTossEvent event) {
+		InventoryBaubles inv = PlayerHandler.getPlayerBaubles(event.player);
+		for(int i = 0; i < inv.getSizeInventory(); i++) {
+			ItemStack stack = inv.getStackInSlot(i);
+			if(stack != null && stack.getItem() == this)
+				setCooldown(stack, 100);
+		}
 	}
 
 	@Override
 	public void onWornTick(ItemStack stack, EntityLivingBase player) {
 		super.onWornTick(stack, player);
 
-		if(player.isSneaking()) {
-			int range = 6;
-			double x = player.posX;
-			double y = player.posY -(player.worldObj.isRemote ? 1.62 : 0) + 0.75;
-			double z = player.posZ;
+		int cooldown = getCooldown(stack);
+		
+		if(cooldown <= 0) {
+			if(!player.isSneaking()) {
+				int range = 6;
+				double x = player.posX;
+				double y = player.posY -(player.worldObj.isRemote ? 1.62 : 0) + 0.75;
+				double z = player.posZ;
 
-			List<EntityItem> items = player.worldObj.getEntitiesWithinAABB(EntityItem.class, AxisAlignedBB.getBoundingBox(x - range, y - range, z - range, x + range, y + range, z + range));
-			for(EntityItem item : items) {
-				MathHelper.setEntityMotionFromVector(item, new Vector3(x, y, z), 0.45F);
-				if(player.worldObj.isRemote) {
-					boolean red = player.worldObj.rand.nextBoolean();
-					Botania.proxy.sparkleFX(player.worldObj, item.posX, item.posY, item.posZ, red ? 1F : 0F, 0F, red ? 0F : 1F, 1F, 3);
+				List<EntityItem> items = player.worldObj.getEntitiesWithinAABB(EntityItem.class, AxisAlignedBB.getBoundingBox(x - range, y - range, z - range, x + range, y + range, z + range));
+				for(EntityItem item : items) {
+					MathHelper.setEntityMotionFromVector(item, new Vector3(x, y, z), 0.45F);
+					if(player.worldObj.isRemote) {
+						boolean red = player.worldObj.rand.nextBoolean();
+						Botania.proxy.sparkleFX(player.worldObj, item.posX, item.posY, item.posZ, red ? 1F : 0F, 0F, red ? 0F : 1F, 1F, 3);
+					}
 				}
 			}
-		}
+		} else setCooldown(stack, cooldown - 1);
 	}
 
+	public static int getCooldown(ItemStack stack) {
+		return ItemNBTHelper.getInt(stack, TAG_COOLDOWN, 0);
+	}
+	
+	public static void setCooldown(ItemStack stack, int cooldown) {
+		ItemNBTHelper.setInt(stack, TAG_COOLDOWN, cooldown);
+	}
+	
 	@Override
 	public BaubleType getBaubleType(ItemStack arg0) {
 		return BaubleType.RING;
