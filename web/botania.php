89963fdf8214ca7e5ca6a18c488f5973a82dda17
Lizbeth Rika <vazkii@hotmail.com>
2014-11-18 13:14:04 +0000

    Getting the website to look proper, some material design and basic work on the downloads page.

--- web/botania.php
+++ web/botania.php
@@ -4,7 +4,9 @@
 	<head>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
-		<link rel="stylesheet" href="botania.css">
+		<link rel="stylesheet" href="css/ripples.min.css">
+        <link rel="stylesheet" href="css/material-wfont.min.css">
+		<link rel="stylesheet" href="css/botania.css">
 	</head>
 
 	<body>
@@ -26,20 +28,25 @@
 				<li id="header-btn-index" class="header-btn"><a href="./index.php">Home</a></li>
 				<li id="header-btn-downloads" class="header-btn"><a href="./downloads.php">Downloads</a></li>
 				<li id="header-btn-changelog" class="header-btn"><a href="./changelog.php">Changelog</a></li>
+				<li id="header-btn-addons" class="header-btn"><a href="./addons.php">Addons</a></li>
+				<li id="header-btn-modpacks" class="header-btn"><a href="./modpacks.php">Modpacks</a></li>
 				<li id="header-btn-faq" class="header-btn"><a href="./faq.php">FAQ</a></li>
 				<li id="header-btn-credits" class="header-btn"><a href="./credits.php">Credits</a></li>
-				<li id="header-btn-back" class="header-btn"><a href="http://vazkii.us">Back to vazkii.us</a></li>
+				<li id="header-btn-back" class="header-btn"><a href="http://vazkii.us">vazkii.us</a></li>
 			  </ul>
 			</div>
 		  </div>
 		</div>
 		
-		<div class="container">
+		<div class="container module-contents">
 			<?php require_once "module/$module.php"; ?>
 		</div>
 		
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<script src="js/ripples.min.js"></script>
+        <script src="js/material.min.js"></script>
+		<script src="js/botania.js"></script>
 		
 		<?php echo "<script>\$(function(){\$('#header-btn-$module').addClass('active');});</script>"; ?>
 	</body>
--- web/module/downloads.php
+++ web/module/downloads.php
@@ -1,1 +1,63 @@
-asdf
+<?php
+	include 'dl.php';
+				
+	$files = scandir('files/');
+	natsort($files);
+	$files = array_reverse($files);
+	$versions = parse_ini_file('versions.ini');
+	$first = true;
+	$total_dls = 0;
+	$downloads_str = '';
+	$last_version = '';
+	$old = false;
+	
+	foreach($files as $file)
+		if(strlen($file) > 2) {
+			$dls_num = get_dls($file);
+			$total_dls += intval($dls_num);
+			$dls = number_format($dls_num);
+										
+			$file_version = substr($file, strlen('Botania '));
+			$version_name = substr($file_version, 0, strlen($file_version) - strlen('.jar'));
+			$mc_version = $versions[$version_name];
+			$version_name = str_replace('.', '-', $version_name);
+										
+			$dls_txt = 'Downloads';
+			if($dls_num == 0)
+				$dls_txt = 'Downloads :(';
+			elseif($dls_num == 1)
+				$dls_txt = 'Download';
+			
+			$div_id = 'version-' . str_replace('.', '-', $mc_version);
+			if($last_version != $mc_version && !$first)
+				$downloads_str .= "</div><br><font size='4'>Minecraft <b>$mc_version</b></font> <a class='hide-div' id='hide-$div_id'>(hide)</a><br><div id='$div_id'>";
+			
+			$downloads_str .= "<a href='dl.php?file=$file' class='btn btn-material-lightgreen dl'><b><span class='glyphicon glyphicon-download'></span> $file</b></a> <a href='changelog.php#$version_name' class='btn btn-succeed btn-material-cyan'><b>Changelog</b></a> <a class='btn btn-succeed btn-material-pink dl-counter'>Minecraft <b>$mc_version</b></a> <a href='dl.php?file=$file' class='btn btn-succeed btn-material-orange dl-counter'><b>$dls</b> $dls_txt</a>";
+			
+			if($first) {
+				$downloads_str .= '<hr><font size="6">Old Versions</font><div>';
+				$first = false;
+			} else {
+				$downloads_str .= '<br>';
+				$last_version = $mc_version;
+			}
+		}
+	$downloads_str .= '</div>';
+	
+	$total_dl_str = number_format($total_dls);
+	$mod_launch = strtotime('2014-02-28 17:32:22');
+	$current_time = time();
+	$days_passed = ($current_time - $mod_launch) / 86400;
+	$days_int = round($days_passed);
+	$daily_dls = round($total_dls / $days_passed, 2);
+	
+	print("<div class='total-dls'><span class='glyphicon glyphicon-star moe-scroll'></span> Botania has been downloaded a total of <b>$total_dl_str</b> times over $days_int days. (About <i>$daily_dls</i> dls/day)</div><span class='warning-txt'>Botania requires <a href='http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/1294623-baubles-1-0-1-5-updated-20-8-2014'>Baubles</a> to work properly, make sure you have it!<br>1.7.2 versions also work on 1.7.10!</span><br><br><font size='5'>Latest Download</font><br>");
+	print($downloads_str);
+	
+	$nekos = '<br><hr><br><div id="moe-counter">';
+	foreach(str_split($total_dls) as $num)
+		$nekos .= "<img src='img/moe/$num.gif'></ing>";
+	$nekos .= '<br>.jars served, whatcha waiting for? <a class="hashlink">(back to top)</a></div>';
+		
+	print($nekos);
+?>
