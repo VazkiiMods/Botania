3fb7f9bc2b00bd0b1ffd51a443ecd860424087e5
Lizbeth Rika <vazkii@hotmail.com>
2014-12-05 17:09:50 +0000

    Added effects to the Cloaks.

--- src/main/java/vazkii/botania/common/item/equipment/bauble/ItemUnholyCloak.java
+++ src/main/java/vazkii/botania/common/item/equipment/bauble/ItemUnholyCloak.java
@@ -11,10 +11,19 @@
  */
 package vazkii.botania.common.item.equipment.bauble;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import java.util.List;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import vazkii.botania.client.lib.LibResources;
+import vazkii.botania.common.Botania;
 import vazkii.botania.common.lib.LibItemNames;
 
 public class ItemUnholyCloak extends ItemHolyCloak {
@@ -23,6 +32,31 @@ public class ItemUnholyCloak extends ItemHolyCloak {
 
 	public ItemUnholyCloak() {
 		super(LibItemNames.UNHOLY_CLOAK);
+	}
+	
+	@Override
+	public boolean effectOnDamage(LivingHurtEvent event, EntityPlayer player, ItemStack stack) {
+		if(!event.source.isUnblockable()) {
+			int range = 6;
+			List<IMob> mobs = player.worldObj.getEntitiesWithinAABB(IMob.class, AxisAlignedBB.getBoundingBox(player.posX - range, player.posY - range, player.posZ - range, player.posX + range, player.posY + range, player.posZ + range));
+			for(IMob mob : mobs)
+				if(mob instanceof EntityLiving) {
+					EntityLiving entity = (EntityLiving) mob;
+					entity.attackEntityFrom(DamageSource.causePlayerDamage(player), event.ammount);
+				}
+			
+			player.worldObj.playSoundAtEntity(player, "botania:unholyCloak", 1F, 1F);
+			for(int i = 0; i < 90; i++) {
+				float rad = (float) i * 4F * (float) Math.PI / 180F;
+				float xMotion = (float) Math.cos(rad) * 0.2F;
+				float zMotion = (float) Math.sin(rad) * 0.2F;
+				Botania.proxy.wispFX(player.worldObj, player.posX, player.posY + 0.5, player.posZ, 0.4F + (float) Math.random() + 0.25F, 0F, 0F, 0.6F + (float) Math.random() * 0.2F, xMotion, 0F, zMotion);
+			}
+			
+			return true;
+		}
+		
+		return false;
 	}
 	
 	@Override
