17bc5ea3d72e37d14865160f57310987dc8eafb4
Lizbeth Rika <vazkii@hotmail.com>
2014-12-12 15:11:19 +0000

    Updated api version

--- src/main/java/vazkii/botania/api/package-info.java
+++ src/main/java/vazkii/botania/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Botania", apiVersion = "24", provides = "BotaniaAPI")
+@API(owner = "Botania", apiVersion = "25", provides = "BotaniaAPI")
 package vazkii.botania.api;
 import cpw.mods.fml.common.API;
 
