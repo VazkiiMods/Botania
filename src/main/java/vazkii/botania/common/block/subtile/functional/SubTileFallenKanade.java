89e17bcb0ad1e15ed69119a7cf069c2bda978397
Lizbeth Rika <vazkii@hotmail.com>
2015-01-24 22:26:15 +0000

    Made the Fallen Kanade not work in the end to prevent cheesing the Ender Dragon.

--- src/main/java/vazkii/botania/common/block/subtile/functional/SubTileFallenKanade.java
+++ src/main/java/vazkii/botania/common/block/subtile/functional/SubTileFallenKanade.java
@@ -30,7 +30,7 @@ public class SubTileFallenKanade extends SubTileFunctional {
 		final int range = 2;
 		final int cost = 120;
 
-		if(!supertile.getWorldObj().isRemote) {
+		if(!supertile.getWorldObj().isRemote && supertile.getWorldObj().provider.dimensionId != 1) {
 			List<EntityPlayer> players = supertile.getWorldObj().getEntitiesWithinAABB(EntityPlayer.class, AxisAlignedBB.getBoundingBox(supertile.xCoord - range, supertile.yCoord - range, supertile.zCoord - range, supertile.xCoord + range + 1, supertile.yCoord + range + 1, supertile.zCoord + range + 1));
 			for(EntityPlayer player : players) {
 				if(player.getActivePotionEffect(Potion.regeneration) == null && mana >= cost) {
