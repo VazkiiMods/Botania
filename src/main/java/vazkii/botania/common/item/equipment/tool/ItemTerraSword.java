d2f151bb7de634b936af1a95549f2c9cbed49f9a
Lizbeth Rika <vazkii@hotmail.com>
2015-02-21 00:10:56 +0000

    Added missing super call to ItemTerraSword.onUpdate. Closes #588

--- src/main/java/vazkii/botania/common/item/equipment/tool/ItemTerraSword.java
+++ src/main/java/vazkii/botania/common/item/equipment/tool/ItemTerraSword.java
@@ -48,6 +48,7 @@ public class ItemTerraSword extends ItemManasteelSword implements ILensEffect, I
 
 	@Override
 	public void onUpdate(ItemStack par1ItemStack, World par2World, Entity par3Entity, int par4, boolean par5) {
+		super.onUpdate(par1ItemStack, par2World, par3Entity, par4, par5);
 		if(par3Entity instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) par3Entity;
 			PotionEffect haste = player.getActivePotionEffect(Potion.digSpeed);
