9bd0e594d5e79591ed78aae541113a1fd25be328
SoundLogic <SoundLogic2236@gmail.com>
2015-04-18 20:31:14 -0500

    Made mana bursts and mana spreader's have more addon friendly code.

---
+++ src/main/java/vazkii/botania/api/mana/IManaSpreader.java
@@ -0,0 +1,36 @@
+/**
+ * This class was created by <SoundLogic>. It's distributed as
+ * part of the Botania Mod. Get the Source Code in github:
+ * https://github.com/Vazkii/Botania
+ * 
+ * Botania is Open Source and distributed under the
+ * Botania License: http://botaniamod.net/license.php
+ * 
+ * File Created @ [Apr 18, 2015, 7:30:00 PM (GMT)]
+ */
+package vazkii.botania.api.mana;
+
+import net.minecraft.world.World;
+
+/**
+ * Any TileEntity that implements this is considered a Mana Spreader,
+ * by which can fire mana bursts as a spreader.
+ * 
+ */
+public interface IManaSpreader extends IManaBlock {
+
+	public float getRotationX();
+	
+	public float getRotationY();
+	
+	public void setCanShoot(boolean canShoot);
+	
+	public int getBurstParticleTick();
+	
+	public void setBurstParticleTick(int i);
+	
+	public int getLastBurstDeathTick();
+	
+	public void setLastBurstDeathTick(int ticksExisted);
+
+}
--- src/main/java/vazkii/botania/api/package-info.java
+++ src/main/java/vazkii/botania/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Botania", apiVersion = "42", provides = "BotaniaAPI")
+@API(owner = "Botania", apiVersion = "43", provides = "BotaniaAPI")
 package vazkii.botania.api;
 import cpw.mods.fml.common.API;
 
