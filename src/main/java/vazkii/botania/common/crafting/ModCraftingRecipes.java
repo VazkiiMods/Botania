2496bf0d000f54af9bd9fd8fd5a2fb5cd56bc7be
Sam Quinn <lazersmoke@gmail.com>
2015-05-06 16:42:47 -0500

    Git was stupid so it did this:
    Add call to add Revealing recipe
    
    Create HelmRevealingRecipe.java
    
    Finished-ish Revealing Helm crafting handler
    
    This calls Item<type>HelmRevealing(ItemStack Item<type>Helm); but is NYI as of this writing
    
    Added contructor to copy NBT
    
    WARNING: I HAVE NO IDEA WHAT I AM DOING
    
    Fixed formatting
    
    That last commit made my code explode so I fixed it :)
    
    Let's forget that ever happened
    
    you saw nothing
    
    Added hook to custom crafting handler
    
    it might help
    
    Stable + hacky!
    
    yay
    
    These are a bunch of commits merged into one!
    
    Updated Everything so that it...
    
    actually makes sense. The remap is now like phantom ink instead of like
    Terra Pick Tipping. Everything here is tested and it resolved the issue!
    Also got rid of that fake recipe nonsense so that nei will work again!
    
    Cleanup
    
    you saw nothing
    
    Removed git crap
    
    gosh darnit why github why!?!

--- src/main/java/vazkii/botania/common/crafting/ModCraftingRecipes.java
+++ src/main/java/vazkii/botania/common/crafting/ModCraftingRecipes.java
@@ -1727,11 +1727,10 @@ public final class ModCraftingRecipes {
 
 		// Revealing Helmet Recipes
 		if(Botania.thaumcraftLoaded) {
-			Item goggles = (Item) Item.itemRegistry.getObject("Thaumcraft:ItemGoggles");
-			GameRegistry.addShapelessRecipe(new ItemStack(ModItems.manasteelHelmRevealing), new ItemStack(ModItems.manasteelHelm), new ItemStack(goggles));
-			recipeHelmetOfRevealing = BotaniaAPI.getLatestAddedRecipe();
-			GameRegistry.addShapelessRecipe(new ItemStack(ModItems.terrasteelHelmRevealing), new ItemStack(ModItems.terrasteelHelm), new ItemStack(goggles));
-			GameRegistry.addShapelessRecipe(new ItemStack(ModItems.elementiumHelmRevealing), new ItemStack(ModItems.elementiumHelm), new ItemStack(goggles));
+			GameRegistry.addShapelessRecipe(new ItemStack(ModItems.manasteelHelmRevealing), new ItemStack(ModItems.manasteelHelm), (Item) Item.itemRegistry.getObject("Thaumcraft:ItemGoggles"));
+			recipeHelmetOfRevealing = BotaniaAPI.getLatestAddedRecipe(); //We want manasteel to show in the Lexicon
+			GameRegistry.addShapelessRecipe(new ItemStack(ModItems.terrasteelHelmRevealing), new ItemStack(ModItems.terrasteelHelm), (Item) Item.itemRegistry.getObject("Thaumcraft:ItemGoggles"));
+			GameRegistry.addShapelessRecipe(new ItemStack(ModItems.elementiumHelmRevealing), new ItemStack(ModItems.elementiumHelm), (Item) Item.itemRegistry.getObject("Thaumcraft:ItemGoggles"));
 		}
 
 		// Slab & Stair Recipes
