52cb20754e538921ff5a9273d055635063d1d7c5
Sam Quinn <lazersmoke@gmail.com>
2015-05-15 19:21:00 -0500

    YOU MUST SPEEL EVRYTHING COREKTLY
    lol

--- src/main/java/vazkii/botania/common/lib/LibMisc.java
+++ src/main/java/vazkii/botania/common/lib/LibMisc.java
@@ -21,7 +21,7 @@ public final class LibMisc {
 	public static final String VERSION = "GRADLE:VERSION-" + BUILD;
 	public static final String DEPENDENCIES = "required-after:Forge@[10.12.2.1147,);required-after:Baubles;after:Thaumcraft";
 
-	// Network Contants
+	// Network Constants
 	public static final String NETWORK_CHANNEL = MOD_ID;
 
 	// Proxy Constants
