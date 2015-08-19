748b9cd3e3235b58c267898f898addeaea5c7d96
TheWhiteWolves <JMSmith5@uclan.ac.uk>
2015-08-19 18:44:26 +0100

    Increase api version
    Bad me, I forgot to do this

--- src/main/java/vazkii/botania/api/package-info.java
+++ src/main/java/vazkii/botania/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Botania", apiVersion = "59", provides = "BotaniaAPI")
+@API(owner = "Botania", apiVersion = "60", provides = "BotaniaAPI")
 package vazkii.botania.api;
 import cpw.mods.fml.common.API;
 
