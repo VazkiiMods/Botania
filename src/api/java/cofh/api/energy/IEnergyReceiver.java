5c98dc562c2083f32d16b91fd3f7e8c28b6c22dd
Vincent Lee <vincent.lee@outlook.com>
2015-11-18 00:54:48 -0600

    Update RF API to Progressive Automation / FluxDucts' variant

--- src/api/java/cofh/api/energy/IEnergyReceiver.java
+++ src/api/java/cofh/api/energy/IEnergyReceiver.java
@@ -1,6 +1,7 @@
 package cofh.api.energy;
 
-import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraft.util.EnumFacing;
+
 
 /**
  * Implement this interface on Tile Entities which should receive energy, generally storing it in one or more internal {@link IEnergyStorage} objects.
@@ -23,16 +24,16 @@ public interface IEnergyReceiver extends IEnergyConnection {
 	 *            If TRUE, the charge will only be simulated.
 	 * @return Amount of energy that was (or would have been, if simulated) received.
 	 */
-	int receiveEnergy(ForgeDirection from, int maxReceive, boolean simulate);
+	int receiveEnergy(EnumFacing facing, int maxReceive, boolean simulate);
 
 	/**
 	 * Returns the amount of energy currently stored.
 	 */
-	int getEnergyStored(ForgeDirection from);
+	int getEnergyStored(EnumFacing facing);
 
 	/**
 	 * Returns the maximum amount of energy that can be stored.
 	 */
-	int getMaxEnergyStored(ForgeDirection from);
+	int getMaxEnergyStored(EnumFacing facing);
 
 }
