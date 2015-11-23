653d0421279f1e12a6d897b97526f24d8fe0d59e
TheWhiteWolves <JMSmith5@uclan.ac.uk>
2015-11-23 19:32:03 +0000

    bump api version

--- src/main/java/vazkii/botania/api/package-info.java
+++ src/main/java/vazkii/botania/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Botania", apiVersion = "68", provides = "BotaniaAPI")
+@API(owner = "Botania", apiVersion = "69", provides = "BotaniaAPI")
 package vazkii.botania.api;
 import cpw.mods.fml.common.API;
 
