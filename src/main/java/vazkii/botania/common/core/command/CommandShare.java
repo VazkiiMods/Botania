2b62f61fd91f100435883f8ed16d11553313547c
Apollo Perry <alex@alexgeek.co.uk>
2015-11-27 00:10:42 +0000

    Fixed  /botania-share command to work with non-Botania lexicon entries.

--- src/main/java/vazkii/botania/common/core/command/CommandShare.java
+++ src/main/java/vazkii/botania/common/core/command/CommandShare.java
@@ -2,10 +2,10 @@
  * This class was created by <Vazkii>. It's distributed as
  * part of the Botania Mod. Get the Source Code in github:
  * https://github.com/Vazkii/Botania
- * 
+ *
  * Botania is Open Source and distributed under the
  * Botania License: http://botaniamod.net/license.php
- * 
+ *
  * File Created @ [Sep 23, 2014, 8:06:49 PM (GMT)]
  */
 package vazkii.botania.common.core.command;
@@ -34,7 +34,7 @@ public class CommandShare extends CommandBase {
 		String json = StatCollector.translateToLocal("botaniamisc.shareMsg");
 		json = json.replaceAll("%name%", sender.getCommandSenderName());
 		json = json.replaceAll("%entry%", args[0]);
-		json = json.replaceAll("%entryname%", StatCollector.translateToLocal("botania.entry." + args[0]));
+		json = json.replaceAll("%entryname%", StatCollector.translateToLocal(args[0]));
 
 		IChatComponent component = IChatComponent.Serializer.func_150699_a(json);
 		MinecraftServer.getServer().getConfigurationManager().sendChatMsg(component);
