c14b2c8aaf05ff82c84e22cc8498abbe0fd4bfb8
Michael Brenan <blacksmithgu@gmail.com>
2016-01-15 19:35:14 -0600

    Refactor client.challenge.* - Add comments and fix whitespace

--- src/main/java/vazkii/botania/client/challenge/Challenge.java
+++ src/main/java/vazkii/botania/client/challenge/Challenge.java
@@ -13,23 +13,58 @@ package vazkii.botania.client.challenge;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
+/**
+ * A in-game challenge as found in the Lexica Botania challenge sections -
+ * contains a simple name for the challenge, the display icon, the difficulty,
+ * and a marker for completion.
+ */
 public class Challenge {
 
+	/**
+	 * The unlocalized name of this challenge.
+	 */
 	public final String unlocalizedName;
+
+	/**
+	 * The icon to display in the visuals for this challenge.
+	 */
 	public final ItemStack icon;
+
+	/**
+	 * The percieved difficulty of this challenge.
+	 */
 	public final EnumChallengeLevel level;
+
+	/**
+	 * Whether or not this challenge is considered complete
+	 * (true if it is, false otherwise).
+	 */
 	public boolean complete = false;
 
+	/**
+	 * Creates a new challenge with the specified arguments.
+	 * @param unlocalizedName The unlocalized name of the challenge.
+	 * @param icon The icon of the challenge, represented by the icon of the provided item stack.
+	 * @param level The difficulty level of the challenge.
+	 */
 	public Challenge(String unlocalizedName, ItemStack icon, EnumChallengeLevel level) {
 		this.unlocalizedName = unlocalizedName;
 		this.icon = icon;
 		this.level = level;
 	}
 
+	/**
+	 * Write this challenge to the provided NBT tag.
+	 * @param cmp The tag to write this challenge to.
+	 */
 	public void writeToNBT(NBTTagCompound cmp) {
 		cmp.setBoolean(unlocalizedName, complete);
 	}
 
+	/**
+	 * Read this challenge from the provided NBT tag.
+	 * @param cmp The tag to read this challenge from.
+	 */
 	public void readFromNBT(NBTTagCompound cmp) {
 		complete = cmp.getBoolean(unlocalizedName);
 	}
--- src/main/java/vazkii/botania/client/challenge/ModChallenges.java
+++ src/main/java/vazkii/botania/client/challenge/ModChallenges.java
@@ -25,13 +25,26 @@ import vazkii.botania.common.lib.LibBlockNames;
 
 public final class ModChallenges {
 
-	public static final EnumMap<EnumChallengeLevel, List<Challenge>> challenges = new EnumMap(EnumChallengeLevel.class);
-	public static final HashMap<String, Challenge> challengeLookup = new HashMap();
+	/**
+	 * A map of challenge levels to a list of the challenges of that level.
+	 */
+	public static final EnumMap<EnumChallengeLevel, List<Challenge>> challenges =
+			new EnumMap<EnumChallengeLevel, List<Challenge>>(EnumChallengeLevel.class);
 
+	/**
+	 * A map of challenge names to their corresponding challenge objects.
+	 */
+	public static final HashMap<String, Challenge> challengeLookup = new HashMap<String, Challenge>();
+
+	/**
+	 * Initializes the default challenges in Botania, adding them to the lookup tables.
+	 */
 	public static void init() {
-		for(EnumChallengeLevel level : EnumChallengeLevel.class.getEnumConstants())
-			challenges.put(level, new ArrayList());
+		// Fill the challenge map with empty lists
+		for(EnumChallengeLevel level : EnumChallengeLevel.values())
+			challenges.put(level, new ArrayList<Challenge>());
 
+		// Easy Challenges
 		addChallenge(EnumChallengeLevel.EASY, "flowerFarm", new ItemStack(ModBlocks.flower, 1, 6));
 		addChallenge(EnumChallengeLevel.EASY, "recordFarm", new ItemStack(Items.record_13));
 		addChallenge(EnumChallengeLevel.EASY, "reedFarm", new ItemStack(Items.reeds));
@@ -39,6 +52,7 @@ public final class ModChallenges {
 		addChallenge(EnumChallengeLevel.EASY, "pureDaisy", ItemBlockSpecialFlower.ofType(LibBlockNames.SUBTILE_PUREDAISY));
 		addChallenge(EnumChallengeLevel.EASY, "battery", new ItemStack(ModBlocks.pool));
 
+		// Normal Challenges
 		addChallenge(EnumChallengeLevel.NORMAL, "apothecaryRefill", new ItemStack(ModBlocks.altar));
 		addChallenge(EnumChallengeLevel.NORMAL, "treeFarm", new ItemStack(Blocks.sapling));
 		addChallenge(EnumChallengeLevel.NORMAL, "fullCropFarm", new ItemStack(Items.wheat_seeds));
@@ -46,18 +60,29 @@ public final class ModChallenges {
 		addChallenge(EnumChallengeLevel.NORMAL, "boneMealFarm", new ItemStack(Items.dye, 1, 15));
 		addChallenge(EnumChallengeLevel.NORMAL, "orechid", ItemBlockSpecialFlower.ofType(LibBlockNames.SUBTILE_ORECHID));
 
+		// Hard Challenges
 		addChallenge(EnumChallengeLevel.HARD, "mobTower", new ItemStack(Items.bone));
 		addChallenge(EnumChallengeLevel.HARD, "entropinnyumSetup", ItemBlockSpecialFlower.ofType(LibBlockNames.SUBTILE_ENTROPINNYUM));
 		addChallenge(EnumChallengeLevel.HARD, "spectrolusSetup", ItemBlockSpecialFlower.ofType(LibBlockNames.SUBTILE_SPECTROLUS));
 		addChallenge(EnumChallengeLevel.HARD, "potionBrewer", new ItemStack(ModBlocks.brewery));
 
+		// You have a problem
 		addChallenge(EnumChallengeLevel.LUNATIC, "kekimurusSetup", ItemBlockSpecialFlower.ofType(LibBlockNames.SUBTILE_KEKIMURUS));
 		addChallenge(EnumChallengeLevel.LUNATIC, "autoQuarry", new ItemStack(Items.diamond_pickaxe));
 		addChallenge(EnumChallengeLevel.LUNATIC, "runeCrafter", new ItemStack(ModItems.rune));
 	}
 
+	/**
+	 * Adds a challenge to the Botania challenge registry, so that it will show up in the
+	 * Lexica Botania as a challenge.
+	 * @param level The difficulty level of this challenge.
+	 * @param name The name of this challenge.
+	 * @param icon The icon of this challenge, represented as the icon of the provided item stack.
+	 */
 	public static void addChallenge(EnumChallengeLevel level, String name, ItemStack icon) {
 		Challenge c = new Challenge("botania.challenge." + name, icon, level);
+
+		// Insert the challenge
 		challenges.get(level).add(c);
 		challengeLookup.put(c.unlocalizedName, c);
 	}
