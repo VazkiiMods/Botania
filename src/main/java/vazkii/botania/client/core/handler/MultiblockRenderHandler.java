7e642cb03af0d7c56a8ca010be8282e5db7e5218
Vincent Lee <vincent.lee@outlook.com>
2016-02-05 15:54:42 -0600

    Mop check for multiblocks. Closes #95

--- src/main/java/vazkii/botania/client/core/handler/MultiblockRenderHandler.java
+++ src/main/java/vazkii/botania/client/core/handler/MultiblockRenderHandler.java
@@ -96,7 +96,7 @@ public final class MultiblockRenderHandler {
 	@SubscribeEvent
 	public void onWorldRenderLast(RenderWorldLastEvent event) {
 		Minecraft mc = Minecraft.getMinecraft();
-		if(mc.thePlayer != null && mc.objectMouseOver != null && (!mc.thePlayer.isSneaking() || anchor != null)) {
+		if(mc.thePlayer != null && mc.objectMouseOver != null && mc.objectMouseOver.getBlockPos() != null && (!mc.thePlayer.isSneaking() || anchor != null)) {
 			mc.thePlayer.getCurrentEquippedItem();
 			renderPlayerLook(mc.thePlayer, mc.objectMouseOver);
 		}
