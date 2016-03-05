274b4ab05abc5d3fdd52239fae7026ccaec75ecd
Vincent Lee <vincent.lee@outlook.com>
2016-03-04 23:07:16 -0600

    onContentsChanged exists

--- src/main/java/vazkii/botania/common/block/tile/TileSimpleInventory.java
+++ src/main/java/vazkii/botania/common/block/tile/TileSimpleInventory.java
@@ -92,21 +92,20 @@ public abstract class TileSimpleInventory extends TileMod {
 		@Override
 		public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
 			if(allowWrite) {
-				ItemStack ret = super.insertItem(slot, stack, simulate);
-				if(!simulate)
-					tile.markDirty();
-				return ret;
+				return super.insertItem(slot, stack, simulate);
 			} else return stack;
 		}
 
 		@Override
 		public ItemStack extractItem(int slot, int amount, boolean simulate) {
 			if(allowWrite) {
-				ItemStack ret = super.extractItem(slot, amount, simulate);
-				if(!simulate)
-					tile.markDirty();
-				return ret;
+				return super.extractItem(slot, amount, simulate);
 			} else return null;
+		}
+
+		@Override
+		public void onContentsChanged(int slot) {
+			tile.markDirty();
 		}
 
 		// Override since our tile handles serialization itself
