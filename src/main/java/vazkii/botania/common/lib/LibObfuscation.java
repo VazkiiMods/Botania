5b9a5bf4804cae5d4c07dad0a3d62cdb981c9f04
Vincent Lee <vincent.lee@outlook.com>
2016-03-24 00:02:36 -0500

    Update most reflection names

--- src/main/java/vazkii/botania/common/lib/LibObfuscation.java
+++ src/main/java/vazkii/botania/common/lib/LibObfuscation.java
@@ -18,8 +18,8 @@ public final class LibObfuscation {
 	public static final String[] RENDERPOSZ = { "renderPosZ", "field_78723_d", "q" };
 
 	// EntityItem
-	public static final String[] AGE = { "age", "field_70292_b", "c" };
-	public static final String[] PICKUP_DELAY = { "delayBeforeCanPickup", "field_145804_b", "d" };
+	public static final String[] AGE = { "age", "field_70292_b", "d" };
+	public static final String[] PICKUP_DELAY = { "delayBeforeCanPickup", "field_145804_b", "e" };
 
 	// EffectRenderer
 	public static final String[] PARTICLE_TEXTURES = new String[] { "particleTextures", "field_110737_b", "b" };
@@ -29,69 +29,64 @@ public final class LibObfuscation {
 	public static final String[] TARGET_ENTITY = new String[] { "targetEntity", "field_75309_a", "d" };
 	public static final String[] TARGET_ENTITY_SELECTOR = new String[] { "targetEntitySelector", "field_82643_g", "c" };
 
-	// EntityAIAttackOnCollide
+	// EntityAIAttackMelee todo 1.9
 	public static final String[] CLASS_TARGET = new String[] { "classTarget", "field_75444_h", "g" };
 
 	// EntityAIAvoidEntity
-	public static final String[] TARGET_ENTITY_CLASS = new String[] { "field_181064_i", "i" };
+	public static final String[] TARGET_ENTITY_CLASS = new String[] { "classToAvoid", "field_181064_i", "i" };
 
 	// EntityCreeper
-	public static final String[] TIME_SINCE_IGNITED = new String[] {  "timeSinceIgnited", "field_70833_d", "c" };
-
-	// ThreadDownloadImageData
-	public static final String[] TEXTURE_UPLOADED = new String[] { "textureUploaded", "field_110559_g", "n" };
-	public static final String[] BUFFERED_IMAGE = new String[] { "bufferedImage", "field_110560_d", "l" };
+	public static final String[] TIME_SINCE_IGNITED = new String[] {  "timeSinceIgnited", "field_70833_d", "bw" };
 
 	// Entity
-	public static final String[] IS_IMMUNE_TO_FIRE = new String[] { "isImmuneToFire", "field_70178_ae", "ab" };
+	public static final String[] IS_IMMUNE_TO_FIRE = new String[] { "isImmuneToFire", "field_70178_ae", "Y" };
 
 	// ItemBlockSpecial
 	public static final String[] REED_ITEM = new String[] { "block", "field_150935_a", "a" };
 
 	// EntityAnimal
-	public static final String[] IN_LOVE = new String[] { "inLove", "field_70881_d", "bk" };
+	public static final String[] IN_LOVE = new String[] { "inLove", "field_70881_d", "bv" };
 
-	// EntityPlayer
-	public static final String[] ITEM_IN_USE = new String[] { "itemInUse", "field_71074_e", "g" };
-	public static final String[] ITEM_IN_USE_COUNT = new String[] { "itemInUseCount", "field_71072_f", "h" };
+	// EntityLivingBase
+	public static final String[] ITEM_IN_USE_COUNT = new String[] { "activeItemStackUseCount", "field_184628_bn", "bn" };
 
 	// EntityHorse
-	public static final String[] HORSE_JUMP_STRENGTH = new String[] { "horseJumpStrength", "field_110271_bv", "br" };
-	public static final String[] HORSE_CHEST = new String[] { "horseChest", "field_110296_bG", "bC" };
+	public static final String[] HORSE_JUMP_STRENGTH = new String[] { "horseJumpStrength", "field_110271_bv", "bC" };
+	public static final String[] HORSE_CHEST = new String[] { "horseChest", "field_110296_bG", "bR" };
 
 	// PlayerControllerMP
 	public static final String[] NET_CLIENT_HANDLER = new String[] { "netClientHandler", "field_78774_b", "b" };
 	public static final String[] CURRENT_GAME_TYPE = new String[] { "currentGameType", "field_78779_k", "i" };
 
 	// MobSpawnerBaseLogic
-	public static final String[] IS_ACTIVATED = new String[] { "isActivated" ,"func_98279_f", "g" };
-	public static final String[] SPAWN_RANGE = new String[] { "spawnRange", "field_98290_m", "m" };
-	public static final String[] SPAWN_COUNT = new String[] { "spawnCount", "field_98294_i", "i" };
-	public static final String[] MAX_NEARBY_ENTITIES = new String[] { "maxNearbyEntities", "field_98292_k", "k" };
-	public static final String[] MAX_SPAWN_DELAY = new String[] { "maxSpawnDelay", "field_98293_h", "h" };
-	public static final String[] MIN_SPAWN_DELAY = new String[] { "minSpawnDelay", "field_98283_g", "g" };
-	public static final String[] POTENTIAL_ENTITY_SPAWNS = new String[] { "minecartToSpawn", "field_98285_e", "c" }; // Todo 1.8 new name makes no sense -> "minecartToSpawn" see MCP name github issue
+	public static final String[] IS_ACTIVATED = new String[] { "isActivated" ,"func_98279_f", "h" };
+	public static final String[] SPAWN_RANGE = new String[] { "spawnRange", "field_98290_m", "l" };
+	public static final String[] SPAWN_COUNT = new String[] { "spawnCount", "field_98294_i", "h" };
+	public static final String[] MAX_NEARBY_ENTITIES = new String[] { "maxNearbyEntities", "field_98292_k", "j" };
+	public static final String[] MAX_SPAWN_DELAY = new String[] { "maxSpawnDelay", "field_98293_h", "g" };
+	public static final String[] MIN_SPAWN_DELAY = new String[] { "minSpawnDelay", "field_98283_g", "f" };
+	public static final String[] POTENTIAL_ENTITY_SPAWNS = new String[] { "minecartToSpawn", "field_98285_e", "b" }; // Todo 1.8 new name makes no sense -> "minecartToSpawn" see MCP name github issue
 	public static final String[] SPAWN_DELAY = new String[] { "spawnDelay", "field_98286_b", "a" };
-	public static final String[] PREV_MOB_ROTATION = new String[] { "prevMobRotation", "field_98284_d", "f" };
-	public static final String[] MOB_ROTATION = new String[] { "mobRotation", "field_98287_c", "e" };
-	public static final String[] GET_ENTITY_TO_SPAWN = new String[] { "getEntityNameToSpawn", "func_98276_e", "f" };
-	public static final String[] SPAWN_NEW_ENTITY = new String[] { "spawnNewEntity", "func_180613_a", "a" };
+	public static final String[] PREV_MOB_ROTATION = new String[] { "prevMobRotation", "field_98284_d", "e" };
+	public static final String[] MOB_ROTATION = new String[] { "mobRotation", "field_98287_c", "d" };
+	public static final String[] GET_ENTITY_TO_SPAWN = new String[] { "getEntityNameToSpawn", "func_98276_e", "g" };
+	public static final String[] SPAWN_NEW_ENTITY = new String[] { "spawnNewEntity", "func_180613_a", "a" }; // todo 1.9
 
 	// GuiIngame
-	public static final String[] REMAINING_HIGHLIGHT_TICKS = new String[] { "remainingHighlightTicks", "field_92017_k", "r" };
+	public static final String[] REMAINING_HIGHLIGHT_TICKS = new String[] { "remainingHighlightTicks", "field_92017_k", "q" };
 
 	// EntityThrowable
-	public static final String[] THROWER = new String[] { "thrower", "field_70192_c", "g" };
+	public static final String[] THROWER = new String[] { "thrower", "field_70192_c", "h" };
 
 	// GuiContainer
 	public static final String[] THE_SLOT = new String[] { "theSlot", "field_147006_u", "u" };
 
 	// GuiChat
 	public static final String[] INPUT_FIELD = new String[] { "inputField", "field_146415_a", "a" };
-	public static final String[] COMPLETE_FLAG = new String[] { "waitingOnAutocomplete", "field_146414_r", "r" };
+	public static final String[] COMPLETE_FLAG = new String[] { "waitingOnAutocomplete", "field_146414_r", "r" }; // todo 1.9
 
 	// Entityliving
-	public static final String[] GET_LIVING_SOUND = new String[] { "getLivingSound", "func_70639_aQ", "z" };
+	public static final String[] GET_LIVING_SOUND = new String[] { "getAmbientSound", "func_184639_G", "G" };
 	
 	// RenderGlobal
 	public static final String[] STAR_GL_CALL_LIST = new String[] { "starGLCallList", "field_72772_v", "p" };
@@ -106,11 +101,8 @@ public final class LibObfuscation {
 	public static final String[] DISABLE_LIGHTMAP = { "disableLightmap", "func_175072_h", "h" };
 	public static final String[] GET_FOV_MODIFIER = { "getFOVModifier", "func_78481_a", "a" };
 
-	// ItemRenderer
+	// ItemRenderer todo 1.9 dual wielding
 	public static final String[] PREV_EQUIPPED_PROGRESS = { "prevEquippedProgress", "field_78451_d", "f" };
 	public static final String[] EQUIPPED_PROGRESS = { "equippedProgress", "field_78454_c", "e" };
-
-	// RenderPlayer
-	public static final String[] ADD_LAYER = { "addLayer", "", "" };
 
 }
