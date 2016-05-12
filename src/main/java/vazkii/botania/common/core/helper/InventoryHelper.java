b0fad5db166a9950f2d8b62b6eeff60dc0915db3
Vincent Lee <vincent.lee@outlook.com>
2016-05-12 17:51:58 -0500

    Fix hopperhock being broken with vanilla chests

--- src/main/java/vazkii/botania/common/core/helper/InventoryHelper.java
+++ src/main/java/vazkii/botania/common/core/helper/InventoryHelper.java
@@ -107,13 +107,18 @@ public class InventoryHelper {
 
         if(te == null)
             return null;
-        IItemHandler ret = te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side) ?
-                te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side) : null;
-        if(te instanceof TileEntityChest)
-            ret = VanillaDoubleChestItemHandler.get(((TileEntityChest) te));
+
+		if(te instanceof TileEntityChest) {
+			IItemHandler doubleChest = VanillaDoubleChestItemHandler.get(((TileEntityChest) te));
+			if(doubleChest != VanillaDoubleChestItemHandler.NO_ADJACENT_CHESTS_INSTANCE)
+				return doubleChest;
+		}
+
+		IItemHandler ret = te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side) ?
+				te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side) : null;
+
         if(ret == null && te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null))
             ret = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null);
-
 
         return ret;
     }
