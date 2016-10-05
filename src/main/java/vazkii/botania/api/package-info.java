ca6361af2d217260248ef70562978ffad9eda504
Flaxbeard <theflaxbeard@gmail.com>
2016-10-04 22:04:42 -0500

    Updated package-info.java

--- src/main/java/vazkii/botania/api/package-info.java
+++ src/main/java/vazkii/botania/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Botania", apiVersion = "82", provides = "BotaniaAPI")
+@API(owner = "Botania", apiVersion = "83", provides = "BotaniaAPI")
 package vazkii.botania.api;
 import net.minecraftforge.fml.common.API;
 
