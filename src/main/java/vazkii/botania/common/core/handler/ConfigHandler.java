893683050a54f5daf3fc7ac3b98c316b76e8fc15
Vasco Lavos <vazkii@hotmail.com>
2016-10-23 16:14:08 +0100

    Added a config option to disable shedding entirely. This improves load times by not checking every mob. Closes #1969

--- src/main/java/vazkii/botania/common/core/handler/ConfigHandler.java
+++ src/main/java/vazkii/botania/common/core/handler/ConfigHandler.java
@@ -82,6 +82,7 @@ public final class ConfigHandler {
 	public static boolean ringOfOdinFireResist = true;
 	public static boolean invertMagnetRing = false;
 	public static boolean enableThaumcraftStablizers = true;
+	public static boolean enableShedding = false;
 
 	public static int harvestLevelWeight = 2;
 	public static int harvestLevelBore = 3;
@@ -229,6 +230,9 @@ public final class ConfigHandler {
 		desc = "Set this to false to disable Thaumcraft Infusion Stabilizing in botania blocks";
 		enableThaumcraftStablizers = loadPropBool("thaumraftStabilizers.enabled", desc, enableThaumcraftStablizers);
 
+		desc = "Set this to true to enable the Shedding feature from 1.7.10. You'll need to load the game to generate the options. No options are enabled by default. This increases load time.";
+		enableShedding = loadPropBool("shedding.enable", desc, enableShedding);
+		
 		desc = "The harvest level of the Mana Lens: Weight. 3 is diamond level. Defaults to 2 (iron level)";
 		harvestLevelWeight = loadPropInt("harvestLevel.weightLens", desc, harvestLevelWeight);
 
@@ -264,7 +268,8 @@ public final class ConfigHandler {
 	}
 
 	public static void loadPostInit() {
-		SheddingHandler.loadFromConfig(config);
+		if(enableShedding)
+			SheddingHandler.loadFromConfig(config);
 
 		if(config.hasChanged())
 			config.save();
