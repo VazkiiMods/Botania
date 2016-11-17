66a2ef509eda3910e01d18859e32cb47e5f79735
Vasco Lavos <vazkii@hotmail.com>
2016-11-17 15:20:59 +0000

    Fixed bad garden of glass download on the site. Redirect to curseforge instead.

--- web/module/gardenofglass.php
+++ web/module/gardenofglass.php
@@ -9,7 +9,7 @@ The goal of this addon is to allow for every single item in the game to be acqui
 The <b>GardenOfGlass.jar</b> file required to play this mode is simply an empty mod, used to signal to the base Botania mod to enable the GoG features.<br>
 This jar doesn't need to be updated, only the base Botania one does. In order for Garden of Glass to work, you need at least <b>Botania r1.7-199</b>.<br><br>
 <b><a href="https://twitter.com/Vazkii/status/622105452457906176">Garden of Glass for Server Owners</a></b><br><br>
-<a class="btn btn-lg btn-material-cyan" href="./downloads.php" role="button" style="width:500px">Download Garden of Glass</a>
+<a class="btn btn-lg btn-material-cyan" href="https://minecraft.curseforge.com/projects/botania-garden-of-glass" role="button" style="width:500px">Download Garden of Glass</a>
 <hr>
 
 <img class="featurette-image img-responsive" src="img/gog.png" alt="Generic placeholder image">
