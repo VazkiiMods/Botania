5b9a19231e05d81fd2b0ed9e676cf6bdc811ad05
Vincent Lee <vincent.lee@outlook.com>
2017-11-05 14:28:47 -0600

    Extra imports

--- src/main/java/vazkii/botania/common/block/mana/BlockPool.java
+++ src/main/java/vazkii/botania/common/block/mana/BlockPool.java
@@ -25,7 +25,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.tileentity.TileEntityFlowerPot;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
@@ -54,7 +53,6 @@ import vazkii.botania.common.lib.LibBlockNames;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
 import java.util.List;
 
 public class BlockPool extends BlockMod implements IWandHUD, IWandable, ILexiconable {
