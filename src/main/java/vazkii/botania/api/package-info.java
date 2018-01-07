980988a674ec1446acd73910390dc4d47bd4ee0a
Vincent Lee <vincent.lee@outlook.com>
2018-01-07 12:19:53 -0600

    Bump API version since last commit changes the API

--- src/main/java/vazkii/botania/api/package-info.java
+++ src/main/java/vazkii/botania/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Botania", apiVersion = "88", provides = "BotaniaAPI")
+@API(owner = "Botania", apiVersion = "89", provides = "BotaniaAPI")
 package vazkii.botania.api;
 import net.minecraftforge.fml.common.API;
 
