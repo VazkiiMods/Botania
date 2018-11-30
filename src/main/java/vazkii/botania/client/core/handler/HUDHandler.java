d0575240e690326590cc3f450157aa7a9e43af2a
Hubry <Hubry@users.noreply.github.com>
2018-11-30 05:35:05 +0100

    Add offline mode config option and localize all config options (#2780)

--- src/main/java/vazkii/botania/client/core/handler/HUDHandler.java
+++ src/main/java/vazkii/botania/client/core/handler/HUDHandler.java
@@ -18,7 +18,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiChat;
-import net.minecraft.client.gui.GuiIngame;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
@@ -35,7 +34,6 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
@@ -71,7 +69,6 @@ import vazkii.botania.common.item.equipment.bauble.ItemDodgeRing;
 import vazkii.botania.common.item.equipment.bauble.ItemFlightTiara;
 import vazkii.botania.common.item.equipment.bauble.ItemMonocle;
 import vazkii.botania.common.lib.LibMisc;
-import vazkii.botania.common.lib.LibObfuscation;
 
 import java.awt.Color;
 import java.util.List;
@@ -401,7 +398,7 @@ public final class HUDHandler {
 			}
 		}
 
-		if(!draw && pos.entityHit == null) {
+		if(!ConfigHandler.lexicaOfflineMode && !draw && pos.entityHit == null) {
 			profiler.startSection("wikiLookup");
 			if(!block.isAir(state, mc.world, pos.getBlockPos()) && !(block instanceof BlockLiquid)) {
 				IWikiProvider provider = WikiHooks.getWikiFor(block);
