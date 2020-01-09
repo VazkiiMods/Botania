3c1bcce64c6ca60d357039797a043787847cc015
Hubry <Hubry@users.noreply.github.com>
2020-01-10 00:32:58 +0100

    Replace GoG seed drops with a loot table inject

--- src/main/java/vazkii/botania/common/core/loot/LootHandler.java
+++ src/main/java/vazkii/botania/common/core/loot/LootHandler.java
@@ -32,7 +32,7 @@ public final class LootHandler {
 		}
 	}
 
-	private static LootPool getInjectPool(String entryName) {
+	public static LootPool getInjectPool(String entryName) {
 		return LootPool.builder()
 				.addEntry(getInjectEntry(entryName, 1))
 				.bonusRolls(0, 1)
