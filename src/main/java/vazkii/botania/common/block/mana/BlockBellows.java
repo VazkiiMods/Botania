221b049ebb40e5ed1b8237bde25c71a96c02debd
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-05-08 12:36:41 +0100

    Blank line.

--- src/main/java/vazkii/botania/common/block/mana/BlockBellows.java
+++ src/main/java/vazkii/botania/common/block/mana/BlockBellows.java
@@ -25,6 +25,7 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+
 import vazkii.botania.common.block.BlockMod;
 import vazkii.botania.common.block.tile.mana.TileBellows;
 import vazkii.botania.common.entity.EntityDoppleganger;
