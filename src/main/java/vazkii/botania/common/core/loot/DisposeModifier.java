873850148c8fb4b3e9c899e9e8610f1e77dae6e7
Vincent Lee <vincent.lee@outlook.com>
2020-06-26 20:17:32 -0500

    Fix dispose modifier, move JEI integration to disabled folder

--- src/main/java/vazkii/botania/common/core/loot/DisposeModifier.java
+++ src/main/java/vazkii/botania/common/core/loot/DisposeModifier.java
@@ -12,10 +12,10 @@ import com.google.gson.JsonObject;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
+import net.minecraft.loot.LootParameters;
+import net.minecraft.loot.conditions.ILootCondition;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootContext;
-import net.minecraft.world.storage.loot.LootParameters;
-import net.minecraft.world.storage.loot.conditions.ILootCondition;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
 import net.minecraftforge.event.RegistryEvent;
