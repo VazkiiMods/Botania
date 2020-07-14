ae8fbd29e6ba145b194171cef54e8b5910b48fdc
Vincent Lee <vincent.lee@outlook.com>
2020-07-14 17:08:18 -0500

    Separate segment generation from lightning particle itself

--- src/main/java/vazkii/botania/client/fx/FXLightning.java
+++ src/main/java/vazkii/botania/client/fx/FXLightning.java
@@ -11,6 +11,7 @@ package vazkii.botania.client.fx;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.IParticleRenderType;
@@ -38,40 +39,26 @@ import java.util.*;
 public class FXLightning extends Particle {
 
 	private static final int fadetime = 20;
-	private final Map<Integer, Integer> splitParents = new HashMap<>();
-	private final double length;
-	private final Random rand;
+	private final int expandTime;
 	private final int colorOuter;
 	private final int colorInner;
 
-	private List<FXLightningSegment> segments = new ArrayList<>();
-	private int segmentCount = 1;
-	private int splitCount;
-	private float speed = 1.5F;
+	private List<FXLightningSegment> segments;
+	private int segmentCount;
 
-	public FXLightning(ClientWorld world, Vector3 sourcevec, Vector3 targetvec, float ticksPerMeter, long seed, int colorOuter, int colorInner) {
+	public FXLightning(ClientWorld world, Vector3 sourcevec, Vector3 targetvec, float speed, long seed, int colorOuter, int colorInner) {
 		super(world, sourcevec.x, sourcevec.y, sourcevec.z);
-		rand = new Random(seed);
-		speed = ticksPerMeter;
 		this.colorOuter = colorOuter;
 		this.colorInner = colorInner;
-		length = targetvec.subtract(sourcevec).mag();
-		maxAge = fadetime + rand.nextInt(fadetime) - fadetime / 2;
+		double length = targetvec.subtract(sourcevec).mag();
+		maxAge = fadetime + world.rand.nextInt(fadetime) - fadetime / 2;
+		expandTime = (int) (length * speed);
 		age = -(int) (length * speed);
 
-		segments.add(new FXLightningSegment(sourcevec, targetvec));
-
-		fractal(2, length / 1.5, 0.7F, 0.7F, 45);
-		fractal(2, length / 4, 0.5F, 0.8F, 50);
-		fractal(2, length / 15, 0.5F, 0.9F, 55);
-		fractal(2, length / 30, 0.5F, 1.0F, 60);
-		fractal(2, length / 60, 0, 0, 0);
-		fractal(2, length / 100, 0, 0, 0);
-		fractal(2, length / 400, 0, 0, 0);
-
-		calculateCollisionAndDiffs();
-
-		segments.sort((o1, o2) -> Float.compare(o2.light, o1.light));
+		LightningSegmentGenerator gen = new LightningSegmentGenerator(seed);
+		Pair<Integer, List<FXLightningSegment>> res = gen.compute(sourcevec, targetvec, length);
+		segmentCount = res.getFirst();
+		segments = res.getSecond();
 	}
 
 	@Override
@@ -96,8 +83,6 @@ public class FXLightning extends Particle {
 		} else {
 			mainAlpha = 1 - boltAge * 0.5F;
 		}
-
-		int expandTime = (int) (length * speed);
 
 		int renderstart = (int) ((expandTime / 2 - maxAge + age) / (float) (expandTime / 2) * segmentCount);
 		int renderend = (int) ((age + expandTime) / (float) expandTime * segmentCount);
@@ -158,131 +143,6 @@ public class FXLightning extends Particle {
 				startvec.add(diff1).vertex(mat, wr);
 				wr.tex(0.5F, 1).lightmap(fullbright).color(r, g, b, a).endVertex();
 			}
-		}
-	}
-
-	private void fractal(int splits, double amount, double splitChance, double splitLength, double splitAngle) {
-		List<FXLightningSegment> oldSegments = segments;
-		segments = new ArrayList<>();
-
-		FXLightningSegment prev;
-
-		for (FXLightningSegment segment : oldSegments) {
-			prev = segment.prev;
-
-			Vector3 subsegment = segment.diff.multiply(1F / splits);
-
-			FXLightningBoltPoint[] newpoints = new FXLightningBoltPoint[splits + 1];
-
-			Vector3 startpoint = segment.startPoint.point;
-			newpoints[0] = segment.startPoint;
-			newpoints[splits] = segment.endPoint;
-
-			for (int i = 1; i < splits; i++) {
-				Vector3 randoff = segment.diff.perpendicular().normalize().rotate(rand.nextFloat() * 360, segment.diff);
-				randoff = randoff.multiply((rand.nextFloat() - 0.5F) * amount * 2);
-
-				Vector3 basepoint = startpoint.add(subsegment.multiply(i));
-
-				newpoints[i] = new FXLightningBoltPoint(basepoint, randoff);
-			}
-
-			for (int i = 0; i < splits; i++) {
-				FXLightningSegment next = new FXLightningSegment(newpoints[i], newpoints[i + 1], segment.light, segment.segmentNo * splits + i, segment.splitNo);
-				next.prev = prev;
-				if (prev != null) {
-					prev.next = next;
-				}
-
-				if (i != 0 && rand.nextFloat() < splitChance) {
-					Vector3 splitrot = next.diff.xCrossProduct().rotate(rand.nextFloat() * 360, next.diff);
-					Vector3 diff = next.diff.rotate((rand.nextFloat() * 0.66F + 0.33F) * splitAngle, splitrot).multiply(splitLength);
-
-					splitCount++;
-					splitParents.put(splitCount, next.splitNo);
-
-					FXLightningSegment split = new FXLightningSegment(newpoints[i], new FXLightningBoltPoint(newpoints[i + 1].basepoint, newpoints[i + 1].offsetvec.add(diff)), segment.light / 2F, next.segmentNo, splitCount);
-					split.prev = prev;
-
-					segments.add(split);
-				}
-
-				prev = next;
-				segments.add(next);
-			}
-
-			if (segment.next != null) {
-				segment.next.prev = prev;
-			}
-		}
-
-		segmentCount *= splits;
-	}
-
-	private float rayTraceResistance(Vector3 start, Vector3 end, float prevresistance) {
-		RayTraceContext ctx = new RayTraceContext(start.toVector3d(), end.toVector3d(), RayTraceContext.BlockMode.OUTLINE, RayTraceContext.FluidMode.NONE, null);
-		RayTraceResult mop = world.rayTraceBlocks(ctx);
-
-		if (mop.getType() == RayTraceResult.Type.BLOCK) {
-			BlockPos pos = ((BlockRayTraceResult) mop).getPos();
-			Block block = world.getBlockState(pos).getBlock();
-
-			if (world.isAirBlock(pos)) {
-				return prevresistance;
-			}
-
-			return prevresistance + block.getExplosionResistance() + 0.3F;
-		} else {
-			return prevresistance;
-		}
-	}
-
-	private void calculateCollisionAndDiffs() {
-		Map<Integer, Integer> lastactivesegment = new HashMap<>();
-
-		segments.sort((o1, o2) -> {
-			int comp = Integer.compare(o1.splitNo, o2.splitNo);
-			if (comp == 0) {
-				return Integer.compare(o1.segmentNo, o2.segmentNo);
-			} else {
-				return comp;
-			}
-		});
-
-		int lastSplitCalc = 0;
-		int lastActiveSegment = 0;// unterminated
-		float splitResistance = 0;
-
-		for (FXLightningSegment segment : segments) {
-			if (segment.splitNo > lastSplitCalc) {
-				lastactivesegment.put(lastSplitCalc, lastActiveSegment);
-				lastSplitCalc = segment.splitNo;
-				lastActiveSegment = lastactivesegment.get(splitParents.get(segment.splitNo));
-				splitResistance = lastActiveSegment < segment.segmentNo ? 50 : 0;
-			}
-
-			if (splitResistance >= 40 * segment.light) {
-				continue;
-			}
-
-			splitResistance = rayTraceResistance(segment.startPoint.point, segment.endPoint.point, splitResistance);
-			lastActiveSegment = segment.segmentNo;
-		}
-		lastactivesegment.put(lastSplitCalc, lastActiveSegment);
-
-		lastSplitCalc = 0;
-		lastActiveSegment = lastactivesegment.get(0);
-		for (Iterator<FXLightningSegment> iterator = segments.iterator(); iterator.hasNext();) {
-			FXLightningSegment segment = iterator.next();
-			if (lastSplitCalc != segment.splitNo) {
-				lastSplitCalc = segment.splitNo;
-				lastActiveSegment = lastactivesegment.get(segment.splitNo);
-			}
-
-			if (segment.segmentNo > lastActiveSegment) {
-				iterator.remove();
-			}
-			segment.calcEndDiffs();
 		}
 	}
 
