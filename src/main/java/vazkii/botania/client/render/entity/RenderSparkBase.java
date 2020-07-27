2e5e766c617056d2e07f1c9f995e0f73306f3575
Vincent Lee <vincent.lee@outlook.com>
2020-07-27 13:55:02 -0500

    Fix spark icons not spinning properly when daylight cycle is stopped

--- src/main/java/vazkii/botania/client/render/entity/RenderSparkBase.java
+++ src/main/java/vazkii/botania/client/render/entity/RenderSparkBase.java
@@ -41,10 +41,10 @@ public abstract class RenderSparkBase<T extends EntitySparkBase> extends EntityR
 
 		ms.push();
 
-		double time = tEntity.world.getDayTime() + partialTicks + new Random(tEntity.getEntityId()).nextInt(200);
+		double time = (tEntity.world.getGameTime() % 24000) + partialTicks + new Random(tEntity.getEntityId()).nextInt(200);
 		float a = 0.1F + (tEntity.isInvisible() ? 0 : 1) * 0.8F;
 
-		int alpha = (int) ((0.7F + 0.3F * (float) (Math.sin(time / 5.0) + 0.5) * 2) * a * 255.0F);
+		int alpha = (int) ((0.7 + 0.3 * (Math.sin(time / 5.0) + 0.5) * 2) * a * 255.0);
 		int iconColor = 0xFFFFFF | (alpha << 24);
 
 		float scale = 0.75F + 0.1F * (float) Math.sin(time / 10);
@@ -57,7 +57,7 @@ public abstract class RenderSparkBase<T extends EntitySparkBase> extends EntityR
 		renderIcon(ms, buffer, iicon, iconColor);
 
 		ms.push();
-		ms.translate(-0.02F + (float) Math.sin(time / 20) * 0.2F, 0.24F + (float) Math.cos(time / 20) * 0.2F, 0.005F);
+		ms.translate(-0.02 + Math.sin(time / 20) * 0.2, 0.24 + Math.cos(time / 20) * 0.2, 0.005);
 		ms.scale(0.2F, 0.2F, 0.2F);
 		int starColor = tEntity.getNetwork().getColorValue() | ((int) (a * 255.0F) << 24);
 		renderIcon(ms, buffer, MiscellaneousIcons.INSTANCE.corporeaIconStar, starColor);
@@ -65,7 +65,7 @@ public abstract class RenderSparkBase<T extends EntitySparkBase> extends EntityR
 
 		TextureAtlasSprite spinningIcon = getSpinningIcon(tEntity);
 		if (spinningIcon != null) {
-			ms.translate(-0.02F + (float) Math.sin(time / 20) * -0.2F, 0.24F + (float) Math.cos(time / 20) * -0.2F, 0.005F);
+			ms.translate(-0.02 + Math.sin(time / 20) * -0.2, 0.24 + Math.cos(time / 20) * -0.2, 0.005);
 			ms.scale(0.2F, 0.2F, 0.2F);
 			renderIcon(ms, buffer, spinningIcon, iconColor);
 		}
