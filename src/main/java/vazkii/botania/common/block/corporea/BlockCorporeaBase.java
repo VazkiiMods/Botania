b7c220189014a94583735a7abdfda0dcfdc2246a
Vincent Lee <vincent.lee@outlook.com>
2020-07-28 11:05:23 -0500

    Fix corporea blocks crashing when broken

--- src/main/java/vazkii/botania/common/block/corporea/BlockCorporeaBase.java
+++ src/main/java/vazkii/botania/common/block/corporea/BlockCorporeaBase.java
@@ -9,30 +9,14 @@
 package vazkii.botania.common.block.corporea;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.ITileEntityProvider;
-import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 import vazkii.botania.common.block.BlockMod;
-import vazkii.botania.common.block.tile.TileSimpleInventory;
-
-import javax.annotation.Nonnull;
 
 public abstract class BlockCorporeaBase extends BlockMod implements ITileEntityProvider {
 
 	public BlockCorporeaBase(Block.Properties builder) {
 		super(builder);
-	}
-
-	@Override
-	public void onReplaced(@Nonnull BlockState state, @Nonnull World world, @Nonnull BlockPos pos, @Nonnull BlockState newState, boolean isMoving) {
-		if (state.getBlock() != newState.getBlock()) {
-			TileSimpleInventory inv = (TileSimpleInventory) world.getTileEntity(pos);
-			InventoryHelper.dropInventoryItems(world, pos, inv.getItemHandler());
-			super.onReplaced(state, world, pos, newState, isMoving);
-		}
 	}
 
 }
