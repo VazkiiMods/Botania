a33d30100ec6408b970a6230f7191c71ba159f6c
Al Winfy <20421383+Alwinfy@users.noreply.github.com>
2020-08-06 14:26:21 -0500

    Update site (replace images/links, lint text) (#3380)

--- web/module/credits.php
+++ web/module/credits.php
@@ -4,33 +4,35 @@
 </div>
 
 
-No project is done with only one member, Botania is no exception, here's the cast of people to thank!<br><br>
+No project is completable with just one contributor, and Botania is no exception. Here's a cast of people to thank!<br><br>
+
+
 <ul>
-<li><b>All of my <a href="http://www.patreon.com/Vazkii">Patrons</a> and Donators</b> for being extremely kind as to support me every month with their own income.<br></li>
-<li><b>Adubbz</b> for the Pixie model and render from Biomes O Plenty.<br></li>
+<li><b>williewillus</b> has been responsible for porting and maintaining Botania from 1.8 all the way to the present. Send him your thanks!<br></li>
+<li><b>All of my <a href="http://www.patreon.com/Vazkii">Patrons</a> and Donators</b> for being kind enough to support me monthly with their own income.<br></li>
+<li><b>Adubbz</b> for the Pixie model and render from Biomes O' Plenty.<br></li>
 <li><b>Auric245524</b> for the old armor textures.<br></li>
-<li><b>Azanor</b> for letting me use his particles from Thaumcraft and for making the Baubles API.<br></li>
+<li><b>Azanor</b> for letting me use his particles from Thaumcraft and for creating the old Baubles API.<br></li>
 <li><b>ChickenBones</b> for the lightning effects from WRCBE.<br></li>
 <li><b>cpw and Player</b> for help with some internals.<br></li>
 <li><b>dylan4ever</b> for a bunch of texture work.<br></li>
-<li><b>Enterbrain</b> for a bunch of resources (both sound and graphical) from the RPG Maker VX Ace RTP.<br></li>
+<li><b>Enterbrain</b> for a bunch of resources (audio and graphics) from the RPG Maker VX Ace RTP.<br></li>
 <li><b>Flaxbeard</b> for the Rod of the Shaded Mesa, and for texture work when dylan isn't available.<br></li>
 <li><b>Futureazoo</b> for the Terra Shatterer texture and the alternate textures in the Unity texture pack style that were available in the config.<br></li>
-<li><b>Gelbooru</b> (nsfw) for the cute catgirls in the downloads page.<br></li>
 <li><b>Haighyorkie</b> for being a goldfish and always being supportive with making videos to show off the mod.<br></li>
+<li><b>Hubry</b> for contributing a whole bunch of bugfixes and improvements.<br></li>
 <li><b>Kain Vinosec</b> for the music, from <i>The Nothing Battles</i> used in the Gaia Guardian fight.<br></li>
 <li><b>Le_Alchemist</b> for providing many hours of shameless fun.<br></li>
-<li><b>Lorc</b> of <a href="http://game-icons.net/">game-icons.net</a> for the icons used in the main lexicon page.<br></li>
+<li><b>Lorc</b> of <a href="http://game-icons.net/">game-icons.net</a> for the icons that were used in the main lexicon page.<br></li>
 <li><b>MCPBot</b> for always being there for my obfuscation woes.<br></li>
 <li><b>MegaManki</b> for the mod's logo.<br></li>
-<li><b>Pokefenn</b> for the Hydroangeas and breaking all the things, every single time.<br></li>
+<li><b>Pokefenn</b> for the Hydroangeas and for breaking all the things, every single time.<br></li>
 <li><b>PowerCrystals</b> for the code for the spawner mover from MineFactoryReloaded.<br></li>
 <li><b>Rorax</b> for the Pylon model and texture.<br></li>
 <li><b>ScottWears</b> for hosting my website.<br></li>
 <li><b>The TConstruct and OpenBlocks teams</b> for code snippets I took from their repositories.<br></li>
 <li><b>TheIllusiveC4</b> for creating the Curios API.<br></li>
-<li><b>Tobbvald</b> for the Livingwood, Dreamwood and Lexica Botania textures and the alternate textures in the Unity texture pack style that are available in the config.<br></li>
+<li><b>Tobbvald</b> for the Livingwood, Dreamwood and Lexica Botania textures and the alternate textures in the Unity texture pack style that were available in the config.<br></li>
 <li><b>wiiv</b> for a bunch more texture work both before and after dylan4ever left.<br></li>
-<li><b>williewillus</b> for porting and maintaining Botania throughout 1.8 to the present.<br></li>
-<li><b>WTFG33ks</b> for making the getting started video that's linked within the Lexica Botania.<br></li>
+<li><b>WTFG33ks</b> for making the getting started video that was linked within the Lexica Botania.<br></li>
 </ul>
