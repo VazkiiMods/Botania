cc66f426e1e0beac2a4337d2cf9babb7182309d5
Vincent Lee <vincent.lee@outlook.com>
2020-09-09 23:41:40 -0500

    Port water bowl to LBA

--- src/main/java/vazkii/botania/common/item/ItemWaterBowl.java
+++ src/main/java/vazkii/botania/common/item/ItemWaterBowl.java
@@ -8,40 +8,53 @@
  */
 package vazkii.botania.common.item;
 
-import net.minecraft.fluid.Fluids;
+import alexiil.mc.lib.attributes.AttributeProviderItem;
+import alexiil.mc.lib.attributes.ItemAttributeList;
+import alexiil.mc.lib.attributes.Simulation;
+import alexiil.mc.lib.attributes.fluid.FluidExtractable;
+import alexiil.mc.lib.attributes.fluid.FluidVolumeUtil;
+import alexiil.mc.lib.attributes.fluid.amount.FluidAmount;
+import alexiil.mc.lib.attributes.fluid.filter.FluidFilter;
+import alexiil.mc.lib.attributes.fluid.volume.FluidKeys;
+import alexiil.mc.lib.attributes.fluid.volume.FluidVolume;
+import alexiil.mc.lib.attributes.misc.AbstractItemBasedAttribute;
+import alexiil.mc.lib.attributes.misc.LimitedConsumer;
+import alexiil.mc.lib.attributes.misc.Reference;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.nbt.CompoundTag;
-import javax.annotation.Nonnull;
 
-public class ItemWaterBowl extends Item {
+public class ItemWaterBowl extends Item implements AttributeProviderItem {
 
 	public ItemWaterBowl(Settings builder) {
 		super(builder);
 	}
 
-	@Nonnull
 	@Override
-	public ICapabilityProvider initCapabilities(ItemStack stack, CompoundTag nbt) {
-		return new Handler(stack);
+	public void addAllAttributes(Reference<ItemStack> stack, LimitedConsumer<ItemStack> excess, ItemAttributeList<?> to) {
+		to.offer(new Handler(stack, excess));
 	}
 
-	private static class Handler extends FluidHandlerItemStackSimple.SwapEmpty {
+	private static class Handler extends AbstractItemBasedAttribute implements FluidExtractable {
 
-		private Handler(ItemStack stack) {
-			super(stack, new ItemStack(Items.BOWL), FluidAttributes.BUCKET_VOLUME);
-			setFluid(new FluidStack(Fluids.WATER, FluidAttributes.BUCKET_VOLUME));
+		private Handler(Reference<ItemStack> stack, LimitedConsumer<ItemStack> excess) {
+			super(stack, excess);
 		}
 
 		@Override
-		public boolean canFillFluidType(FluidStack fluid) {
-			return false;
-		}
+		public FluidVolume attemptExtraction(FluidFilter filter, FluidAmount maxAmount, Simulation simulation) {
+			if (!filter.matches(FluidKeys.WATER) || !maxAmount.equals(FluidAmount.BUCKET)) {
+				return FluidVolumeUtil.EMPTY;
+			}
 
-		@Override
-		public boolean canDrainFluidType(FluidStack fluid) {
-			return fluid.getFluid() == Fluids.WATER;
+			ItemStack oldStack = stackRef.get().copy();
+			oldStack.decrement(1);
+
+			if (setStacks(simulation, oldStack, new ItemStack(Items.BOWL))) {
+				return FluidKeys.WATER.withAmount(FluidAmount.BUCKET);
+			} else {
+				return FluidVolumeUtil.EMPTY;
+			}
 		}
 	}
 
