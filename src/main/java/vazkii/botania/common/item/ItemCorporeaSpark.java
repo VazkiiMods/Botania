25a1186d3ce982ad89becec5a3cc6b58e8052c37
Vincent Lee <vincent.lee@outlook.com>
2020-10-02 21:18:57 -0500

    Use the same logic when determining whether a corporea spark can be placed as when it checks to pop off

--- src/main/java/vazkii/botania/common/item/ItemCorporeaSpark.java
+++ src/main/java/vazkii/botania/common/item/ItemCorporeaSpark.java
@@ -11,16 +11,14 @@ package vazkii.botania.common.item;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.items.CapabilityItemHandler;
 
 import vazkii.botania.api.corporea.CorporeaHelper;
 import vazkii.botania.common.entity.EntityCorporeaSpark;
 import vazkii.botania.common.entity.ModEntities;
+import vazkii.botania.common.impl.corporea.DummyCorporeaNode;
 import vazkii.botania.common.lib.ModTags;
 
 import javax.annotation.Nonnull;
@@ -37,25 +35,20 @@ public class ItemCorporeaSpark extends Item {
 		return attachSpark(ctx.getWorld(), ctx.getPos(), ctx.getItem()) ? ActionResultType.SUCCESS : ActionResultType.PASS;
 	}
 
-	private static boolean canPlaceOn(World world, BlockPos pos) {
-		if (world.getBlockState(pos).isIn(ModTags.Blocks.CORPOREA_SPARK_OVERRIDE)) {
-			return true;
-		}
-
-		TileEntity tile = world.getTileEntity(pos);
-		return tile != null
-				&& (tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, Direction.UP).isPresent()
-						|| tile.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).isPresent());
+	private static boolean canPlace(World world, EntityCorporeaSpark spark) {
+		return world.getBlockState(spark.getAttachPos()).isIn(ModTags.Blocks.CORPOREA_SPARK_OVERRIDE)
+				|| !(spark.getSparkNode() instanceof DummyCorporeaNode);
 	}
 
 	public static boolean attachSpark(World world, BlockPos pos, ItemStack stack) {
-		if (canPlaceOn(world, pos) && !CorporeaHelper.instance().doesBlockHaveSpark(world, pos)) {
+		EntityCorporeaSpark spark = ModEntities.CORPOREA_SPARK.create(world);
+		if (stack.getItem() == ModItems.corporeaSparkMaster) {
+			spark.setMaster(true);
+		}
+		spark.setPosition(pos.getX() + 0.5, pos.getY() + 1.25, pos.getZ() + 0.5);
+
+		if (canPlace(world, spark) && !CorporeaHelper.instance().doesBlockHaveSpark(world, pos)) {
 			if (!world.isRemote) {
-				EntityCorporeaSpark spark = ModEntities.CORPOREA_SPARK.create(world);
-				if (stack.getItem() == ModItems.corporeaSparkMaster) {
-					spark.setMaster(true);
-				}
-				spark.setPosition(pos.getX() + 0.5, pos.getY() + 1.25, pos.getZ() + 0.5);
 				world.addEntity(spark);
 				stack.shrink(1);
 			}
