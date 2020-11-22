d9ab36fee86454a0c3835979654a2c2c636a53bc
Vincent Lee <vincent.lee@outlook.com>
2020-11-22 09:08:25 -0600

    Reimplement mana burst no-drag as a couple line mixin instead of having to copy and maintain the vanilla method

--- src/main/java/vazkii/botania/common/entity/EntityManaBurst.java
+++ src/main/java/vazkii/botania/common/entity/EntityManaBurst.java
@@ -12,7 +12,6 @@ import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.entity.projectile.ThrowableEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.ItemStack;
@@ -22,9 +21,7 @@ import net.minecraft.network.IPacket;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraft.tags.ITag;
-import net.minecraft.tileentity.EndGatewayTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
@@ -146,68 +143,10 @@ public class EntityManaBurst extends ThrowableEntity implements IManaBurst {
 		setBurstMotion(mx, my, mz);
 	}
 
-	private void superUpdate() {
-		// Botania: inline Entity.tick()
-		{
-			if (!this.world.isRemote) {
-				this.setFlag(6, this.isGlowing());
-			}
-
-			this.baseTick();
-		}
-
-		RayTraceResult raytraceresult = ProjectileHelper.func_234618_a_(this, this::func_230298_a_);
-		boolean flag = false;
-		if (raytraceresult.getType() == RayTraceResult.Type.BLOCK) {
-			BlockPos blockpos = ((BlockRayTraceResult) raytraceresult).getPos();
-			BlockState blockstate = this.world.getBlockState(blockpos);
-			if (blockstate.isIn(Blocks.NETHER_PORTAL)) {
-				this.setPortal(blockpos);
-				flag = true;
-			} else if (blockstate.isIn(Blocks.END_GATEWAY)) {
-				TileEntity tileentity = this.world.getTileEntity(blockpos);
-				if (tileentity instanceof EndGatewayTileEntity) {
-					((EndGatewayTileEntity) tileentity).teleportEntity(this);
-				}
-
-				flag = true;
-			}
-		}
-
-		if (raytraceresult.getType() != RayTraceResult.Type.MISS && !flag && !net.minecraftforge.event.ForgeEventFactory.onProjectileImpact(this, raytraceresult)) {
-			this.onImpact(raytraceresult);
-		}
-
-		Vector3d vector3d = this.getMotion();
-		double d2 = this.getPosX() + vector3d.x;
-		double d0 = this.getPosY() + vector3d.y;
-		double d1 = this.getPosZ() + vector3d.z;
-		this.func_234617_x_();
-		float f;
-		if (this.isInWater()) {
-			for (int i = 0; i < 4; ++i) {
-				float f1 = 0.25F;
-				this.world.addParticle(ParticleTypes.BUBBLE, d2 - vector3d.x * 0.25D, d0 - vector3d.y * 0.25D, d1 - vector3d.z * 0.25D, vector3d.x, vector3d.y, vector3d.z);
-			}
-
-			f = 0.8F;
-		} else {
-			f = 0.99F;
-		}
-
-		// Botania: no drag this.setMotion(vector3d.scale((double)f));
-		if (!this.hasNoGravity()) {
-			Vector3d vector3d1 = this.getMotion();
-			this.setMotion(vector3d1.x, vector3d1.y - (double) this.getGravityVelocity(), vector3d1.z);
-		}
-
-		this.setPosition(d2, d0, d1);
-	}
-
 	@Override
 	public void tick() {
 		setTicksExisted(getTicksExisted() + 1);
-		superUpdate();
+		super.tick();
 
 		if (!fake && isAlive() && !scanBeam) {
 			ping();
