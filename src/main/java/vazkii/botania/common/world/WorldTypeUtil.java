eecf060a13d5f55d71422ff4fd80245363c6bb73
MelanX <info@melanx.de>
2020-11-26 19:00:41 +0100

    use mixin instead of ATs

--- src/main/java/vazkii/botania/common/world/WorldTypeUtil.java
+++ src/main/java/vazkii/botania/common/world/WorldTypeUtil.java
@@ -21,13 +21,15 @@ import net.minecraft.world.gen.settings.DimensionGeneratorSettings;
 import net.minecraft.world.storage.ServerWorldInfo;
 
 import vazkii.botania.common.Botania;
+import vazkii.botania.mixin.AccessorPropertyManager;
+import vazkii.botania.mixin.AccessorServerWorldInfo;
 
 import java.util.Locale;
 import java.util.Optional;
 
 public class WorldTypeUtil {
 	private static boolean isServerLevelSkyblock(DedicatedServer server) {
-		String levelType = Optional.ofNullable((String) server.getServerProperties().serverProperties.get("level-type")).map((str) -> str.toLowerCase(Locale.ROOT)).orElse("default");
+		String levelType = Optional.ofNullable((String) ((AccessorPropertyManager) server.getServerProperties()).getServerProperties().get("level-type")).map((str) -> str.toLowerCase(Locale.ROOT)).orElse("default");
 		return Botania.gardenOfGlassLoaded && levelType.equals("botania-skyblock");
 	}
 
@@ -37,12 +39,13 @@ public class WorldTypeUtil {
 		}
 		DynamicRegistries registries = server.func_244267_aX();
 		ServerWorldInfo worldInfo = (ServerWorldInfo) server.func_240793_aU_();
-		long seed = worldInfo.generatorSettings.getSeed();
+		DimensionGeneratorSettings generatorSettings = ((AccessorServerWorldInfo) worldInfo).getGeneratorSettings();
+		long seed = generatorSettings.getSeed();
 		Registry<DimensionType> dimensions = registries.getRegistry(Registry.DIMENSION_TYPE_KEY);
 		Registry<Biome> biomes = registries.getRegistry(Registry.BIOME_KEY);
 		Registry<DimensionSettings> dimensionSettings = registries.getRegistry(Registry.NOISE_SETTINGS_KEY);
 		SimpleRegistry<Dimension> simpleregistry = DimensionType.getDefaultSimpleRegistry(dimensions, biomes, dimensionSettings, seed);
 		SimpleRegistry<Dimension> skyblock = DimensionGeneratorSettings.func_242749_a(dimensions, simpleregistry, new SkyblockChunkGenerator(new OverworldBiomeProvider(seed, false, false, biomes), seed, () -> dimensionSettings.getOrThrow(DimensionSettings.field_242734_c)));
-		worldInfo.generatorSettings = new DimensionGeneratorSettings(worldInfo.generatorSettings.getSeed(), worldInfo.generatorSettings.doesGenerateFeatures(), worldInfo.generatorSettings.hasBonusChest(), skyblock);
+		((AccessorServerWorldInfo) worldInfo).setGeneratorSettings(new DimensionGeneratorSettings(generatorSettings.getSeed(), generatorSettings.doesGenerateFeatures(), generatorSettings.hasBonusChest(), skyblock));
 	}
 }
---
+++ src/main/java/vazkii/botania/mixin/AccessorPropertyManager.java
@@ -0,0 +1,22 @@
+/*
+ * This class is distributed as part of the Botania Mod.
+ * Get the Source Code in github:
+ * https://github.com/Vazkii/Botania
+ *
+ * Botania is Open Source and distributed under the
+ * Botania License: http://botaniamod.net/license.php
+ */
+package vazkii.botania.mixin;
+
+import net.minecraft.server.dedicated.PropertyManager;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Properties;
+
+@Mixin(PropertyManager.class)
+public interface AccessorPropertyManager {
+	@Accessor
+	Properties getServerProperties();
+}
