ea859f1d5bab2c33420e8f7d15a69a3eb0682245
Vincent Lee <vincent.lee@outlook.com>
2020-11-27 13:02:02 -0600

    Run data generators besides models/blockstates

--- src/main/java/vazkii/botania/mixin/MixinRecipesProvider.java
+++ src/main/java/vazkii/botania/mixin/MixinRecipesProvider.java
@@ -1,11 +1,13 @@
 package vazkii.botania.mixin;
 
+import net.minecraft.data.DataCache;
 import net.minecraft.data.server.RecipesProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vazkii.botania.data.recipes.BotaniaRecipeProvider;
 
 import java.util.function.Consumer;
@@ -22,5 +24,19 @@ public class MixinRecipesProvider {
 			return ignored -> {};
 		}
 		return consumer;
+	}
+
+	@Inject(at = @At(
+			shift = At.Shift.AFTER,
+			value = "INVOKE",
+			target = "Lnet/minecraft/data/server/RecipesProvider;generate(Ljava/util/function/Consumer;)V"
+		),
+		method = "run",
+		cancellable = true)
+	private void stop(DataCache cache, CallbackInfo ci) {
+		if (this instanceof BotaniaRecipeProvider) {
+			// Don't generate the "root.json", stop right after calling generate
+			ci.cancel();
+		}
 	}
 }
