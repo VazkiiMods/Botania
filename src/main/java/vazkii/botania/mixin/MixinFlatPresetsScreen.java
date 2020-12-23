5aaf6a1ece15844c8e4b716c391b768ff43e83b4
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-12-23 18:13:34 +0000

    Run spotless

--- src/main/java/vazkii/botania/mixin/MixinFlatPresetsScreen.java
+++ src/main/java/vazkii/botania/mixin/MixinFlatPresetsScreen.java
@@ -8,18 +8,8 @@
  */
 package vazkii.botania.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import vazkii.botania.common.block.ModBlocks;
-import vazkii.botania.common.item.ModItems;
-
 import net.minecraft.block.Blocks;
 import net.minecraft.client.gui.screen.FlatPresetsScreen;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.text.ITextComponent;
@@ -29,6 +19,15 @@ import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.gen.FlatLayerInfo;
 import net.minecraft.world.gen.feature.structure.Structure;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import vazkii.botania.common.block.ModBlocks;
+import vazkii.botania.common.item.ModItems;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -36,8 +35,7 @@ import java.util.List;
 public class MixinFlatPresetsScreen {
 
 	@Invoker("func_238640_a_")
-	public static void botania_addFlatWorldPreset(ITextComponent name, IItemProvider icon, RegistryKey<Biome> biome, List<Structure<?>> structures, boolean b1, boolean b2, boolean b3, FlatLayerInfo... layers) {
-	}
+	public static void botania_addFlatWorldPreset(ITextComponent name, IItemProvider icon, RegistryKey<Biome> biome, List<Structure<?>> structures, boolean b1, boolean b2, boolean b3, FlatLayerInfo... layers) {}
 
 	@Inject(method = "<clinit>", at = @At("RETURN"))
 	private static void onClinit(CallbackInfo ci) {
