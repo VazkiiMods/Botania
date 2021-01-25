4be5856e24ebf41b94f53ebab71ffc61e7a3a708
Vincent Lee <vincent.lee@outlook.com>
2021-01-24 19:58:52 -0600

    Fix williewillus/botania-fabric-issues#44

--- src/main/java/vazkii/botania/common/integration/corporea/VanillaNodeDetector.java
+++ src/main/java/vazkii/botania/common/integration/corporea/VanillaNodeDetector.java
@@ -8,9 +8,15 @@
  */
 package vazkii.botania.common.integration.corporea;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
+import net.minecraft.block.InventoryProvider;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ChestBlockEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SidedInventory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
@@ -26,11 +32,28 @@ public class VanillaNodeDetector implements ICorporeaNodeDetector {
 	@Nullable
 	@Override
 	public ICorporeaNode getNode(World world, ICorporeaSpark spark) {
-		BlockEntity te = world.getBlockEntity(spark.getAttachPos());
-		if (te instanceof SidedInventory) {
-			return new SidedVanillaCorporeaNode(world, spark.getAttachPos(), spark, (SidedInventory) te, Direction.UP);
-		} else if (te instanceof Inventory) {
-			return new VanillaCorporeaNode(world, spark.getAttachPos(), (Inventory) te, spark);
+		BlockPos pos = spark.getAttachPos();
+
+		// [VanillaCopy] HopperBlockEntity
+		Inventory inventory = null;
+		BlockState blockState = world.getBlockState(pos);
+		Block block = blockState.getBlock();
+		if (block instanceof InventoryProvider) {
+			inventory = ((InventoryProvider) block).getInventory(blockState, world, pos);
+		} else if (block.hasBlockEntity()) {
+			BlockEntity blockEntity = world.getBlockEntity(pos);
+			if (blockEntity instanceof Inventory) {
+				inventory = (Inventory) blockEntity;
+				if (inventory instanceof ChestBlockEntity && block instanceof ChestBlock) {
+					inventory = ChestBlock.getInventory((ChestBlock) block, blockState, world, pos, true);
+				}
+			}
+		}
+
+		if (inventory instanceof SidedInventory) {
+			return new SidedVanillaCorporeaNode(world, spark.getAttachPos(), spark, (SidedInventory) inventory, Direction.UP);
+		} else if (inventory != null) {
+			return new VanillaCorporeaNode(world, spark.getAttachPos(), inventory, spark);
 		}
 		return null;
 	}
