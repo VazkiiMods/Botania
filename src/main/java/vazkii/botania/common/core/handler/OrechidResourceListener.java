fdda0f47b5b465306eaa7d0c8e387d67598d4ad6
Hubry <26120076+Hubry@users.noreply.github.com>
2021-05-02 23:59:57 +0200

    Add removal of orechid entries or clearing them fully through datapacks
    The resource listener now has a format description in its javadoc.
    
    Note that they will no longer be loaded from files outside Botania's
    namespace, as accessing the full list of datapacks from the manager
    is client-only. This may change for 1.17.
    
    Closes #3713

--- src/main/java/vazkii/botania/common/core/handler/OrechidResourceListener.java
+++ src/main/java/vazkii/botania/common/core/handler/OrechidResourceListener.java
@@ -46,6 +46,27 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
+import static vazkii.botania.common.lib.ResourceLocationHelper.prefix;
+
+/**
+ * Loads the orechid data files.
+ * Files for the basic Orechid are located in {@code botania:orechid_ore_weights/orechid.json},
+ * files for the Orechid Ignem are in {@code botania:orechid_ore_weights/orechid_ignem.json}.
+ *
+ * <p>
+ * The structure of the file is loosely based on tag jsons. It consists of the following values:
+ * <ul>
+ * <li>{@code replace} - an optional boolean flag signifying that all values read from preceding
+ * datapacks should be discarded.</li>
+ * <li>{@code remove} - an optional JSON array of {@linkplain StateIngredient state ingredients} to remove.
+ * The exact same ingredients will be removed from the list.</li>
+ * <li>{@code values} (<i>required</i>) - a JSON array of serialized {@linkplain StateIngredient state ingredients}
+ * with an additional {@code weight} integer value.</li>
+ * </ul>
+ * </p>
+ *
+ * @see vazkii.botania.common.integration.crafttweaker.OrechidManager CraftTweaker Orechid integration
+ */
 public class OrechidResourceListener extends ReloadListener<OrechidResourceListener.Data> {
 	private static final Gson GSON = new Gson();
 
@@ -57,26 +78,22 @@ public class OrechidResourceListener extends ReloadListener<OrechidResourceListe
 	@Override
 	protected Data prepare(@Nonnull IResourceManager manager, @Nonnull IProfiler profiler) {
 		profiler.startSection("orechidParse");
-		Object2IntMap<StateIngredient> weights = loadWeights(manager, "orechid.json");
-		Object2IntMap<StateIngredient> nether = loadWeights(manager, "orechid_ignem.json");
+		Object2IntMap<StateIngredient> weights = loadWeights(manager, prefix("orechid.json"));
+		Object2IntMap<StateIngredient> nether = loadWeights(manager, prefix("orechid_ignem.json"));
 		profiler.endSection();
 		return new Data(weights, nether);
 	}
 
-	protected static Object2IntMap<StateIngredient> loadWeights(IResourceManager manager, String filename) {
+	protected static Object2IntMap<StateIngredient> loadWeights(IResourceManager manager, ResourceLocation filename) {
 		Stopwatch stopwatch = Stopwatch.createStarted();
 		Object2IntMap<StateIngredient> map = new Object2IntOpenHashMap<>();
-
-		for (String namespace : manager.getResourceNamespaces()) {
-			ResourceLocation location = new ResourceLocation(namespace, "orechid_ore_weights/" + filename);
-			try {
-				for (IResource resource : manager.getAllResources(location)) {
-					readResource(map, resource, location);
-				}
-			} catch (IOException ignored) {
-				// Probably data file missing in namespace, ignored
+		ResourceLocation location = new ResourceLocation(filename.getNamespace(), "orechid_ore_weights/" + filename.getPath());
+		try {
+			for (IResource resource : manager.getAllResources(location)) {
+				readResource(map, resource, location);
 			}
-		}
+		} catch (IOException ignored) {}
+
 		Botania.LOGGER.debug("Read {} data files in {}", filename, stopwatch.stop());
 		return map;
 	}
@@ -85,10 +102,25 @@ public class OrechidResourceListener extends ReloadListener<OrechidResourceListe
 		try (IResource r = resource;
 				InputStream stream = r.getInputStream();
 				BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+			Botania.LOGGER.debug("Reading {} from {}", r.getLocation(), r.getPackName());
 			JsonObject json = GSON.fromJson(reader, JsonObject.class);
 
-			for (JsonElement values : JSONUtils.getJsonArray(json, "values")) {
-				JsonObject object = values.getAsJsonObject();
+			if (JSONUtils.getBoolean(json, "replace", false)) {
+				map.clear();
+			}
+
+			if (JSONUtils.isJsonArray(json, "remove")) {
+				for (JsonElement value : JSONUtils.getJsonArray(json, "remove")) {
+					JsonObject object = value.getAsJsonObject();
+					StateIngredient state = StateIngredientHelper.tryDeserialize(object);
+					if (state != null) {
+						map.removeInt(state);
+					}
+				}
+			}
+
+			for (JsonElement value : JSONUtils.getJsonArray(json, "values")) {
+				JsonObject object = value.getAsJsonObject();
 				StateIngredient state = StateIngredientHelper.tryDeserialize(object);
 				if (state != null) {
 					int weight = JSONUtils.getInt(object, "weight");
