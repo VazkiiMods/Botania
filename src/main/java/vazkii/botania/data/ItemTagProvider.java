VazkiiMods/Botania

commit 73ad018bbfa995dd5e487a83f94cda9c24b03e7d

Remove IManaGivingItem and ItemGreaterAuraRing (#3461)
* Remove IManaGivingItem and ItemGreaterAuraRing

* I can't type

* Make ItemAuraRing.interval final

* Put back IManaGivingItem but deprecate it

src/main/java/vazkii/botania/data/ItemTagProvider.java
@@ -60,6 +60,7 @@ public class ItemTagProvider extends ItemTagsProvider {
 		this.getOrCreateBuilder(ItemTags.SMALL_FLOWERS).addTag(ModTags.Items.MYSTICAL_FLOWERS).addTag(ModTags.Items.SPECIAL_FLOWERS);
 
 		this.getOrCreateBuilder(ModTags.Items.BURST_VIEWERS).addItemEntry(ModItems.monocle);
+		this.getOrCreateBuilder(ModTags.Items.TERRA_PICK_BLACKLIST).add(ModItems.auraRing, ModItems.auraRingGreater, ModItems.terrasteelHelm);
 		this.getOrCreateBuilder(ModTags.Items.LENS).add(
 				Registry.ITEM.stream().filter(i -> i instanceof ItemLens && Registry.ITEM.getKey(i).getNamespace().equals(LibMisc.MOD_ID))
 						.sorted(Comparator.comparing(Registry.ITEM::getKey))

src/main/java/vazkii/botania/api/mana/IManaGivingItem.java
@@ -9,10 +9,9 @@
 package vazkii.botania.api.mana;
 
 /**
- * An item that implements this is an item that can give mana to other items.
- * This is used in botania for the terra shatterer to not be constantly
- * receiving mana from these items..
+ * @deprecated put the item in {@link vazkii.botania.common.lib.ModTags.Items#TERRA_PICK_BLACKLIST} instead.
  */
+@Deprecated
 public interface IManaGivingItem {
 
 }

src/main/java/vazkii/botania/common/item/ItemSpark.java
@@ -16,13 +16,12 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-import vazkii.botania.api.mana.IManaGivingItem;
 import vazkii.botania.api.mana.spark.ISparkAttachable;
 import vazkii.botania.common.entity.EntitySpark;
 
 import javax.annotation.Nonnull;
 
-public class ItemSpark extends Item implements IManaGivingItem {
+public class ItemSpark extends Item {
 
 	public ItemSpark(Properties builder) {
 		super(builder);

src/main/java/vazkii/botania/common/item/ModItems.java
@@ -268,8 +268,8 @@ public final class ModItems {
 	// Baubles / trinkets / curios / etc.
 	public static final Item manaRing = new ItemManaRing(unstackable());
 	public static final Item manaRingGreater = new ItemGreaterManaRing(unstackable());
-	public static final Item auraRing = new ItemAuraRing(unstackable());
-	public static final Item auraRingGreater = new ItemGreaterAuraRing(unstackable());
+	public static final Item auraRing = new ItemAuraRing(unstackable(), 10);
+	public static final Item auraRingGreater = new ItemAuraRing(unstackable(), 2);
 	public static final Item magnetRing = new ItemMagnetRing(unstackable());
 	public static final Item magnetRingGreater = new ItemMagnetRing(unstackable(), 16);
 	public static final Item waterRing = new ItemWaterRing(unstackable().rarity(Rarity.RARE));

src/main/java/vazkii/botania/common/item/equipment/armor/terrasteel/ItemTerrasteelHelm.java
@@ -25,7 +25,6 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
 import vazkii.botania.api.item.IAncientWillContainer;
 import vazkii.botania.api.mana.IManaDiscountArmor;
-import vazkii.botania.api.mana.IManaGivingItem;
 import vazkii.botania.api.mana.ManaItemHandler;
 import vazkii.botania.common.core.helper.ItemNBTHelper;
 
@@ -34,7 +33,7 @@ import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Locale;
 
-public class ItemTerrasteelHelm extends ItemTerrasteelArmor implements IManaDiscountArmor, IAncientWillContainer, IManaGivingItem {
+public class ItemTerrasteelHelm extends ItemTerrasteelArmor implements IManaDiscountArmor, IAncientWillContainer {
 
 	public static final String TAG_ANCIENT_WILL = "AncientWill";
 

src/main/java/vazkii/botania/common/item/equipment/bauble/ItemAuraRing.java
@@ -12,18 +12,20 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 
-import vazkii.botania.api.mana.IManaGivingItem;
 import vazkii.botania.api.mana.ManaItemHandler;
 
-public class ItemAuraRing extends ItemBauble implements IManaGivingItem {
+public class ItemAuraRing extends ItemBauble {
 
-	public ItemAuraRing(Properties props) {
+	private final int interval;
+
+	public ItemAuraRing(Properties props, int interval) {
 		super(props);
+		this.interval = interval;
 	}
 
 	@Override
 	public void onWornTick(ItemStack stack, LivingEntity player) {
-		if (!player.world.isRemote && player instanceof PlayerEntity && player.ticksExisted % 10 == 0) {
+		if (!player.world.isRemote && player instanceof PlayerEntity && player.ticksExisted % interval == 0) {
 			ManaItemHandler.instance().dispatchManaExact(stack, (PlayerEntity) player, 1, true);
 		}
 	}

src/main/java/vazkii/botania/common/item/equipment/bauble/ItemGreaterAuraRing.java
@@ -1,30 +1,0 @@
-/*
- * This class is distributed as part of the Botania Mod.
- * Get the Source Code in github:
- * https://github.com/Vazkii/Botania
- *
- * Botania is Open Source and distributed under the
- * Botania License: http://botaniamod.net/license.php
- */
-package vazkii.botania.common.item.equipment.bauble;
-
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-
-import vazkii.botania.api.mana.IManaGivingItem;
-import vazkii.botania.api.mana.ManaItemHandler;
-
-public class ItemGreaterAuraRing extends ItemBauble implements IManaGivingItem {
-
-	public ItemGreaterAuraRing(Properties props) {
-		super(props);
-	}
-
-	@Override
-	public void onWornTick(ItemStack stack, LivingEntity player) {
-		if (!player.world.isRemote && player instanceof PlayerEntity && player.ticksExisted % 2 == 0) {
-			ManaItemHandler.instance().dispatchManaExact(stack, (PlayerEntity) player, 1, true);
-		}
-	}
-}

src/main/java/vazkii/botania/common/item/equipment/tool/terrasteel/ItemTerraPick.java
@@ -32,7 +32,6 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.item.ISequentialBreaker;
-import vazkii.botania.api.mana.IManaGivingItem;
 import vazkii.botania.api.mana.IManaItem;
 import vazkii.botania.common.core.handler.ModSounds;
 import vazkii.botania.common.core.helper.ItemNBTHelper;
@@ -41,6 +40,7 @@ import vazkii.botania.common.item.ItemTemperanceStone;
 import vazkii.botania.common.item.equipment.tool.ToolCommons;
 import vazkii.botania.common.item.equipment.tool.manasteel.ItemManasteelPick;
 import vazkii.botania.common.item.relic.ItemThorRing;
+import vazkii.botania.common.lib.ModTags;
 
 import javax.annotation.Nonnull;
 
@@ -264,7 +264,7 @@ public class ItemTerraPick extends ItemManasteelPick implements IManaItem, ISequ
 
 	@Override
 	public boolean canReceiveManaFromItem(ItemStack stack, ItemStack otherStack) {
-		return !(otherStack.getItem() instanceof IManaGivingItem);
+		return !otherStack.getItem().isIn(ModTags.Items.TERRA_PICK_BLACKLIST);
 	}
 
 	@Override

commit 2b6c514ebe96ffa356285c81eab3bf2468fdcc9a

Fix terra shatterer charging from dispersive sparks
Caused by #3461 (73ad018)

src/main/java/vazkii/botania/data/ItemTagProvider.java
@@ -70,7 +70,7 @@ public class ItemTagProvider extends ItemTagsProvider {
 		this.getOrCreateBuilder(ItemTags.SMALL_FLOWERS).addTag(ModTags.Items.MYSTICAL_FLOWERS).addTag(ModTags.Items.SPECIAL_FLOWERS);
 
 		this.getOrCreateBuilder(ModTags.Items.BURST_VIEWERS).addItemEntry(ModItems.monocle);
-		this.getOrCreateBuilder(ModTags.Items.TERRA_PICK_BLACKLIST).add(ModItems.auraRing, ModItems.auraRingGreater, ModItems.terrasteelHelm);
+		this.getOrCreateBuilder(ModTags.Items.TERRA_PICK_BLACKLIST).add(ModItems.auraRing, ModItems.auraRingGreater, ModItems.terrasteelHelm, ModItems.spark);
 		this.getOrCreateBuilder(ModTags.Items.LENS).add(
 				Registry.ITEM.stream().filter(i -> i instanceof ItemLens && Registry.ITEM.getKey(i).getNamespace().equals(LibMisc.MOD_ID))
 						.sorted(Comparator.comparing(Registry.ITEM::getKey))