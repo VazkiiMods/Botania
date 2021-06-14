0d47edeb2aff05897073fff1f5c6db28defdccb0
Hubry <26120076+Hubry@users.noreply.github.com>
2021-06-14 23:21:10 +0200

    Check for fake players in loot table

--- src/main/java/vazkii/botania/common/core/loot/ModLootModifiers.java
+++ src/main/java/vazkii/botania/common/core/loot/ModLootModifiers.java
@@ -17,11 +17,13 @@ import static vazkii.botania.common.lib.ResourceLocationHelper.prefix;
 public class ModLootModifiers {
 	public static final LootConditionType TRUE_GUARDIAN_KILLER = new LootConditionType(new TrueGuardianKiller.Serializer());
 	public static final LootConditionType ENABLE_RELICS = new LootConditionType(new EnableRelics.Serializer());
+	public static final LootConditionType KILLED_BY_REAL_PLAYER = new LootConditionType(new RealPlayerCondition.Serializer());
 	public static final LootFunctionType BIND_UUID = new LootFunctionType(new BindUuid.Serializer());
 
 	public static void init() {
 		Registry.register(Registry.LOOT_CONDITION_TYPE, prefix("true_guardian_killer"), TRUE_GUARDIAN_KILLER);
 		Registry.register(Registry.LOOT_CONDITION_TYPE, prefix("enable_relics"), ENABLE_RELICS);
+		Registry.register(Registry.LOOT_CONDITION_TYPE, prefix("killed_by_player"), KILLED_BY_REAL_PLAYER);
 		Registry.register(Registry.LOOT_FUNCTION_TYPE, prefix("bind_uuid"), BIND_UUID);
 	}
 }
