5284d74dad4b0ce4b4872bc8030ce5995fe0b293
Vincent Lee <vincent@vincent-lee.net>
2021-06-20 01:19:15 -0500

    WIP contest details

---
+++ web/module/contest.php
@@ -0,0 +1,77 @@
+<div class='section-header'>
+	<span class='glyphicon glyphicon-briefcase'></span>
+	[TBD] The Summer 2021 Botania Build Contest
+</div>
+
+DETAILS NOT FINALIZED
+
+<h2>What</h2>
+The Vazkii's Mods developers are holding a Botania build contest!
+
+<h2>Who</h2>
+Players who are not members of the Vazkii's Mods team and who are not judges are eligible to participate.
+
+Judges:
+<ul>
+  <li>williewillus</li>
+  <li>Hubry</li>
+  <li>Alwinfy</li>
+  <li>...a couple more TBD</li>
+</ul>
+
+<h2>When</h2>
+Submission window: TBD<br />
+Judging period: TBD<br />
+Winners Announced: TBD<br />
+
+<h2>Rules</h2>
+About the build:
+<ul>
+  <li>The build must be a contraption that "does something" in Botania. Examples:
+    <ul>
+      <li>A Botania-powered tree farm</li>
+      <li>A fully-automated Entropinnyum setup</li>
+      <li>A high-chain-count Rannuncarpus or Gourmaryllis setup</li>
+      <li>A corporea-integrated storage system that has a fancy availability display and automatically-recrafts missing items</li>
+      <li>...Your imagination is the limit!</li>
+    </ul>
+    Please follow the spirit of this rule, rather than the letter.
+    If you're concerned about whether something will qualify, ask a member of the judge panel.
+  </li>
+  <li>The build must be completed on Botania version 416 or later.</li>
+  <li>Any functional blocks in the build must be from Botania and vanilla Minecraft. Decorative blocks from other mods are allowed.</li>
+  <li>The build should be reasonably sized (follow the spirit of the rule) i.e. do not just submit your entire base as a "contraption".</li>
+  <li>Abusing egregious glitches such as TNT duping is not allowed.</li>
+  <li>Datapacks are not allowed. Resource packs and shaders are allowed, but discouraged.</li>
+</ul>
+
+About the submission:
+<ul>
+  <li>Submissions may be made in your preferred medium (screenshot album, GIF, video). Please choose whichever you think will present your build in the best light.</li>
+  <li>
+    All submissions should be posted on the <a href="https://old.reddit.com/r/Botania">Botania subreddit</a>.
+    Additionally, please PM /u/williewillus a copy of your world.
+    Whether you publish your world otherwise is your choice.
+  </li>
+  <li>
+    If you would like to share your submission elsewhere (for example, on /r/minecraft or /r/feedthebeast),
+    feel free to crosspost your /r/Botania post into those subreddits.
+  </li>
+  <li>Submissions must be clearly marked as being entries for the build contest. Adding "[Botania build contest]" to the title will do.</li>
+</ul>
+
+Your build will be judged on the following aspects, in rough order of importance from most important to least important:
+<ul>
+  <li>Creativity: Does this build make use of vanilla and Botania mechanics in a interesting or unexpected manner?</li>
+  <li>Utility: Is this a build that would be useful to have in a world?</li>
+  <li>Style and Coolness: Does this build make you think "wow, that's cool" when watching it run?</li>
+  <li>Appearance: Is this build decorated nicely?</li>
+  <li>Presentation: Does the submission post represent the build well?</li>
+</ul>
+
+<h2>Prizes?</h2>
+The winning build will be featured on the Botania website homepage (you'll have to provide us a GIF of your build, if you didn't submit one),
+and will also receive a $50 Steam gift card.<br />
+The runner-up build will receive a $25 Steam gift card.<br />
+Both the winner and runner-up will also be given a Botania head-flower of their choice.<br />
+Some honorable mentions may be given, which come with no prize besides bragging rights.
