75291f76a3c5895080b65b4c6072ad2dc60a21a9
Hubry <26120076+Hubry@users.noreply.github.com>
2021-06-22 02:03:51 +0200

    Revert "Hide labellia stuff and remove recipe until we get the texture"
    This reverts commit 3eff0fe0

--- src/main/java/vazkii/botania/client/integration/jei/JEIBotaniaPlugin.java
+++ src/main/java/vazkii/botania/client/integration/jei/JEIBotaniaPlugin.java
@@ -11,7 +11,6 @@ package vazkii.botania.client.integration.jei;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaRecipeCategoryUid;
-import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.recipe.IRecipeManager;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
@@ -66,7 +65,6 @@ import vazkii.botania.common.item.equipment.tool.terrasteel.ItemTerraPick;
 import javax.annotation.Nonnull;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
@@ -218,9 +216,6 @@ public class JEIBotaniaPlugin implements IModPlugin {
 				.ifPresent(r -> recipeRegistry.hideRecipe(r, PetalApothecaryRecipeCategory.UID));
 		recipeManager.getRecipe(prefix("petal_apothecary/nightshade_motif"))
 				.ifPresent(r -> recipeRegistry.hideRecipe(r, PetalApothecaryRecipeCategory.UID));
-
-		jeiRuntime.getIngredientManager().removeIngredientsAtRuntime(VanillaTypes.ITEM, //TODO unhide labelia when ready
-				Arrays.asList(new ItemStack(ModSubtiles.labelia), new ItemStack(ModSubtiles.labeliaFloating)));
 
 		CorporeaInputHandler.jeiPanelSupplier = () -> {
 			Object o = jeiRuntime.getIngredientListOverlay().getIngredientUnderMouse();
