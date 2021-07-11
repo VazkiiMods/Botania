dbe820e9690d2edbd1a007963d6c8bce27d37464
Vincent Lee <vincent@vincent-lee.net>
2021-07-11 15:42:24 -0500

    Remove Quat. The only thing using it was Vector3.rotate which is easily done with a vector-only formula. :dragnmelt:

--- src/main/java/vazkii/botania/common/core/helper/Vector3.java
+++ src/main/java/vazkii/botania/common/core/helper/Vector3.java
@@ -14,6 +14,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.api.distmarker.Dist;
@@ -193,8 +194,22 @@ public class Vector3 {
 		return b.multiply(m);
 	}
 
-	public Vector3 rotate(double angle, Vector3 axis) {
-		return Quat.aroundAxis(axis.normalize(), angle).rotate(this);
+	public Vector3 rotate(double theta, Vector3 axis) {
+		if (MathHelper.epsilonEquals(theta, 0)) {
+			return this;
+		}
+
+		// Rodrigues rotation formula
+		Vector3 k = axis.normalize();
+		Vector3 v = this;
+
+		float cosTheta = MathHelper.cos((float) theta);
+		Vector3 firstTerm = v.multiply(cosTheta);
+		Vector3 secondTerm = k.crossProduct(v).multiply(MathHelper.sin((float) theta));
+		Vector3 thirdTerm = k.multiply(k.dotProduct(v) * (1 - cosTheta));
+		return new Vector3(firstTerm.x + secondTerm.x + thirdTerm.x,
+				firstTerm.y + secondTerm.y + thirdTerm.y,
+				firstTerm.z + secondTerm.z + thirdTerm.z);
 	}
 
 	public AxisAlignedBB boxForRange(double range) {
