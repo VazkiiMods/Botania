85094784721192764c2d0321840bf27d164e7a24
Vincent Lee <vincent.lee@outlook.com>
2021-08-30 20:46:16 -0500

    Fix int color values being wrong

--- src/main/java/vazkii/botania/common/core/helper/ColorHelper.java
+++ src/main/java/vazkii/botania/common/core/helper/ColorHelper.java
@@ -45,8 +45,8 @@ public final class ColorHelper {
 	public static int getColorValue(DyeColor color) {
 		float[] colors = color.getTextureDiffuseColors();
 		int r = (int) (colors[0] * 255.0F);
-		int g = (int) (colors[0] * 255.0F);
-		int b = (int) (colors[0] * 255.0F);
+		int g = (int) (colors[1] * 255.0F);
+		int b = (int) (colors[2] * 255.0F);
 		return (r << 16) | (g << 8) | b;
 	}
 
