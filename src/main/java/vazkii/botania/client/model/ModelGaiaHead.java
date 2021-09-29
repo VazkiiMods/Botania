4518d37a3a23eb3eb1d4ddf9ff4bd9476f3a7567
Vincent Lee <vincent.lee@outlook.com>
2021-09-28 20:56:01 -0700

    Fix gaia head render. A bit hacky but it works.

--- src/main/java/vazkii/botania/client/model/ModelGaiaHead.java
+++ src/main/java/vazkii/botania/client/model/ModelGaiaHead.java
@@ -13,10 +13,24 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 
 import net.minecraft.client.model.SkullModelBase;
 
+import vazkii.botania.client.render.tile.RenderTileGaiaHead;
+
 public class ModelGaiaHead extends SkullModelBase {
 	@Override
-	public void setupAnim(float animationProgress, float yRot, float xRot) {}
+	public void setupAnim(float animationProgress, float yRot, float xRot) {
+		var type = RenderTileGaiaHead.getViewType();
+		var model = RenderTileGaiaHead.models.get(type);
+		if (model != null) {
+			model.setupAnim(animationProgress, yRot, xRot);
+		}
+	}
 
 	@Override
-	public void renderToBuffer(PoseStack ms, VertexConsumer buffer, int light, int overlay, float r, float g, float b, float a) {}
+	public void renderToBuffer(PoseStack ms, VertexConsumer buffer, int light, int overlay, float r, float g, float b, float a) {
+		var type = RenderTileGaiaHead.getViewType();
+		var model = RenderTileGaiaHead.models.get(type);
+		if (model != null) {
+			model.renderToBuffer(ms, buffer, light, overlay, r, g, b, a);
+		}
+	}
 }
--- src/main/java/vazkii/botania/mixin/MixinSkullBlockRenderer.java
+++ src/main/java/vazkii/botania/mixin/MixinSkullBlockRenderer.java
@@ -9,15 +9,13 @@
 package vazkii.botania.mixin;
 
 import com.google.common.collect.ImmutableMap;
-import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.authlib.GameProfile;
 
 import net.minecraft.client.model.SkullModelBase;
 import net.minecraft.client.model.geom.EntityModelSet;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
 import net.minecraft.client.resources.DefaultPlayerSkin;
-import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.SkullBlock;
 
@@ -26,21 +24,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import vazkii.botania.client.model.ModelGaiaHead;
+import vazkii.botania.client.render.tile.RenderTileGaiaHead;
 import vazkii.botania.common.block.BlockGaiaHead;
 
 import java.util.Map;
 
 @Mixin(SkullBlockRenderer.class)
 public abstract class MixinSkullBlockRenderer {
-	@Shadow
-	@Final
-	private Map<SkullBlock.Type, SkullModelBase> modelByType;
-
 	@Shadow
 	@Final
 	private static Map<SkullBlock.Type, ResourceLocation> SKIN_BY_TYPE;
@@ -54,23 +48,14 @@ public abstract class MixinSkullBlockRenderer {
 			ImmutableMap.Builder<SkullBlock.Type, SkullModelBase> builder) {
 		builder.put(BlockGaiaHead.GAIA_TYPE, new ModelGaiaHead());
 
-		// todo 1.17 placeholder to avoid crash
-		if (!SKIN_BY_TYPE.containsKey(BlockGaiaHead.GAIA_TYPE)) {
-			SKIN_BY_TYPE.put(BlockGaiaHead.GAIA_TYPE, DefaultPlayerSkin.getDefaultSkin());
-		}
+		// placeholder to avoid crash
+		SKIN_BY_TYPE.put(BlockGaiaHead.GAIA_TYPE, DefaultPlayerSkin.getDefaultSkin());
 	}
 
-	/**
-	 * This hook is necessary instead of just overriding render() in RenderTileGaiaHead as normal
-	 * because vanilla ItemStackTileEntityRenderer checks for skull blocks and calls this static method directly.
-	 */
-	@Inject(at = @At("HEAD"), method = "renderSkull", cancellable = true)
-	private static void onRender(Direction direction, float angle, float animationProgress, PoseStack ms,
-			MultiBufferSource buffers, int light, SkullModelBase skullModelBase,
-			RenderType renderType, CallbackInfo ci) {
-		if (skullModelBase instanceof ModelGaiaHead) {
-			// todo 1.17 RenderTileGaiaHead.gaiaRender(direction, angle, animationProgress, ms, buffers, light, this.modelByType);
-			ci.cancel();
+	@Inject(at = @At("HEAD"), method = "getRenderType", cancellable = true)
+	private static void hookGetRenderType(SkullBlock.Type type, GameProfile gameProfile, CallbackInfoReturnable<RenderType> cir) {
+		if (type == BlockGaiaHead.GAIA_TYPE) {
+			RenderTileGaiaHead.hookGetRenderType(cir);
 		}
 	}
 }
