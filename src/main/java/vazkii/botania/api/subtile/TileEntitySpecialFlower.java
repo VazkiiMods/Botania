6bd57a394b6a8c682d2a7e6f739e3468d438152a
quat1024 <quat1024@users.noreply.github.com>
2021-10-03 00:56:10 -0400

    No need.

--- src/main/java/vazkii/botania/api/subtile/TileEntitySpecialFlower.java
+++ src/main/java/vazkii/botania/api/subtile/TileEntitySpecialFlower.java
@@ -60,7 +60,7 @@ public class TileEntitySpecialFlower extends BlockEntity implements IWandBindabl
 		}
 	};
 
-	protected int ticksExisted = 0;
+	public int ticksExisted = 0;
 
 	/** true if this flower is working on Enchanted Soil **/
 	public boolean overgrowth = false;
