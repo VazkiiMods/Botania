0d85bc89f5e97d670ccb547133b87e388f21f9a0
Vincent Lee <vincent.lee@outlook.com>
2021-10-08 20:28:01 -0700

    First import of Mekanism's lightning bolt effects, not hooked up yet

---
+++ src/main/java/vazkii/botania/client/fx/BoltEffect.java
@@ -0,0 +1,453 @@
+/*
+ * This class is distributed as part of the Botania Mod.
+ * Get the Source Code in github:
+ * https://github.com/Vazkii/Botania
+ *
+ * Botania is Open Source and distributed under the
+ * Botania License: http://botaniamod.net/license.php
+ */
+package vazkii.botania.client.fx;
+/*
+ * This class is derived from the Mekanism project.
+ * See ALTERNATE_LICENSES.txt.
+ * Imported October 2021 and heavily modified.
+ */
+
+import com.mojang.math.Vector4f;
+
+import net.minecraft.world.phys.Vec3;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+
+public class BoltEffect {
+
+	private final Random random = new Random();
+
+	private final BoltRenderInfo renderInfo;
+
+	private final Vec3 start;
+	private final Vec3 end;
+
+	private final int segments;
+
+	private int count = 1;
+	private float size = 0.1F;
+
+	private int lifespan = 30;
+
+	private SpawnFunction spawnFunction = SpawnFunction.delay(60);
+	private FadeFunction fadeFunction = FadeFunction.fade(0.5F);
+
+	public BoltEffect(Vec3 start, Vec3 end) {
+		this(BoltRenderInfo.DEFAULT, start, end, (int) (Math.sqrt(start.distanceTo(end) * 100)));
+	}
+
+	public BoltEffect(BoltRenderInfo info, Vec3 start, Vec3 end, int segments) {
+		this.renderInfo = info;
+		this.start = start;
+		this.end = end;
+		this.segments = segments;
+	}
+
+	/**
+	 * Set the amount of bolts to render for this single bolt instance.
+	 *
+	 * @param count amount of bolts to render
+	 *
+	 * @return this
+	 */
+	public BoltEffect count(int count) {
+		this.count = count;
+		return this;
+	}
+
+	/**
+	 * Set the starting size (or width) of bolt segments.
+	 *
+	 * @param size starting size of bolt segments
+	 *
+	 * @return this
+	 */
+	public BoltEffect size(float size) {
+		this.size = size;
+		return this;
+	}
+
+	/**
+	 * Define the {@link SpawnFunction} for this bolt effect.
+	 *
+	 * @param spawnFunction spawn function to use
+	 *
+	 * @return this
+	 */
+	public BoltEffect spawn(SpawnFunction spawnFunction) {
+		this.spawnFunction = spawnFunction;
+		return this;
+	}
+
+	/**
+	 * Define the {@link FadeFunction} for this bolt effect.
+	 *
+	 * @param fadeFunction fade function to use
+	 *
+	 * @return this
+	 */
+	public BoltEffect fade(FadeFunction fadeFunction) {
+		this.fadeFunction = fadeFunction;
+		return this;
+	}
+
+	/**
+	 * Define the lifespan (in ticks) of this bolt, at the end of which the bolt will expire.
+	 *
+	 * @param lifespan lifespan to use in ticks
+	 *
+	 * @return this
+	 */
+	public BoltEffect lifespan(int lifespan) {
+		this.lifespan = lifespan;
+		return this;
+	}
+
+	public int getLifespan() {
+		return lifespan;
+	}
+
+	public SpawnFunction getSpawnFunction() {
+		return spawnFunction;
+	}
+
+	public FadeFunction getFadeFunction() {
+		return fadeFunction;
+	}
+
+	public Vector4f getColor() {
+		return renderInfo.color;
+	}
+
+	public List<BoltQuads> generate() {
+		List<BoltQuads> quads = new ArrayList<>();
+		Vec3 diff = end.subtract(start);
+		float totalDistance = (float) diff.length();
+		for (int i = 0; i < count; i++) {
+			Queue<BoltInstructions> drawQueue = new LinkedList<>();
+			drawQueue.add(new BoltInstructions(start, 0, new Vec3(0, 0, 0), null, false));
+			while (!drawQueue.isEmpty()) {
+				BoltInstructions data = drawQueue.poll();
+				Vec3 perpendicularDist = data.perpendicularDist;
+				float progress = data.progress + (1F / segments) * (1 - renderInfo.parallelNoise + random.nextFloat() * renderInfo.parallelNoise * 2);
+				Vec3 segmentEnd;
+				float segmentDiffScale = renderInfo.spreadFunction.getMaxSpread(progress);
+				if (progress >= 1 && segmentDiffScale <= 0) {
+					segmentEnd = end;
+				} else {
+					float maxDiff = renderInfo.spreadFactor * segmentDiffScale * totalDistance;
+					Vec3 randVec = findRandomOrthogonalVector(diff, random);
+					double rand = renderInfo.randomFunction.getRandom(random);
+					perpendicularDist = renderInfo.segmentSpreader.getSegmentAdd(perpendicularDist, randVec, maxDiff, segmentDiffScale, progress, rand);
+					// new vector is original + current progress through segments + perpendicular change
+					segmentEnd = start.add(diff.scale(progress)).add(perpendicularDist);
+				}
+				float boltSize = size * (0.5F + (1 - progress) * 0.5F);
+				Pair<BoltQuads, QuadCache> quadData = createQuads(data.cache, data.start, segmentEnd, boltSize);
+				quads.add(quadData.getLeft());
+
+				if (progress >= 1) {
+					break; // break if we've reached the defined end point
+				} else if (!data.isBranch) {
+					// continue the bolt if this is the primary (non-branch) segment
+					drawQueue.add(new BoltInstructions(segmentEnd, progress, perpendicularDist, quadData.getRight(), false));
+				} else if (random.nextFloat() < renderInfo.branchContinuationFactor) {
+					// branch continuation
+					drawQueue.add(new BoltInstructions(segmentEnd, progress, perpendicularDist, quadData.getRight(), true));
+				}
+
+				while (random.nextFloat() < renderInfo.branchInitiationFactor * (1 - progress)) {
+					// branch initiation (probability decreases as progress increases)
+					drawQueue.add(new BoltInstructions(segmentEnd, progress, perpendicularDist, quadData.getRight(), true));
+				}
+			}
+		}
+		return quads;
+	}
+
+	private static Vec3 findRandomOrthogonalVector(Vec3 vec, Random rand) {
+		Vec3 newVec = new Vec3(-0.5 + rand.nextDouble(), -0.5 + rand.nextDouble(), -0.5 + rand.nextDouble());
+		return vec.cross(newVec).normalize();
+	}
+
+	private Pair<BoltQuads, QuadCache> createQuads(QuadCache cache, Vec3 startPos, Vec3 end, float size) {
+		Vec3 diff = end.subtract(startPos);
+		Vec3 rightAdd = diff.cross(new Vec3(0.5, 0.5, 0.5)).normalize().scale(size);
+		Vec3 backAdd = diff.cross(rightAdd).normalize().scale(size), rightAddSplit = rightAdd.scale(0.5F);
+
+		Vec3 start = cache != null ? cache.prevEnd : startPos;
+		Vec3 startRight = cache != null ? cache.prevEndRight : start.add(rightAdd);
+		Vec3 startBack = cache != null ? cache.prevEndBack : start.add(rightAddSplit).add(backAdd);
+		Vec3 endRight = end.add(rightAdd), endBack = end.add(rightAddSplit).add(backAdd);
+
+		BoltQuads quads = new BoltQuads();
+		quads.addQuad(start, end, endRight, startRight);
+		quads.addQuad(startRight, endRight, end, start);
+
+		quads.addQuad(startRight, endRight, endBack, startBack);
+		quads.addQuad(startBack, endBack, endRight, startRight);
+
+		return Pair.of(quads, new QuadCache(end, endRight, endBack));
+	}
+
+	private static class QuadCache {
+
+		private final Vec3 prevEnd, prevEndRight, prevEndBack;
+
+		private QuadCache(Vec3 prevEnd, Vec3 prevEndRight, Vec3 prevEndBack) {
+			this.prevEnd = prevEnd;
+			this.prevEndRight = prevEndRight;
+			this.prevEndBack = prevEndBack;
+		}
+	}
+
+	protected static class BoltInstructions {
+
+		private final Vec3 start;
+		private final Vec3 perpendicularDist;
+		private final QuadCache cache;
+		private final float progress;
+		private final boolean isBranch;
+
+		private BoltInstructions(Vec3 start, float progress, Vec3 perpendicularDist, QuadCache cache, boolean isBranch) {
+			this.start = start;
+			this.perpendicularDist = perpendicularDist;
+			this.progress = progress;
+			this.cache = cache;
+			this.isBranch = isBranch;
+		}
+	}
+
+	public static class BoltQuads {
+
+		private final List<Vec3> vecs = new ArrayList<>();
+
+		protected void addQuad(Vec3... quadVecs) {
+			vecs.addAll(Arrays.asList(quadVecs));
+		}
+
+		public List<Vec3> getVecs() {
+			return vecs;
+		}
+	}
+
+	/**
+	 * A SpreadFunction defines how far bolt segments can stray from the straight-line vector, based on parallel
+	 * 'progress' from start to finish.
+	 *
+	 * @author aidancbrady
+	 */
+	public interface SpreadFunction {
+
+		/** A steady linear increase in perpendicular noise. */
+		SpreadFunction LINEAR_ASCENT = progress -> progress;
+		/**
+		 * A steady linear increase in perpendicular noise, followed by a steady decrease after the halfway point.
+		 */
+		SpreadFunction LINEAR_ASCENT_DESCENT = progress -> (progress - Math.max(0, 2 * progress - 1)) / 0.5F;
+		/** Represents a unit sine wave from 0 to PI, scaled by progress. */
+		SpreadFunction SINE = progress -> (float) Math.sin(Math.PI * progress);
+
+		float getMaxSpread(float progress);
+	}
+
+	/**
+	 * A RandomFunction defines the behavior of the RNG used in various bolt generation calculations.
+	 *
+	 * @author aidancbrady
+	 */
+	public interface RandomFunction {
+
+		/** Uniform probability distribution. */
+		RandomFunction UNIFORM = Random::nextFloat;
+		/** Gaussian probability distribution. */
+		RandomFunction GAUSSIAN = rand -> (float) rand.nextGaussian();
+
+		float getRandom(Random rand);
+	}
+
+	/**
+	 * A SegmentSpreader defines how successive bolt segments are arranged in the bolt generation calculation, based on
+	 * previous state.
+	 *
+	 * @author aidancbrady
+	 */
+	public interface SegmentSpreader {
+
+		/**
+		 * Don't remember where the last segment left off, just randomly move from the straight-line vector.
+		 */
+		SegmentSpreader NO_MEMORY = (perpendicularDist, randVec, maxDiff, scale, progress, rand) -> randVec.scale(maxDiff * rand);
+
+		/**
+		 * Move from where the previous segment ended by a certain memory factor. Higher memory will restrict
+		 * perpendicular movement.
+		 */
+		static SegmentSpreader memory(float memoryFactor) {
+			return (perpendicularDist, randVec, maxDiff, spreadScale, progress, rand) -> {
+				double nextDiff = maxDiff * (1 - memoryFactor) * rand;
+				Vec3 cur = randVec.scale(nextDiff);
+				perpendicularDist = perpendicularDist.add(cur);
+				double length = perpendicularDist.length();
+				if (length > maxDiff) {
+					perpendicularDist = perpendicularDist.scale(maxDiff / length);
+				}
+				return perpendicularDist.add(cur);
+			};
+		}
+
+		Vec3 getSegmentAdd(Vec3 perpendicularDist, Vec3 randVec, float maxDiff, float scale, float progress, double rand);
+	}
+
+	/**
+	 * A bolt's spawn function defines its spawn behavior (handled by the renderer). A spawn function generates a lower
+	 * and upper bound on a spawn delay (via
+	 * getSpawnDelayBounds()), for which an intermediate value is chosen randomly from a uniform distribution
+	 * (getSpawnDelay()). Spawn functions can also be defined as
+	 * 'consecutive,' in which cases the Bolt Renderer will always begin rendering a new bolt instance when one expires.
+	 *
+	 * @author aidancbrady
+	 */
+	public interface SpawnFunction {
+
+		/** Allow for bolts to be spawned each update call without any delay. */
+		SpawnFunction NO_DELAY = rand -> Pair.of(0F, 0F);
+		/** Will re-spawn a bolt each time one expires. */
+		SpawnFunction CONSECUTIVE = new SpawnFunction() {
+			@Override
+			public Pair<Float, Float> getSpawnDelayBounds(Random rand) {
+				return Pair.of(0F, 0F);
+			}
+
+			@Override
+			public boolean isConsecutive() {
+				return true;
+			}
+		};
+
+		/** Spawn bolts with a specified constant delay. */
+		static SpawnFunction delay(float delay) {
+			return rand -> Pair.of(delay, delay);
+		}
+
+		/**
+		 * Spawns bolts with a specified delay and specified noise value, which will be randomly applied at either end
+		 * of the delay bounds.
+		 */
+		static SpawnFunction noise(float delay, float noise) {
+			return rand -> Pair.of(delay - noise, delay + noise);
+		}
+
+		Pair<Float, Float> getSpawnDelayBounds(Random rand);
+
+		default float getSpawnDelay(Random rand) {
+			Pair<Float, Float> bounds = getSpawnDelayBounds(rand);
+			return bounds.getLeft() + (bounds.getRight() - bounds.getLeft()) * rand.nextFloat();
+		}
+
+		default boolean isConsecutive() {
+			return false;
+		}
+	}
+
+	/**
+	 * A bolt's fade function allows one to define lower and upper bounds on the bolt segments rendered based on
+	 * lifespan. This allows for dynamic 'fade-in' and
+	 * 'fade-out' effects.
+	 *
+	 * @author aidancbrady
+	 */
+	public interface FadeFunction {
+
+		/** No fade; render the bolts entirely throughout their lifespan. */
+		FadeFunction NONE = (totalBolts, lifeScale) -> Pair.of(0, totalBolts);
+
+		/**
+		 * Render bolts with a segment-by-segment 'fade' in and out, with a specified fade duration (applied to start
+		 * and finish).
+		 */
+		static FadeFunction fade(float fade) {
+			return (totalBolts, lifeScale) -> {
+				int start = lifeScale > (1 - fade) ? (int) (totalBolts * (lifeScale - (1 - fade)) / fade) : 0;
+				int end = lifeScale < fade ? (int) (totalBolts * (lifeScale / fade)) : totalBolts;
+				return Pair.of(start, end);
+			};
+		}
+
+		Pair<Integer, Integer> getRenderBounds(int totalBolts, float lifeScale);
+	}
+
+	public static class BoltRenderInfo {
+
+		public static final BoltRenderInfo DEFAULT = new BoltRenderInfo();
+		public static final BoltRenderInfo ELECTRICITY = electricity();
+
+		/** How much variance is allowed in segment lengths (parallel to straight line). */
+		private float parallelNoise = 0.1F;
+		/**
+		 * How much variance is allowed perpendicular to the straight line vector. Scaled by distance and spread
+		 * function.
+		 */
+		private float spreadFactor = 0.1F;
+
+		/** The chance of creating an additional branch after a certain segment. */
+		private float branchInitiationFactor = 0.0F;
+		/** The chance of a branch continuing (post-initiation). */
+		private float branchContinuationFactor = 0.0F;
+
+		private Vector4f color = new Vector4f(0.45F, 0.45F, 0.5F, 0.8F);
+
+		private RandomFunction randomFunction = RandomFunction.GAUSSIAN;
+		private SpreadFunction spreadFunction = SpreadFunction.SINE;
+		private SegmentSpreader segmentSpreader = SegmentSpreader.NO_MEMORY;
+
+		public static BoltRenderInfo electricity() {
+			return new BoltRenderInfo().color(new Vector4f(0.54F, 0.91F, 1F, 0.8F)).noise(0.2F, 0.2F).branching(0.1F, 0.6F).spreader(SegmentSpreader.memory(0.9F));
+		}
+
+		public BoltRenderInfo noise(float parallelNoise, float spreadFactor) {
+			this.parallelNoise = parallelNoise;
+			this.spreadFactor = spreadFactor;
+			return this;
+		}
+
+		public BoltRenderInfo branching(float branchInitiationFactor, float branchContinuationFactor) {
+			this.branchInitiationFactor = branchInitiationFactor;
+			this.branchContinuationFactor = branchContinuationFactor;
+			return this;
+		}
+
+		public BoltRenderInfo spreader(SegmentSpreader segmentSpreader) {
+			this.segmentSpreader = segmentSpreader;
+			return this;
+		}
+
+		public BoltRenderInfo randomFunction(RandomFunction randomFunction) {
+			this.randomFunction = randomFunction;
+			return this;
+		}
+
+		public BoltRenderInfo spreadFunction(SpreadFunction spreadFunction) {
+			this.spreadFunction = spreadFunction;
+			return this;
+		}
+
+		public BoltRenderInfo color(Vector4f color) {
+			this.color = color;
+			return this;
+		}
+	}
+}
---
+++ src/main/java/vazkii/botania/client/fx/BoltRenderer.java
@@ -0,0 +1,179 @@
+/*
+ * This class is distributed as part of the Botania Mod.
+ * Get the Source Code in github:
+ * https://github.com/Vazkii/Botania
+ *
+ * Botania is Open Source and distributed under the
+ * Botania License: http://botaniamod.net/license.php
+ */
+package vazkii.botania.client.fx;
+/*
+ * This class is derived from the Mekanism project.
+ * See ALTERNATE_LICENSES.txt.
+ * Imported October 2021 and heavily modified.
+ */
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Matrix4f;
+
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+public class BoltRenderer {
+
+	/** Amount of times per tick we refresh. 3 implies 60 Hz. */
+	private static final float REFRESH_TIME = 3F;
+	/** We will keep track of an owner's render data for 100 ticks after there are no bolts remaining. */
+	private static final double MAX_OWNER_TRACK_TIME = 100;
+
+	private Timestamp refreshTimestamp = new Timestamp();
+
+	private final Random random = new Random();
+	private final Minecraft minecraft = Minecraft.getInstance();
+
+	private final Map<Object, BoltOwnerData> boltOwners = new Object2ObjectOpenHashMap<>();
+
+	public boolean hasBoltsToRender() {
+		synchronized (boltOwners) {
+			return boltOwners.values().stream().anyMatch(data -> !data.bolts.isEmpty());
+		}
+	}
+
+	public void render(float partialTicks, PoseStack matrixStack, MultiBufferSource bufferIn) {
+		VertexConsumer buffer = null; // todo bufferIn.getBuffer(MekanismRenderType.MEK_LIGHTNING);
+		Matrix4f matrix = matrixStack.last().pose();
+		Timestamp timestamp = new Timestamp(minecraft.level.getGameTime(), partialTicks);
+		boolean refresh = timestamp.isPassed(refreshTimestamp, (1 / REFRESH_TIME));
+		if (refresh) {
+			refreshTimestamp = timestamp;
+		}
+		synchronized (boltOwners) {
+			for (Iterator<Map.Entry<Object, BoltOwnerData>> iter = boltOwners.entrySet().iterator(); iter.hasNext();) {
+				Map.Entry<Object, BoltOwnerData> entry = iter.next();
+				BoltOwnerData data = entry.getValue();
+				// tick our bolts based on the refresh rate, removing if they're now finished
+				if (refresh) {
+					data.bolts.removeIf(bolt -> bolt.tick(timestamp));
+				}
+				if (data.bolts.isEmpty() && data.lastBolt != null && data.lastBolt.getSpawnFunction().isConsecutive()) {
+					data.addBolt(new BoltInstance(data.lastBolt, timestamp), timestamp);
+				}
+				data.bolts.forEach(bolt -> bolt.render(matrix, buffer, timestamp));
+
+				if (data.bolts.isEmpty() && timestamp.isPassed(data.lastUpdateTimestamp, MAX_OWNER_TRACK_TIME)) {
+					iter.remove();
+				}
+			}
+		}
+	}
+
+	public void update(Object owner, BoltEffect newBoltData, float partialTicks) {
+		if (minecraft.level == null) {
+			return;
+		}
+		synchronized (boltOwners) {
+			BoltOwnerData data = boltOwners.computeIfAbsent(owner, o -> new BoltOwnerData());
+			data.lastBolt = newBoltData;
+			Timestamp timestamp = new Timestamp(minecraft.level.getGameTime(), partialTicks);
+			if ((!data.lastBolt.getSpawnFunction().isConsecutive() || data.bolts.isEmpty()) && timestamp.isPassed(data.lastBoltTimestamp, data.lastBoltDelay)) {
+				data.addBolt(new BoltInstance(newBoltData, timestamp), timestamp);
+			}
+			data.lastUpdateTimestamp = timestamp;
+		}
+	}
+
+	public class BoltOwnerData {
+
+		private final Set<BoltInstance> bolts = new ObjectOpenHashSet<>();
+		private BoltEffect lastBolt;
+		private Timestamp lastBoltTimestamp = new Timestamp();
+		private Timestamp lastUpdateTimestamp = new Timestamp();
+		private double lastBoltDelay;
+
+		private void addBolt(BoltInstance instance, Timestamp timestamp) {
+			bolts.add(instance);
+			lastBoltDelay = instance.bolt.getSpawnFunction().getSpawnDelay(random);
+			lastBoltTimestamp = timestamp;
+		}
+	}
+
+	public static class BoltInstance {
+
+		private final BoltEffect bolt;
+		private final List<BoltEffect.BoltQuads> renderQuads;
+		private final Timestamp createdTimestamp;
+
+		public BoltInstance(BoltEffect bolt, Timestamp timestamp) {
+			this.bolt = bolt;
+			this.renderQuads = bolt.generate();
+			this.createdTimestamp = timestamp;
+		}
+
+		public void render(Matrix4f matrix, VertexConsumer buffer, Timestamp timestamp) {
+			float lifeScale = timestamp.subtract(createdTimestamp).value() / bolt.getLifespan();
+			Pair<Integer, Integer> bounds = bolt.getFadeFunction().getRenderBounds(renderQuads.size(), lifeScale);
+			for (int i = bounds.getLeft(); i < bounds.getRight(); i++) {
+				renderQuads.get(i).getVecs().forEach(v -> buffer.vertex(matrix, (float) v.x, (float) v.y, (float) v.z)
+						.color(bolt.getColor().x(), bolt.getColor().y(), bolt.getColor().z(), bolt.getColor().w())
+						.endVertex());
+			}
+		}
+
+		public boolean tick(Timestamp timestamp) {
+			return timestamp.isPassed(createdTimestamp, bolt.getLifespan());
+		}
+	}
+
+	public static class Timestamp {
+
+		private final long ticks;
+		private final float partial;
+
+		public Timestamp() {
+			this(0, 0);
+		}
+
+		public Timestamp(long ticks, float partial) {
+			this.ticks = ticks;
+			this.partial = partial;
+		}
+
+		public Timestamp subtract(Timestamp other) {
+			long newTicks = ticks - other.ticks;
+			float newPartial = partial - other.partial;
+			if (newPartial < 0) {
+				newPartial += 1;
+				newTicks -= 1;
+			}
+			return new Timestamp(newTicks, newPartial);
+		}
+
+		public float value() {
+			return ticks + partial;
+		}
+
+		public boolean isPassed(Timestamp prev, double duration) {
+			long ticksPassed = ticks - prev.ticks;
+			if (ticksPassed > duration) {
+				return true;
+			}
+			duration -= ticksPassed;
+			if (duration >= 1) {
+				return false;
+			}
+			return (partial - prev.partial) >= duration;
+		}
+	}
+}
