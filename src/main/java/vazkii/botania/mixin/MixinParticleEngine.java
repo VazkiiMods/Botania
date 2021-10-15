e3c636a88a79c323090b17a423a7a72a6f629220
Hubry <26120076+Hubry@users.noreply.github.com>
2021-10-15 02:25:13 +0200

    Render our particles before the custom type
    Works around the issue of item pickup particles darkening all bursts
    and other custom particles.

--- src/main/java/vazkii/botania/mixin/MixinParticleEngine.java
+++ src/main/java/vazkii/botania/mixin/MixinParticleEngine.java
@@ -37,9 +37,14 @@ public class MixinParticleEngine {
 
 	@Inject(at = @At("RETURN"), method = "<init>")
 	private void addTypes(ClientLevel world, TextureManager textureManager, CallbackInfo ci) {
-		RENDER_ORDER = ImmutableList.<ParticleRenderType>builder().addAll(RENDER_ORDER)
-				.add(FXWisp.NORMAL_RENDER, FXWisp.DIW_RENDER)
-				.add(FXSparkle.NORMAL_RENDER, FXSparkle.CORRUPT_RENDER)
-				.build();
+		var builder = ImmutableList.<ParticleRenderType>builder();
+		for (var type : RENDER_ORDER) {
+			if (type == ParticleRenderType.CUSTOM) {
+				builder.add(FXWisp.NORMAL_RENDER, FXWisp.DIW_RENDER)
+						.add(FXSparkle.NORMAL_RENDER, FXSparkle.CORRUPT_RENDER);
+			}
+			builder.add(type);
+		}
+		RENDER_ORDER = builder.build();
 	}
 }
