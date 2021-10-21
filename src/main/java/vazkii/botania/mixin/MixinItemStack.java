cc406bf61ac5b518734a85222b4426a39d44daae
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2021-10-20 20:30:58 -0500

    (hackily) fix https://github.com/williewillus/botania-fabric-issues/issues/35

---
+++ src/main/java/vazkii/botania/mixin/MixinItemStack.java
@@ -0,0 +1,36 @@
+/*
+ * This class is distributed as part of the Botania Mod.
+ * Get the Source Code in github:
+ * https://github.com/Vazkii/Botania
+ *
+ * Botania is Open Source and distributed under the
+ * Botania License: http://botaniamod.net/license.php
+ */
+package vazkii.botania.mixin;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import vazkii.botania.common.item.ModItems;
+
+@Mixin(ItemStack.class)
+public abstract class MixinItemStack {
+	@Shadow
+	public abstract boolean is(Item item);
+
+	@Inject(at = @At("HEAD"), method = "is(Lnet/minecraft/world/item/Item;)Z", cancellable = true)
+	private void isBotaniaShears(Item item, CallbackInfoReturnable<Boolean> cir) {
+		if (item == Items.SHEARS) {
+			if (is(ModItems.manasteelShears) || is(ModItems.elementiumShears)) {
+				cir.setReturnValue(true);
+			}
+		}
+	}
+}
