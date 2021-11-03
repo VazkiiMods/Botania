4dc6f22211809b49678c10bb1a817c081bd28848
Hubry <26120076+Hubry@users.noreply.github.com>
2021-11-03 13:45:00 +0100

    Remove instead of adding mana on requestMana
    See #3781

--- src/main/java/vazkii/botania/common/impl/mana/ManaItemHandlerImpl.java
+++ src/main/java/vazkii/botania/common/impl/mana/ManaItemHandlerImpl.java
@@ -10,6 +10,9 @@ package vazkii.botania.common.impl.mana;
 
 import com.google.common.collect.Iterables;
 
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
@@ -108,7 +111,7 @@ public class ManaItemHandlerImpl implements ManaItemHandler {
 		List<ItemStack> items = getManaItems(player);
 		List<ItemStack> acc = getManaAccesories(player);
 		int manaReceived = 0;
-		Map<ItemStack, Integer> manaToRemove = new HashMap<>();
+		Object2IntMap<ItemStack> manaToRemove = new Object2IntOpenHashMap<>();
 		for (ItemStack stackInSlot : Iterables.concat(items, acc)) {
 			if (stackInSlot == stack) {
 				continue;
@@ -134,8 +137,8 @@ public class ManaItemHandlerImpl implements ManaItemHandler {
 		}
 
 		if (manaReceived == manaToGet) {
-			for (Map.Entry<ItemStack, Integer> entry : manaToRemove.entrySet()) {
-				((IManaItem) entry.getKey().getItem()).addMana(entry.getKey(), entry.getValue());
+			for (Object2IntMap.Entry<ItemStack> entry : manaToRemove.object2IntEntrySet()) {
+				((IManaItem) entry.getKey().getItem()).addMana(entry.getKey(), -entry.getIntValue());
 			}
 			return true;
 		}
