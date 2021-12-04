b400a01949c2a63ac6f287fa02e3e7bfdfae8bcb
Vincent Lee <vincent.lee@outlook.com>
2021-12-03 18:33:39 -0600

    update deps

--- src/main/java/vazkii/botania/common/item/ItemModPattern.java
+++ src/main/java/vazkii/botania/common/item/ItemModPattern.java
@@ -9,18 +9,17 @@
 package vazkii.botania.common.item;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 
 import java.util.List;
 
-import io.github.fablabsmc.fablabs.api.bannerpattern.v1.LoomPattern;
-import io.github.fablabsmc.fablabs.api.bannerpattern.v1.LoomPatternItem;
-
-public class ItemModPattern extends LoomPatternItem {
+public class ItemModPattern extends Item {
+	// todo 1.18
 	public ItemModPattern(LoomPattern pattern, Properties settings) {
-		super(pattern, settings);
+		super(settings);
 	}
 
 	@Override
