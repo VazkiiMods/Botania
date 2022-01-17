f81325434d6b7cfb0516483593fb85cfe2f99bc1
Vincent Lee <vincent@vincent-lee.net>
2022-01-16 22:23:50 -0800

    Tweak comment on @SoftImplement

--- Common/src/main/java/vazkii/botania/common/annotations/SoftImplement.java
+++ Common/src/main/java/vazkii/botania/common/annotations/SoftImplement.java
@@ -9,10 +9,13 @@ import java.lang.annotation.Target;
  * A purely-documentative annotation.
  * This annotation is used by developers in xplat code. The annotated method is intended
  * to "soft implement" a certain method in a loader specific interface that cannot be
- * named in xplat code.
+ * named in xplat code and thus cannot be checked with @Override.
  * In this context, "soft implement" means to implement the method by matching the signature
  * with the intended interface method.
- * An example of interface methods that we would use this for is IForgeItem or FabricItem.
+ * Examples of interfaces that we would use this for is IForgeItem or FabricItem.
+ *
+ * The intent is that we audit such sites every major Minecraft version or so, to ensure
+ * that they still properly override the intended target.
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.METHOD)
