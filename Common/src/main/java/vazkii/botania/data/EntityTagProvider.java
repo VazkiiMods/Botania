425f53f6c60365c483a64e4588d88bd6b1b4a68a
Hubry <26120076+Hubry@users.noreply.github.com>
2022-02-07 16:09:25 +0100

    Update cocoon outputs

--- Common/src/main/java/vazkii/botania/data/EntityTagProvider.java
+++ Common/src/main/java/vazkii/botania/data/EntityTagProvider.java
@@ -30,13 +30,13 @@ public class EntityTagProvider extends EntityTypeTagsProvider {
 		tag(ModTags.Entities.COCOON_RARE)
 				.add(EntityType.HORSE, EntityType.DONKEY, EntityType.WOLF, EntityType.OCELOT,
 						EntityType.CAT, EntityType.PARROT, EntityType.LLAMA, EntityType.FOX,
-						EntityType.PANDA, EntityType.TURTLE)
+						EntityType.PANDA, EntityType.TURTLE, EntityType.GOAT)
 				.addOptional(new ResourceLocation("quark", "frog"));
 
 		tag(ModTags.Entities.COCOON_COMMON_AQUATIC)
 				.add(EntityType.COD, EntityType.SALMON, EntityType.TROPICAL_FISH, EntityType.PUFFERFISH, EntityType.SQUID)
 				.addOptional(new ResourceLocation("quark", "crab"));
-		tag(ModTags.Entities.COCOON_RARE_AQUATIC).add(EntityType.DOLPHIN, EntityType.GLOW_SQUID);
+		tag(ModTags.Entities.COCOON_RARE_AQUATIC).add(EntityType.DOLPHIN, EntityType.GLOW_SQUID, EntityType.AXOLOTL);
 
 		tag(ModTags.Entities.SHADED_MESA_BLACKLIST).add(EntityType.ENDER_DRAGON, EntityType.WITHER,
 				EntityType.ITEM_FRAME, EntityType.GLOW_ITEM_FRAME, EntityType.END_CRYSTAL, EntityType.PAINTING,
