15ecf52bb3219e27dbc7e412d19cee7afc5ed580
Vincent Lee <vincent@vincent-lee.net>
2022-02-22 22:52:08 -0800

    Make hopperhock test less flaky
    The flower is now in a fixed spot instead of the furnace, and we
    alwayys drop the item in the same place.
    This fixes flakiness where the item would sometimes get spawned
    inside a block and get bounced out.
    Additionally, use chibi flowers to reduce the template size.

--- Common/src/main/java/vazkii/botania/test/block/HopperhockTest.java
+++ Common/src/main/java/vazkii/botania/test/block/HopperhockTest.java
@@ -19,108 +19,121 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 
-import vazkii.botania.common.block.ModSubtiles;
 import vazkii.botania.test.TestingUtil;
 
 public class HopperhockTest {
 
 	private static final String TEMPLATE = "botania:block/hopperhock_furnace";
-	private static final BlockPos FURNACE_POS = new BlockPos(6, 3, 6);
+	private static final BlockPos FLOWER_POS = new BlockPos(2, 3, 2);
+	// Above the light gray glass which is at 2, 2, 1
+	private static final BlockPos SPAWN_ITEM_POS = new BlockPos(2, 3, 1);
+	private static final float SPAWN_ITEM_X = SPAWN_ITEM_POS.getX();
+	private static final float SPAWN_ITEM_Y = SPAWN_ITEM_POS.getY();
+	private static final float SPAWN_ITEM_Z = SPAWN_ITEM_POS.getZ();
 
 	@GameTest(template = TEMPLATE)
 	public void testInsertTop(GameTestHelper helper) {
-		helper.setBlock(FURNACE_POS.above(), ModSubtiles.hopperhockFloating);
-		helper.spawnItem(Items.COBBLESTONE, 1, 2, 1);
+		var furnacePos = FLOWER_POS.below();
+		helper.setBlock(furnacePos, Blocks.FURNACE);
+		spawnItem(helper, Items.COBBLESTONE);
 
 		helper.succeedWhen(() -> {
-			assertItemInSlot(helper, 0, Items.COBBLESTONE, 1);
+			assertItemInSlot(helper, furnacePos, 0, Items.COBBLESTONE, 1);
 			helper.assertEntityNotPresent(EntityType.ITEM);
 		});
 	}
 
 	@GameTest(template = TEMPLATE)
 	public void testInsertSide(GameTestHelper helper) {
-		helper.setBlock(FURNACE_POS.east(), ModSubtiles.hopperhockFloating);
-		helper.spawnItem(Items.CHARCOAL, 1, 2, 1);
+		var furnacePos = FLOWER_POS.south();
+		helper.setBlock(furnacePos, Blocks.FURNACE);
+		spawnItem(helper, Items.CHARCOAL);
 
 		helper.succeedWhen(() -> {
-			assertItemInSlot(helper, 1, Items.CHARCOAL, 1);
+			assertItemInSlot(helper, furnacePos, 1, Items.CHARCOAL, 1);
 			helper.assertEntityNotPresent(EntityType.ITEM);
 		});
 	}
 
 	@GameTest(template = TEMPLATE)
 	public void testInsertBottom(GameTestHelper helper) {
-		helper.setBlock(FURNACE_POS.below(), ModSubtiles.hopperhock);
-		helper.spawnItem(Items.CHARCOAL, 1, 2, 1);
+		var furnacePos = FLOWER_POS.above();
+		helper.setBlock(furnacePos, Blocks.FURNACE);
+		spawnItem(helper, Items.CHARCOAL);
 
 		helper.succeedWhen(() -> {
 			// Both the fuel and output slots are accessible from the bottom face, but inserting into the
 			// output slot isn't possible.
-			assertItemInSlot(helper, 1, Items.CHARCOAL, 1);
+			assertItemInSlot(helper, furnacePos, 1, Items.CHARCOAL, 1);
 			helper.assertEntityNotPresent(EntityType.ITEM);
 		});
 	}
 
 	@GameTest(template = TEMPLATE)
 	public void testInventoryFull(GameTestHelper helper) {
-		helper.setBlock(FURNACE_POS.above(), ModSubtiles.hopperhockFloating);
-		getFurnaceBlockEntity(helper).setItem(0, new ItemStack(Items.OAK_LOG, 64));
-		helper.spawnItem(Items.OAK_LOG, 1, 2, 1);
+		var furnacePos = FLOWER_POS.below();
+		helper.setBlock(furnacePos, Blocks.FURNACE);
+		getFurnaceBlockEntity(helper, furnacePos).setItem(0, new ItemStack(Items.OAK_LOG, 64));
+		spawnItem(helper, Items.OAK_LOG);
 
 		helper.startSequence().thenExecuteAfter(61, () -> {
-			assertItemInSlot(helper, 0, Items.OAK_LOG, 64);
-			helper.assertItemEntityPresent(Items.OAK_LOG, new BlockPos(1, 2, 1), 0);
+			assertItemInSlot(helper, furnacePos, 0, Items.OAK_LOG, 64);
+			helper.assertItemEntityPresent(Items.OAK_LOG, SPAWN_ITEM_POS, 0);
 		}).thenSucceed();
 	}
 
 	@GameTest(template = TEMPLATE)
 	public void testAddToStack(GameTestHelper helper) {
-		helper.setBlock(FURNACE_POS.above(), ModSubtiles.hopperhockFloating);
-		getFurnaceBlockEntity(helper).setItem(0, new ItemStack(Items.OAK_LOG, 60));
-		ItemEntity itemEntity = helper.spawnItem(Items.OAK_LOG, 1, 2, 1);
+		var furnacePos = FLOWER_POS.below();
+		helper.setBlock(furnacePos, Blocks.FURNACE);
+		getFurnaceBlockEntity(helper, furnacePos).setItem(0, new ItemStack(Items.OAK_LOG, 60));
+		ItemEntity itemEntity = spawnItem(helper, Items.OAK_LOG);
 		itemEntity.getItem().setCount(10);
 
 		helper.succeedWhen(() -> {
-			assertItemInSlot(helper, 0, Items.OAK_LOG, 64);
-			helper.assertItemEntityCountIs(Items.OAK_LOG, new BlockPos(1, 2, 1), 0, 6);
+			assertItemInSlot(helper, furnacePos, 0, Items.OAK_LOG, 64);
+			helper.assertItemEntityCountIs(Items.OAK_LOG, SPAWN_ITEM_POS, 0, 6);
 		});
 	}
 
 	@GameTest(template = TEMPLATE)
 	public void testWrongItemForSlot(GameTestHelper helper) {
-		helper.setBlock(FURNACE_POS.below(), ModSubtiles.hopperhock);
-		helper.spawnItem(Items.BRICKS, 1, 2, 1);
+		var furnacePos = FLOWER_POS.above();
+		helper.setBlock(furnacePos, Blocks.FURNACE);
+		spawnItem(helper, Items.BRICKS);
 
 		helper.startSequence().thenExecuteAfter(61, () -> {
-			helper.assertContainerEmpty(FURNACE_POS);
-			helper.assertItemEntityPresent(Items.BRICKS, new BlockPos(1, 2, 1), 0);
+			helper.assertContainerEmpty(furnacePos);
+			helper.assertItemEntityPresent(Items.BRICKS, SPAWN_ITEM_POS, 0);
 		}).thenSucceed();
 	}
 
 	@GameTest(template = TEMPLATE)
 	public void testFilterMatch(GameTestHelper helper) {
-		helper.setBlock(FURNACE_POS.above(), ModSubtiles.hopperhockFloating);
-		spawnItemFrame(helper, FURNACE_POS.west(), Direction.WEST, Items.COBBLESTONE);
-		helper.spawnItem(Items.COBBLESTONE, 1, 2, 1);
+		var furnacePos = FLOWER_POS.below();
+		helper.setBlock(furnacePos, Blocks.FURNACE);
+		spawnItemFrame(helper, furnacePos.west(), Direction.WEST, Items.COBBLESTONE);
+		spawnItem(helper, Items.COBBLESTONE);
 
 		helper.succeedWhen(() -> {
-			assertItemInSlot(helper, 0, Items.COBBLESTONE, 1);
+			assertItemInSlot(helper, furnacePos, 0, Items.COBBLESTONE, 1);
 			helper.assertEntityNotPresent(EntityType.ITEM);
 		});
 	}
 
 	@GameTest(template = TEMPLATE)
 	public void testFilterNoMatch(GameTestHelper helper) {
-		helper.setBlock(FURNACE_POS.above(), ModSubtiles.hopperhockFloating);
-		spawnItemFrame(helper, FURNACE_POS.west(), Direction.WEST, Items.OAK_LOG);
-		helper.spawnItem(Items.COBBLESTONE, 1, 2, 1);
+		var furnacePos = FLOWER_POS.below();
+		helper.setBlock(furnacePos, Blocks.FURNACE);
+		spawnItemFrame(helper, furnacePos.west(), Direction.WEST, Items.OAK_LOG);
+		spawnItem(helper, Items.COBBLESTONE);
 
 		helper.startSequence().thenExecuteAfter(61, () -> {
-			helper.assertContainerEmpty(FURNACE_POS);
-			helper.assertItemEntityPresent(Items.COBBLESTONE, new BlockPos(1, 2, 1), 0);
+			helper.assertContainerEmpty(furnacePos);
+			helper.assertItemEntityPresent(Items.COBBLESTONE, SPAWN_ITEM_POS, 0);
 		}).thenSucceed();
 	}
 
@@ -130,27 +143,31 @@ public class HopperhockTest {
 		helper.getLevel().addFreshEntity(itemFrame);
 	}
 
-	private static void assertItemInSlot(GameTestHelper helper, int slot, Item expectedItem, int expectedCount) {
-		ItemStack stack = getFurnaceBlockEntity(helper).getItem(slot);
+	private ItemEntity spawnItem(GameTestHelper helper, Item item) {
+		return helper.spawnItem(item, SPAWN_ITEM_X, SPAWN_ITEM_Y, SPAWN_ITEM_Z);
+	}
+
+	private static void assertItemInSlot(GameTestHelper helper, BlockPos furnacePos, int slot, Item expectedItem, int expectedCount) {
+		ItemStack stack = getFurnaceBlockEntity(helper, furnacePos).getItem(slot);
 		if (stack.getItem() != expectedItem) {
 			TestingUtil.throwPositionedAssertion(
 					helper,
-					FURNACE_POS,
+					furnacePos,
 					() -> "Expected " + expectedItem + " in slot " + slot + ", but got " + stack.getItem());
 		} else if (stack.getCount() != expectedCount) {
 			TestingUtil.throwPositionedAssertion(
 					helper,
-					FURNACE_POS,
+					furnacePos,
 					() -> "Expected " + expectedCount + " items in slot " + slot + ", but got " + stack.getCount());
 		}
 	}
 
-	private static AbstractFurnaceBlockEntity getFurnaceBlockEntity(GameTestHelper helper) {
-		if (helper.getBlockEntity(FURNACE_POS) instanceof AbstractFurnaceBlockEntity furnace) {
+	private static AbstractFurnaceBlockEntity getFurnaceBlockEntity(GameTestHelper helper, BlockPos furnacePos) {
+		if (helper.getBlockEntity(furnacePos) instanceof AbstractFurnaceBlockEntity furnace) {
 			return furnace;
 		} else {
 			throw new GameTestAssertPosException(
-					"Expected furnace here", helper.absolutePos(FURNACE_POS), FURNACE_POS, helper.getTick());
+					"Expected furnace here", helper.absolutePos(furnacePos), furnacePos, helper.getTick());
 		}
 	}
 }
