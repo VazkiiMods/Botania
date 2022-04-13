bd82e3d0c32fa0808eece603a1f913525af66fa0
Ramid Khan <ramidzkh@gmail.com>
2022-04-14 09:12:34 +1000

    spotless moment

--- Common/src/main/java/vazkii/botania/common/entity/EntityManaSpark.java
+++ Common/src/main/java/vazkii/botania/common/entity/EntityManaSpark.java
@@ -23,7 +23,6 @@ import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
 
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.mana.IManaItem;
