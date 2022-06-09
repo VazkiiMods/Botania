dd27371c48183d286710095a84946fac29f94261
emilyalexandra <emilyploszaj@gmail.com>
2022-06-08 19:27:11 -0500

    Remove evil yarn based reflection

--- Fabric/src/main/java/vazkii/botania/fabric/integration/emi/OrechidEmiRecipe.java
+++ Fabric/src/main/java/vazkii/botania/fabric/integration/emi/OrechidEmiRecipe.java
@@ -1,8 +1,5 @@
 package vazkii.botania.fabric.integration.emi;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.util.List;
 
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
@@ -11,19 +8,9 @@ import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.WidgetHolder;
 import net.minecraft.world.item.ItemStack;
 import vazkii.botania.api.recipe.IOrechidRecipe;
-import vazkii.botania.api.recipe.StateIngredient;
 
 public class OrechidEmiRecipe extends BotaniaEmiRecipe {
 	private final EmiIngredient orechid;
-	private static MethodHandle handle;
-
-	static {
-		try {
-			handle = MethodHandles.lookup().findVirtual(IOrechidRecipe.class, "getOutput", MethodType.methodType(StateIngredient.class));
-		} catch (Throwable e) {
-			throw new RuntimeException(e);
-		}
-	}
 
 	public OrechidEmiRecipe(EmiRecipeCategory category, IOrechidRecipe recipe, int totalWeight, EmiIngredient orechid) {
 		super(category, recipe);
@@ -31,7 +18,7 @@ public class OrechidEmiRecipe extends BotaniaEmiRecipe {
 		final int amount = Math.max(1, Math.round((float) (weight * 64) / totalWeight));
 		this.input = List.of(EmiStack.of(new ItemStack(recipe.getInput(), 64)));
 		try {
-			this.output = ((StateIngredient) handle.invoke(recipe)).getDisplayed().stream()
+			this.output = recipe.getOutput().getDisplayed().stream()
 				.map(s -> EmiStack.of(new ItemStack(s.getBlock(), amount))).toList();
 		} catch (Throwable e) {
 			throw new RuntimeException(e);
