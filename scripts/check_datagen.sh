d98b4c942885cb3fdaab5d74304f03b4ceba76de
Vincent Lee <vincent@vincent-lee.net>
2022-08-18 19:41:12 -0700

    Make scripts/check_datagen resilient to new 1.19 datagen changes

--- scripts/check_datagen.sh
+++ scripts/check_datagen.sh
@@ -3,12 +3,15 @@ set -e
 
 ./gradlew :Fabric:runXplatDatagen :Forge:runData :Fabric:runFabricDatagen || exit 1
 
-STATUS="$(git status --porcelain)"
+# Only check status of the assets and data folders,
+# because as of 1.19 rerunning datagen will
+# always update the timestamp in the .cache files (Mojang why)
+STATUS="$(git status --porcelain Xplat/src/generated/resources/* Fabric/src/generated/resources/* Forge/src/generated/resources/*)"
 if [ -z "$STATUS" ]
 then
   echo "Datagen ok"
 else
-  echo "The repository is dirty after running data generators. Please make sure you committed generated files. Dirty files:"
+  echo "Generated resources are dirty after running data generators. Please make sure you committed generated files. Dirty files:"
   echo "$STATUS"
   exit 1
 fi
