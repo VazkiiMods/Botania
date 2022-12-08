654c3395e15942f5b7d031ba5210ee3e6085fdc3
Vincent Lee <vincent@vincent-lee.net>
2022-12-07 23:03:21 -0800

    Delete old Forge recipe serializer hack
    IForgeRegistryEntry is crob so this can go too

--- Xplat/src/main/java/vazkii/botania/common/crafting/RecipeSerializerBase.java
+++ Xplat/src/main/java/vazkii/botania/common/crafting/RecipeSerializerBase.java
@@ -1,38 +1,9 @@
 package vazkii.botania.common.crafting;
 
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 
-import org.jetbrains.annotations.Nullable;
-
-import vazkii.botania.common.annotations.SoftImplement;
-
-/**
- * This class exists purely to (soft) implement the IForgeRegistryEntry methods.
- * The actual bridge method for setRegistryName on the Forge side is implemented with a mixin.
- */
+// TODO remove this and fix use sites to implement directly
 public abstract class RecipeSerializerBase<T extends Recipe<?>> implements RecipeSerializer<T> {
-	@Nullable
-	private ResourceLocation registryName;
-
-	@SoftImplement("IForgeRegistryEntry")
-	public RecipeSerializerBase<T> setRegistryName(ResourceLocation name) {
-		registryName = name;
-		return this;
-	}
-
-	@SoftImplement("IForgeRegistryEntry")
-	@Nullable
-	public ResourceLocation getRegistryName() {
-		return registryName;
-	}
-
-	@SoftImplement("IForgeRegistryEntry")
-	@SuppressWarnings("unchecked")
-	public Class<RecipeSerializer<?>> getRegistryType() {
-		Class<?> clazz = RecipeSerializer.class;
-		return (Class<RecipeSerializer<?>>) clazz;
-	}
 
 }
