65c6eefd7a11d762c860f4bf24ef6ad41fda2331
00gogo00 <zachgalaxy@comcast.net>
2023-05-27 00:33:49 -0600

    autoformatter

--- Xplat/src/main/java/vazkii/botania/test/item/GrassSeedDispensingTest.java
+++ Xplat/src/main/java/vazkii/botania/test/item/GrassSeedDispensingTest.java
@@ -6,14 +6,14 @@ import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.level.block.Blocks;
 
 public class GrassSeedDispensingTest {
-    @GameTest(template = "botania:item/grass_seed_dispensing")
-    public void TestGrassSeedDispensing(GameTestHelper helper) {
-        BlockPos dispenserPos = new BlockPos(4,2,4);
-        helper.assertBlockPresent(Blocks.DISPENSER, dispenserPos);
+	@GameTest(template = "botania:item/grass_seed_dispensing")
+	public void TestGrassSeedDispensing(GameTestHelper helper) {
+		BlockPos dispenserPos = new BlockPos(4, 2, 4);
+		helper.assertBlockPresent(Blocks.DISPENSER, dispenserPos);
 
-        helper.setBlock(dispenserPos.east(), Blocks.REDSTONE_BLOCK);
+		helper.setBlock(dispenserPos.east(), Blocks.REDSTONE_BLOCK);
 
-        helper.succeedWhen(() -> helper.assertBlockPresent(Blocks.GRASS_BLOCK, dispenserPos.west()));
-    }
-    
+		helper.succeedWhen(() -> helper.assertBlockPresent(Blocks.GRASS_BLOCK, dispenserPos.west()));
+	}
+
 }
