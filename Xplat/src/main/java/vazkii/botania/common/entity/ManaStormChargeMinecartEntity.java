15776d4ca7a34a614af13b33288b591cf8c75da8
00gogo00 <zachgalaxy@comcast.net>
2023-05-29 23:03:10 -0600

    run autoformatter

--- Xplat/src/main/java/vazkii/botania/common/entity/ManaStormChargeMinecartEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/entity/ManaStormChargeMinecartEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+
 import vazkii.botania.api.entity.ManaSensitive;
 import vazkii.botania.api.internal.ManaBurst;
 import vazkii.botania.common.block.BotaniaBlocks;
@@ -13,45 +14,45 @@ import vazkii.botania.common.item.BotaniaItems;
 
 public class ManaStormChargeMinecartEntity extends AbstractMinecart implements ManaSensitive {
 
-    public ManaStormChargeMinecartEntity(EntityType<ManaStormChargeMinecartEntity> type, Level world) {
+	public ManaStormChargeMinecartEntity(EntityType<ManaStormChargeMinecartEntity> type, Level world) {
 		super(type, world);
 	}
 
-    public ManaStormChargeMinecartEntity(Level world, double x, double y, double z) {
+	public ManaStormChargeMinecartEntity(Level world, double x, double y, double z) {
 		super(BotaniaEntities.CHARGE_MINECART, world, x, y, z);
 	}
 
-    @Override
-    protected Item getDropItem() {
-        return BotaniaItems.chargeMinecart;
-    }
+	@Override
+	protected Item getDropItem() {
+		return BotaniaItems.chargeMinecart;
+	}
 
-    @Override
-    public Type getMinecartType() {
-        return Type.RIDEABLE;
-    }
+	@Override
+	public Type getMinecartType() {
+		return Type.RIDEABLE;
+	}
 
-    @Override
+	@Override
 	protected boolean canAddPassenger(Entity passenger) {
 		return false;
 	}
 
-    @Override
-    public BlockState getDefaultDisplayBlockState() {
-        return BotaniaBlocks.manaBomb.defaultBlockState();
-    }
+	@Override
+	public BlockState getDefaultDisplayBlockState() {
+		return BotaniaBlocks.manaBomb.defaultBlockState();
+	}
 
-    @Override
-    public boolean onBurstCollision(ManaBurst burst) {
-        Level world = this.getLevel();
-        if (!world.isClientSide()) {
-            ManaStormEntity storm = BotaniaEntities.MANA_STORM.create(world);
-		    storm.burstColor = burst.getColor();
-		    storm.setPos(this.position().add(0, 0.4375, 0)); //Since carts sit 0.625 off the ground
-		    world.addFreshEntity(storm);
-            this.discard();
-        }
-        return true;
-    }
-    
+	@Override
+	public boolean onBurstCollision(ManaBurst burst) {
+		Level world = this.getLevel();
+		if (!world.isClientSide()) {
+			ManaStormEntity storm = BotaniaEntities.MANA_STORM.create(world);
+			storm.burstColor = burst.getColor();
+			storm.setPos(this.position().add(0, 0.4375, 0)); //Since carts sit 0.625 off the ground
+			world.addFreshEntity(storm);
+			this.discard();
+		}
+		return true;
+	}
+
 }
