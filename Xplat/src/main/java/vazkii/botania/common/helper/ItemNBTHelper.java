6612ce3618c70ec71ad8ddde6bd75f863a037357
NEstoll <70414895+NEstoll@users.noreply.github.com>
2023-07-16 00:11:25 -0600

    Update ItemNBTHelper.java
    ItemStack.matches() will return false if the two item stacks don't have the same number of items, which breaks corporea blocks. ItemStack.isSameItemSameTags() will check the tags only.

--- Xplat/src/main/java/vazkii/botania/common/helper/ItemNBTHelper.java
+++ Xplat/src/main/java/vazkii/botania/common/helper/ItemNBTHelper.java
@@ -193,7 +193,7 @@ public final class ItemNBTHelper {
 				return ItemStack.matches(duplicateAndClearMana(stack1), duplicateAndClearMana(stack2));
 			}
 		}
-		return ItemStack.matches(stack1, stack2);
+		return ItemStack.isSameItemSameTags(stack1, stack2);
 	}
 
 	/**
