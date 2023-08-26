fcdf53ea72c3d3b9ee88470368dbb1f1990a9b8e
Vincent Lee <vincent@vincent-lee.net>
2023-08-25 22:19:37 -0700

    Spotless

--- Xplat/src/main/java/vazkii/botania/common/block/flower/functional/FallenKanadeBlockEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/block/flower/functional/FallenKanadeBlockEntity.java
@@ -15,7 +15,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 
