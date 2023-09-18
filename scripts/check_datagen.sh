1a7915d562f24b3f5b357e562c7e84a69f48a212
Vincent Lee <vincent@vincent-lee.net>
2023-09-18 09:27:03 -0700

    Update hash file for previous step, fix check_datagen.sh to catch it

--- scripts/check_datagen.sh
+++ scripts/check_datagen.sh
@@ -3,10 +3,7 @@ set -e
 
 ./gradlew :Fabric:runXplatDatagen :Forge:runData :Fabric:runFabricDatagen || exit 1
 
-# Only check status of the assets and data folders,
-# because as of 1.19 rerunning datagen will
-# always update the timestamp in the .cache files (Mojang why)
-STATUS="$(git status --porcelain Xplat/src/generated/resources/* Fabric/src/generated/resources/* Forge/src/generated/resources/*)"
+STATUS="$(git status --porcelain Xplat/src/generated/resources Fabric/src/generated/resources Forge/src/generated/resources)"
 if [ -z "$STATUS" ]
 then
   echo "Datagen ok"
