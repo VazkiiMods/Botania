b80a5b124081f7e8f5882255b0e3bd86d8430f73
TheRealWormbo <github@wormbo.de>
2023-09-27 20:19:45 +0200

    Pollidisiac feeds flowers to brown mooshrooms
    Also includes smaller optimizations to the finding and feeding logic:
    * don't look for anything if out of mana
    * exclude items from the search that can't be interacted with
    * don't look for animals when no items are around
    * exclude animals from the search that would not be fed anyway because they are a baby, or already in the mood and not a brown mooshroom

--- Xplat/src/main/java/vazkii/botania/common/block/flower/functional/PollidisiacBlockEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/block/flower/functional/PollidisiacBlockEntity.java
@@ -9,9 +9,14 @@
 package vazkii.botania.common.block.flower.functional;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.EntityEvent;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.MushroomCow;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.SuspiciousEffectHolder;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 
@@ -20,8 +25,11 @@ import vazkii.botania.api.block_entity.RadiusDescriptor;
 import vazkii.botania.common.block.BotaniaFlowerBlocks;
 import vazkii.botania.common.helper.DelayHelper;
 import vazkii.botania.common.helper.EntityHelper;
+import vazkii.botania.mixin.MushroomCowAccessor;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class PollidisiacBlockEntity extends FunctionalFlowerBlockEntity {
 	private static final int RANGE = 6;
@@ -35,35 +43,74 @@ public class PollidisiacBlockEntity extends FunctionalFlowerBlockEntity {
 	public void tickFlower() {
 		super.tickFlower();
 
-		if (!getLevel().isClientSide) {
-			var pickupBounds = new AABB(getBlockPos().offset(-RANGE, -RANGE, -RANGE), getBlockPos().offset(RANGE + 1, RANGE + 1, RANGE + 1));
-			List<ItemEntity> items = getLevel().getEntitiesOfClass(ItemEntity.class, pickupBounds);
-			var bounds = new AABB(getEffectivePos().offset(-RANGE, -RANGE, -RANGE), getEffectivePos().offset(RANGE + 1, RANGE + 1, RANGE + 1));
-			List<Animal> animals = getLevel().getEntitiesOfClass(Animal.class, bounds);
+		if (!getLevel().isClientSide && getMana() >= MANA_COST) {
+			var pickupBounds = new AABB(getBlockPos()).inflate(RANGE);
+			List<ItemEntity> items = getLevel().getEntitiesOfClass(ItemEntity.class, pickupBounds,
+					itemEntity -> DelayHelper.canInteractWith(this, itemEntity));
+			if (items.isEmpty()) {
+				return;
+			}
+			var bounds = new AABB(getEffectivePos()).inflate(RANGE);
+			List<Animal> animals = getLevel().getEntitiesOfClass(Animal.class, bounds, Predicate.not(Animal::isBaby));
+			Collections.shuffle(animals);
 
 			for (Animal animal : animals) {
-				if (getMana() < MANA_COST) {
-					break;
-				}
-
-				if (animal.getAge() == 0 && !animal.isInLove()) {
+				// Note: Empty item stacks are implicitly excluded in Animal::isFood and ItemStack::is(TagKey)
+				if (!animal.isInLove()) {
 					for (ItemEntity item : items) {
-						if (!DelayHelper.canInteractWith(this, item)) {
+						if (!animal.isFood(item.getItem())) {
 							continue;
 						}
+						consumeFoodItem(item);
 
-						if (animal.isFood(item.getItem())) {
-							EntityHelper.shrinkItem(item);
+						animal.setInLoveTime(1200);
+						getLevel().broadcastEntityEvent(animal, EntityEvent.IN_LOVE_HEARTS);
+						break;
+					}
 
-							addMana(-MANA_COST);
-							animal.setInLoveTime(1200);
-							getLevel().broadcastEntityEvent(animal, EntityEvent.IN_LOVE_HEARTS);
-							break;
+					if (getMana() < MANA_COST) {
+						break;
+					}
+				}
+
+				if (isBrownMooshroomWithoutEffect(animal)) {
+					for (ItemEntity item : items) {
+						ItemStack stack = item.getItem();
+						if (!stack.is(ItemTags.SMALL_FLOWERS)) {
+							continue;
 						}
+						var effect = SuspiciousEffectHolder.tryGet(stack.getItem());
+						if (effect == null) {
+							continue;
+						}
+						consumeFoodItem(item);
+
+						MushroomCowAccessor cowAccessor = (MushroomCowAccessor) animal;
+						cowAccessor.setEffect(effect.getSuspiciousEffect());
+						cowAccessor.setEffectDuration(effect.getEffectDuration());
+						animal.playSound(SoundEvents.MOOSHROOM_EAT, 2.0F, 1.0F);
+						break;
+					}
+
+					if (getMana() < MANA_COST) {
+						break;
 					}
 				}
 			}
 		}
+	}
+
+	private void consumeFoodItem(ItemEntity itemEntity) {
+		EntityHelper.shrinkItem(itemEntity);
+		addMana(-MANA_COST);
+	}
+
+	private static boolean isBrownMooshroomWithoutEffect(Animal animal) {
+		if (animal instanceof MushroomCow mushroomCow && mushroomCow.getVariant() == MushroomCow.MushroomType.BROWN) {
+			MushroomCowAccessor cowAccessor = (MushroomCowAccessor) animal;
+			return cowAccessor.getEffect() == null;
+		}
+		return false;
 	}
 
 	@Override
