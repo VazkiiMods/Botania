bcc32e44afab7b31a8e81c97e001f5231502a7fc
TheRealWormbo <github@wormbo.de>
2024-03-23 17:56:35 +0100

    The Spectator also scans donkeys, mules, llamas, and allays

--- Xplat/src/main/java/vazkii/botania/mixin/AbstractHorseAccessor.java
+++ Xplat/src/main/java/vazkii/botania/mixin/AbstractHorseAccessor.java
@@ -13,9 +13,13 @@ import net.minecraft.world.entity.animal.horse.AbstractHorse;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(AbstractHorse.class)
 public interface AbstractHorseAccessor {
 	@Accessor
 	SimpleContainer getInventory();
+
+	@Invoker("createInventory")
+	void botania_createInventory();
 }
