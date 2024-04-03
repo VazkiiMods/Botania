6068b4ffc5b973810a0397c4a0f8ede8176ffb7e
TheRealWormbo <github@wormbo.de>
2024-04-03 21:33:26 +0200

    Chicken egg timer reduction via Drum of the Gathering
    - if more than 30 seconds remaining to next egg, the time is reduced by 50% (but not below 30 seconds)
    - if less than 10 seconds remaining, chicken gets startled and lays its egg prematurely
    - nothing happens between 10 and 30 seconds remaining

--- Xplat/src/main/java/vazkii/botania/mixin/LivingEntityAccessor.java
+++ Xplat/src/main/java/vazkii/botania/mixin/LivingEntityAccessor.java
@@ -8,13 +8,18 @@
  */
 package vazkii.botania.mixin;
 
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(LivingEntity.class)
 public interface LivingEntityAccessor {
 	@Accessor
 	void setUseItemRemaining(int ticks);
+
+	@Invoker("playHurtSound")
+	void botania_playHurtSound(DamageSource damageSource);
 }
