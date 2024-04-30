fee00a8b3602a0f1759964735614baad6d6de863
TheRealWormbo <github@wormbo.de>
2024-04-30 14:44:18 +0200

    Move GoG extra seed loot injection to existing loot handler
    (fixes #4520, #4573, and hopefully also #4491)

--- Xplat/src/main/java/vazkii/botania/mixin/LootTableMixin.java
+++ Xplat/src/main/java/vazkii/botania/mixin/LootTableMixin.java
@@ -8,7 +8,6 @@
  */
 package vazkii.botania.mixin;
 
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -16,34 +15,15 @@ import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.common.item.equipment.tool.elementium.ElementiumPickaxeItem;
-import vazkii.botania.xplat.XplatAbstractions;
 
 import java.util.function.Consumer;
 
 @Mixin(LootTable.class)
 public class LootTableMixin {
-	private static final ResourceLocation GOG_SEEDS = new ResourceLocation(BotaniaAPI.GOG_MODID, "extra_seeds");
-
-	@Unique
-	private boolean callingGogTable;
-
-	@Inject(at = @At("RETURN"), method = "getRandomItemsRaw(Lnet/minecraft/world/level/storage/loot/LootContext;Ljava/util/function/Consumer;)V")
-	private void addGogSeeds(LootContext context, Consumer<ItemStack> stacksOut, CallbackInfo ci) {
-		if (XplatAbstractions.INSTANCE.gogLoaded() && !callingGogTable) {
-			callingGogTable = true;
-			context.getResolver().getLootTable(GOG_SEEDS).getRandomItems(context, stacksOut);
-			callingGogTable = false;
-		}
-	}
-
 	@ModifyVariable(method = "getRandomItemsRaw(Lnet/minecraft/world/level/storage/loot/LootContext;Ljava/util/function/Consumer;)V", at = @At("HEAD"), argsOnly = true)
 	private Consumer<ItemStack> filterDisposables(Consumer<ItemStack> inner, LootContext context) {
 		return stack -> {
