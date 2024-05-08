5875ed08e5fa55fc314bba3ba39c2b550ce5166d
TheRealWormbo <github@wormbo.de>
2024-05-08 16:32:18 +0200

    Datagen for Loonium loot tables

--- Fabric/src/main/java/vazkii/botania/fabric/data/FabricDatagenInitializer.java
+++ Fabric/src/main/java/vazkii/botania/fabric/data/FabricDatagenInitializer.java
@@ -41,6 +41,7 @@ public class FabricDatagenInitializer implements DataGeneratorEntrypoint {
 
 	private static void configureXplatDatagen(FabricDataGenerator.Pack pack) {
 		pack.addProvider((PackOutput output) -> new BlockLootProvider(output));
+		pack.addProvider((PackOutput output) -> new LooniumStructureLootProvider(output));
 		BlockTagProvider blockTagProvider = pack.addProvider(BlockTagProvider::new);
 		pack.addProvider((output, registriesFuture) -> new ItemTagProvider(output, registriesFuture, blockTagProvider.contentsGetter()));
 		pack.addProvider(EntityTagProvider::new);
