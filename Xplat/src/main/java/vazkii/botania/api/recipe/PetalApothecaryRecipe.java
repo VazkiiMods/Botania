16df0dccd33c51629923b925ab6f3eb5b60f8014
TheRealWormbo <github@wormbo.de>
2024-06-13 20:12:21 +0200

    Lexicon shows reagents for apothecary/altar recipes

--- Xplat/src/main/java/vazkii/botania/api/recipe/PetalApothecaryRecipe.java
+++ Xplat/src/main/java/vazkii/botania/api/recipe/PetalApothecaryRecipe.java
@@ -10,37 +10,18 @@ package vazkii.botania.api.recipe;
 
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.Container;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeType;
 
 import org.jetbrains.annotations.NotNull;
 
 import vazkii.botania.api.BotaniaAPI;
 
-public interface PetalApothecaryRecipe extends Recipe<Container> {
+public interface PetalApothecaryRecipe extends RecipeWithReagent {
 	ResourceLocation TYPE_ID = new ResourceLocation(BotaniaAPI.MODID, "petal_apothecary");
-
-	/**
-	 * @return Ingredient matching the final item that needs to be thrown into the apothecary
-	 *         to perform a craft after a matching recipe is in.
-	 */
-	Ingredient getReagent();
 
 	@NotNull
 	@Override
 	default RecipeType<?> getType() {
 		return BuiltInRegistries.RECIPE_TYPE.get(TYPE_ID);
-	}
-
-	@Override
-	default boolean canCraftInDimensions(int width, int height) {
-		return false;
-	}
-
-	@Override
-	default boolean isSpecial() {
-		return true;
 	}
 }
--- Xplat/src/main/java/vazkii/botania/client/patchouli/processor/PetalApothecaryProcessor.java
+++ Xplat/src/main/java/vazkii/botania/client/patchouli/processor/PetalApothecaryProcessor.java
@@ -9,34 +9,16 @@
 package vazkii.botania.client.patchouli.processor;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.level.Level;
 
 import vazkii.botania.client.patchouli.PatchouliUtils;
 import vazkii.botania.common.crafting.BotaniaRecipeTypes;
-import vazkii.patchouli.api.IComponentProcessor;
-import vazkii.patchouli.api.IVariable;
 import vazkii.patchouli.api.IVariableProvider;
 
-public class PetalApothecaryProcessor implements IComponentProcessor {
-	protected Recipe<?> recipe;
-
+public class PetalApothecaryProcessor extends ReagentRecipeProcessor {
 	@Override
 	public void setup(Level level, IVariableProvider variables) {
 		ResourceLocation id = new ResourceLocation(variables.get("recipe").asString());
 		this.recipe = PatchouliUtils.getRecipe(level, BotaniaRecipeTypes.PETAL_TYPE, id);
-	}
-
-	@Override
-	public IVariable process(Level level, String key) {
-		if (recipe == null) {
-			return null;
-		}
-		return switch (key) {
-			case "recipe" -> IVariable.wrap(recipe.getId().toString());
-			case "output" -> IVariable.from(recipe.getResultItem(level.registryAccess()));
-			case "heading" -> IVariable.from(recipe.getResultItem(level.registryAccess()).getHoverName());
-			default -> null;
-		};
 	}
 }
