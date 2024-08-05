225ec1a334eeedb7eea1a530780c823bb67a8b5e
Artemis System <theArtemisSystem@gmail.com>
2024-08-05 18:42:52 +0200

    oh hi spotless

--- Xplat/src/main/java/vazkii/botania/mixin/ExperienceOrbAccessor.java
+++ Xplat/src/main/java/vazkii/botania/mixin/ExperienceOrbAccessor.java
@@ -1,14 +1,15 @@
 package vazkii.botania.mixin;
 
 import net.minecraft.world.entity.ExperienceOrb;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ExperienceOrb.class)
 public interface ExperienceOrbAccessor {
-    @Accessor("count")
-    int botania_getCount();
+	@Accessor("count")
+	int botania_getCount();
 
-    @Accessor("count")
-    void botania_setCount(int c);
+	@Accessor("count")
+	void botania_setCount(int c);
 }
