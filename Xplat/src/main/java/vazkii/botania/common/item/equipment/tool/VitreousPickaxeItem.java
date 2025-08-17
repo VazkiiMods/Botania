d7a6c9cfd494b65936366cbacd4a8899aa2a4445
TheRealWormbo <github@wormbo.de>
2025-08-17 13:21:38 +0200

    Run spotless apply

--- Xplat/src/main/java/vazkii/botania/common/item/equipment/tool/VitreousPickaxeItem.java
+++ Xplat/src/main/java/vazkii/botania/common/item/equipment/tool/VitreousPickaxeItem.java
@@ -27,7 +27,6 @@ import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 
 import vazkii.botania.common.component.BotaniaDataComponents;
