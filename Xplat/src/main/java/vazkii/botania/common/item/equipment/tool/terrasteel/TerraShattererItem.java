1f9e3b889b093737dca8dc7f61536cb418a038c3
TheRealWormbo <github@wormbo.de>
2025-08-20 18:29:53 +0200

    Spotless apply, again

--- Xplat/src/main/java/vazkii/botania/common/item/equipment/tool/terrasteel/TerraShattererItem.java
+++ Xplat/src/main/java/vazkii/botania/common/item/equipment/tool/terrasteel/TerraShattererItem.java
@@ -25,7 +25,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.*;
-import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
