fbbc0381429608cb9552c6fea18c0338407c7940
TheRealWormbo <github@wormbo.de>
2025-08-31 09:43:43 +0200

    Spotless Apply

--- Xplat/src/main/java/vazkii/botania/api/configdata/LooniumMobAttributeModifier.java
+++ Xplat/src/main/java/vazkii/botania/api/configdata/LooniumMobAttributeModifier.java
@@ -11,12 +11,12 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 public record LooniumMobAttributeModifier(Holder<Attribute> attribute, double amount, AttributeModifier.Operation operation) {
+
 	public static final Codec<LooniumMobAttributeModifier> CODEC = RecordCodecBuilder.create(
 			instance -> instance.group(
 					BuiltInRegistries.ATTRIBUTE.holderByNameCodec().fieldOf("attribute")
