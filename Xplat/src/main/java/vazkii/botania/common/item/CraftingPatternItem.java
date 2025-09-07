6f10ae2d2881bc6eb2d06e6071f0cff34ed7ce08
TheRealWormbo <github@wormbo.de>
2025-09-07 21:58:27 +0200

    Allow using crafting patterns on crafter to set open slots

--- Xplat/src/main/java/vazkii/botania/common/item/CraftingPatternItem.java
+++ Xplat/src/main/java/vazkii/botania/common/item/CraftingPatternItem.java
@@ -9,11 +9,15 @@
 package vazkii.botania.common.item;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.CrafterBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 
 import vazkii.botania.api.state.BotaniaStateProperties;
 import vazkii.botania.api.state.enums.CraftyCratePattern;
@@ -44,8 +48,48 @@ public class CraftingPatternItem extends Item {
 				}
 				return InteractionResult.sidedSuccess(world.isClientSide());
 			}
+		} else if (world.getBlockEntity(pos) instanceof CrafterBlockEntity crafter) {
+			if (!world.isClientSide) {
+				float clickPitch;
+				if (canApplyConfiguration(pattern, crafter)) {
+					if (isConfigurationDifferent(pattern, crafter)) {
+						for (int slot = 0; slot < CrafterBlockEntity.CONTAINER_SIZE; slot++) {
+							crafter.setSlotState(slot, pattern.openSlots.get(slot));
+						}
+						clickPitch = 0.75f;
+					} else {
+						clickPitch = 1f;
+					}
+				} else {
+					clickPitch = 1.2f;
+				}
+				// TODO: should probably define sound events for this
+				world.playSound(null, pos, SoundEvents.UI_BUTTON_CLICK.value(), SoundSource.BLOCKS, 0.5f, clickPitch);
+				world.gameEvent(ctx.getPlayer(), GameEvent.BLOCK_ACTIVATE, pos);
+			}
+			return InteractionResult.sidedSuccess(world.isClientSide());
 		}
-
 		return InteractionResult.PASS;
+	}
+
+	private boolean canApplyConfiguration(CraftyCratePattern pattern, CrafterBlockEntity crafter) {
+		if (pattern.openSlots.size() != CrafterBlockEntity.CONTAINER_SIZE) {
+			return false;
+		}
+		for (int slot = 0; slot < CrafterBlockEntity.CONTAINER_SIZE; slot++) {
+			if (!pattern.openSlots.get(slot) && !crafter.getItem(slot).isEmpty()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private boolean isConfigurationDifferent(CraftyCratePattern pattern, CrafterBlockEntity crafter) {
+		for (int slot = 0; slot < CrafterBlockEntity.CONTAINER_SIZE; slot++) {
+			if (pattern.openSlots.get(slot) == crafter.isSlotDisabled(slot)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
