33045f96813dcc9597d79f70870339b010d107d7
TheRealWormbo <github@wormbo.de>
2025-09-18 15:44:50 +0200

    Spotless apply

--- Xplat/src/main/java/vazkii/botania/common/entity/ThrownItemEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/entity/ThrownItemEntity.java
@@ -8,17 +8,12 @@
  */
 package vazkii.botania.common.entity;
 
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 
