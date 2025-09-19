587e898b429893ac0041a3bc2dc0169779def2db
TheRealWormbo <github@wormbo.de>
2025-09-19 20:13:43 +0200

    Document mana pool cauldron interactions
    - mention washing off pool color
    - add page about crafting manasteel nuggets and blocks after the infusion recipe and somewhat reorder pages to have the layout make more sense
    - extend multi-crafting page template processor to allow for an optional second recipe display via the new `botania:crafting_multi_double template`
    - remove mention of manasteel blocks/nuggets on the page that already covers dyeing

--- Xplat/src/main/java/vazkii/botania/client/patchouli/processor/MultiCraftingProcessor.java
+++ Xplat/src/main/java/vazkii/botania/client/patchouli/processor/MultiCraftingProcessor.java
@@ -16,6 +16,8 @@ import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.crafting.ShapedRecipe;
 import net.minecraft.world.level.Level;
 
+import org.jetbrains.annotations.Nullable;
+
 import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.client.patchouli.PatchouliUtils;
 import vazkii.patchouli.api.IComponentProcessor;
@@ -27,15 +29,37 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MultiCraftingProcessor implements IComponentProcessor {
+	@SuppressWarnings("NotNullFieldNotInitialized")
 	private List<CraftingRecipe> recipes;
+	@Nullable
+	private List<CraftingRecipe> recipes2;
 	private boolean shapeless = true;
+	private boolean shapeless2 = true;
 	private int longestIngredientSize = 0;
+	private int longestIngredientSize2 = 0;
 	private boolean hasCustomHeading;
 
 	@Override
 	public void setup(Level level, IVariableProvider variables) {
-		List<String> names = variables.get("recipes", level.registryAccess()).asStream(level.registryAccess()).map(IVariable::asString).collect(Collectors.toList());
-		this.recipes = new ArrayList<>();
+		RecipeData result = getRecipeData(level, variables, "recipes");
+		this.recipes = result.recipes();
+		this.shapeless = result.shapeless();
+		this.longestIngredientSize = result.longestIngredientSize();
+
+		if (variables.has("recipes2")) {
+			RecipeData result2 = getRecipeData(level, variables, "recipes2");
+			this.recipes2 = result2.recipes();
+			this.shapeless2 = result2.shapeless();
+			this.longestIngredientSize2 = result2.longestIngredientSize();
+		}
+		this.hasCustomHeading = variables.has("heading");
+	}
+
+	private static RecipeData getRecipeData(Level level, IVariableProvider variables, String recipesVariable) {
+		List<String> names = variables.get(recipesVariable, level.registryAccess()).asStream(level.registryAccess()).map(IVariable::asString).toList();
+		var recipes = new ArrayList<CraftingRecipe>();
+		boolean shapeless = true;
+		int longestIngredientSize = 0;
 		for (String name : names) {
 			CraftingRecipe recipe = PatchouliUtils.getRecipe(level, RecipeType.CRAFTING, ResourceLocation.parse(name));
 			if (recipe != null) {
@@ -50,29 +74,47 @@ public class MultiCraftingProcessor implements IComponentProcessor {
 					}
 				}
 			} else {
-				BotaniaAPI.LOGGER.warn("Missing crafting recipe " + name);
+				BotaniaAPI.LOGGER.warn("Missing crafting recipe {}", name);
 			}
 		}
-		this.hasCustomHeading = variables.has("heading");
+		return new RecipeData(recipes, shapeless, longestIngredientSize);
 	}
 
+	private record RecipeData(ArrayList<CraftingRecipe> recipes, boolean shapeless, int longestIngredientSize) {
+	}
+
+	@SuppressWarnings("NullableProblems")
+	@Nullable
 	@Override
 	public IVariable process(Level level, String key) {
-		if (recipes.isEmpty()) {
-			return null;
-		}
 		if (key.equals("heading")) {
-			if (!hasCustomHeading) {
-				return IVariable.from(recipes.get(0).getResultItem(level.registryAccess()).getHoverName(), level.registryAccess());
+			if (!hasCustomHeading && !recipes.isEmpty()) {
+				return IVariable.from(recipes.getFirst().getResultItem(level.registryAccess()).getHoverName(), level.registryAccess());
 			}
 			return null;
 		}
-		if (key.startsWith("input")) {
-			int index = Integer.parseInt(key.substring(5)) - 1;
+		IVariable recipes1Variable = processRecipes(level, key, recipes, "input", "output", "shapeless", longestIngredientSize, shapeless);
+		if (recipes1Variable != null) {
+			return recipes1Variable;
+		}
+		if (recipes2 != null) {
+			return processRecipes(level, key, recipes2, "input2_", "output2", "shapeless2", longestIngredientSize2, shapeless2);
+		}
+		return null;
+	}
+
+	@Nullable
+	private IVariable processRecipes(Level level, String key, List<CraftingRecipe> recipeList, String inputKeyPrefix,
+			String outputKey, String shapelessKey, int size, boolean isShapeless) {
+		if (recipeList.isEmpty()) {
+			return null;
+		}
+		if (key.startsWith(inputKeyPrefix) && key.length() == inputKeyPrefix.length() + 1) {
+			int index = Integer.parseInt(key.substring(inputKeyPrefix.length())) - 1;
 			int shapedX = index % 3;
 			int shapedY = index / 3;
 			List<Ingredient> ingredients = new ArrayList<>();
-			for (CraftingRecipe recipe : recipes) {
+			for (CraftingRecipe recipe : recipeList) {
 				if (recipe instanceof ShapedRecipe shaped) {
 					if (shaped.getWidth() < shapedX + 1) {
 						ingredients.add(Ingredient.EMPTY);
@@ -87,15 +129,15 @@ public class MultiCraftingProcessor implements IComponentProcessor {
 					ingredients.add(list.size() > index ? list.get(index) : Ingredient.EMPTY);
 				}
 			}
-			return PatchouliUtils.interweaveIngredients(ingredients, longestIngredientSize, level);
+			return PatchouliUtils.interweaveIngredients(ingredients, size, level);
 		}
-		if (key.equals("output")) {
-			return IVariable.wrapList(recipes.stream()
+		if (key.equals(outputKey)) {
+			return IVariable.wrapList(recipeList.stream()
 					.map(r -> IVariable.from(r.getResultItem(level.registryAccess()), level.registryAccess()))
 					.collect(Collectors.toList()), level.registryAccess());
 		}
-		if (key.equals("shapeless")) {
-			return IVariable.wrap(shapeless);
+		if (key.equals(shapelessKey)) {
+			return IVariable.wrap(isShapeless, level.registryAccess());
 		}
 		return null;
 	}
