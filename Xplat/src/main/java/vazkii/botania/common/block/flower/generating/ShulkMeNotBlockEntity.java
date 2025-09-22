bbb49e88b772215aaf0a2e904ebaba1b40832aed
TheRealWormbo <github@wormbo.de>
2025-09-22 09:27:40 +0200

    Tweak Shulk-Me-Not effects and performance
    - don't bother looking for shulkers if there's not enough room for the potentially generated mana anyway
    - place explosion particles around center of consumed mob hit boxes instead of southeast corner
    - only send flower particle effect once
    - play the sound once for the flower and once for each consumed mob, not ten times flat

--- Xplat/src/main/java/vazkii/botania/common/block/flower/generating/ShulkMeNotBlockEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/block/flower/generating/ShulkMeNotBlockEntity.java
@@ -13,7 +13,7 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntitySelector;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.monster.Shulker;
@@ -32,6 +32,7 @@ import java.util.List;
 
 public class ShulkMeNotBlockEntity extends GeneratingFlowerBlockEntity {
 	private static final int RADIUS = 8;
+	private static final int MANA_PER_SHULKER = 75000;
 
 	public ShulkMeNotBlockEntity(BlockPos pos, BlockState state) {
 		super(BotaniaFlowerBlocks.SHULK_ME_NOT, pos, state);
@@ -41,49 +42,50 @@ public class ShulkMeNotBlockEntity extends GeneratingFlowerBlockEntity {
 	public void tickFlower() {
 		super.tickFlower();
 
-		int generate = getMaxMana();
-
 		Level world = getLevel();
+		if (world.isClientSide) {
+			return;
+		}
+		int generate = getMaxMana();
+		if (getMaxMana() - getMana() < generate) {
+			return;
+		}
+
 		BlockPos pos = getEffectivePos();
 		Vec3 posD = new Vec3(pos.getX(), pos.getY(), pos.getZ());
-		if (!world.isClientSide) {
-			List<Shulker> shulkers = world.getEntitiesOfClass(Shulker.class, new AABB(pos).inflate(RADIUS));
-			for (Shulker shulker : shulkers) {
-				if (getMaxMana() - getMana() < generate) {
-					break;
-				}
+		List<Shulker> shulkers = world.getEntitiesOfClass(Shulker.class, new AABB(pos).inflate(RADIUS), EntitySelector.ENTITY_STILL_ALIVE);
+		for (Shulker shulker : shulkers) {
+			if (getMaxMana() - getMana() < generate) {
+				break;
+			}
 
-				if (shulker.isAlive() && shulker.distanceToSqr(posD) < RADIUS * RADIUS) {
-					LivingEntity target = shulker.getTarget();
-					if (target instanceof Enemy && target.isAlive()
-							&& target.distanceToSqr(posD) < RADIUS * RADIUS && target.getEffect(MobEffects.LEVITATION) != null) {
-						target.discard();
-						shulker.discard();
+			if (shulker.isAlive() && shulker.distanceToSqr(posD) < RADIUS * RADIUS) {
+				LivingEntity target = shulker.getTarget();
+				if (target instanceof Enemy && target.isAlive()
+						&& target.distanceToSqr(posD) < RADIUS * RADIUS && target.getEffect(MobEffects.LEVITATION) != null) {
+					target.discard();
+					shulker.discard();
 
-						for (int i = 0; i < 10; i++) {
-							// so it's really loud >_>
-							world.playSound(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, BotaniaSounds.shulkMeNot, SoundSource.BLOCKS, 10F, 1F);
-						}
-						particles(world, pos, target);
-						particles(world, pos, shulker);
-
-						world.gameEvent(null, GameEvent.BLOCK_ACTIVATE, pos);
-						addMana(generate);
-						sync();
+					world.playSound(null, pos, BotaniaSounds.shulkMeNot, SoundSource.BLOCKS, 10F, 1F);
+					world.playSound(null, target, BotaniaSounds.shulkMeNot, SoundSource.BLOCKS, 10F, 1F);
+					world.playSound(null, shulker, BotaniaSounds.shulkMeNot, SoundSource.BLOCKS, 10F, 1F);
+					if (world instanceof ServerLevel ws) {
+						ws.sendParticles(ParticleTypes.EXPLOSION,
+								target.getX(), target.getY() + target.getBbHeight() / 2, target.getZ(),
+								100, target.getBbWidth(), target.getBbHeight(), target.getBbWidth(), 0.05);
+						ws.sendParticles(ParticleTypes.EXPLOSION,
+								shulker.getX(), shulker.getY() + shulker.getBbHeight() / 2, shulker.getZ(),
+								100, shulker.getBbWidth(), shulker.getBbHeight(), shulker.getBbWidth(), 0.05);
+						ws.sendParticles(ParticleTypes.PORTAL,
+								pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5,
+								40, 0, 0, 0, 0.6);
 					}
+
+					world.gameEvent(null, GameEvent.BLOCK_ACTIVATE, pos);
+					addMana(generate);
+					sync();
 				}
 			}
-		}
-	}
-
-	private void particles(Level world, BlockPos pos, Entity entity) {
-		if (world instanceof ServerLevel ws) {
-			ws.sendParticles(ParticleTypes.EXPLOSION,
-					entity.getX() + entity.getBbWidth() / 2,
-					entity.getY() + entity.getBbHeight() / 2,
-					entity.getZ() + entity.getBbWidth() / 2,
-					100, entity.getBbWidth(), entity.getBbHeight(), entity.getBbWidth(), 0.05);
-			ws.sendParticles(ParticleTypes.PORTAL, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 40, 0, 0, 0, 0.6);
 		}
 	}
 
@@ -99,7 +101,7 @@ public class ShulkMeNotBlockEntity extends GeneratingFlowerBlockEntity {
 
 	@Override
 	public int getMaxMana() {
-		return 75000;
+		return MANA_PER_SHULKER;
 	}
 
 }
