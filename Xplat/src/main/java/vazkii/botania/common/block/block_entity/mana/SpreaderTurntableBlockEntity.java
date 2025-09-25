542c36e8247c168ae6974fdcb7ebe8e3505cbf3c
TheRealWormbo <github@wormbo.de>
2025-09-25 07:23:30 +0200

    Refactor spreader turntable to use block state properties

--- Xplat/src/main/java/vazkii/botania/common/block/block_entity/mana/SpreaderTurntableBlockEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/block/block_entity/mana/SpreaderTurntableBlockEntity.java
@@ -15,72 +15,56 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.HolderLookup;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 
 import vazkii.botania.api.block.WandHUD;
 import vazkii.botania.api.block.Wandable;
-import vazkii.botania.api.internal.VanillaPacketDispatcher;
+import vazkii.botania.api.state.BotaniaStateProperties;
 import vazkii.botania.client.core.helper.RenderHelper;
 import vazkii.botania.common.block.block_entity.BotaniaBlockEntities;
 import vazkii.botania.common.block.block_entity.BotaniaBlockEntity;
 
 public class SpreaderTurntableBlockEntity extends BotaniaBlockEntity implements Wandable {
-	private static final String TAG_SPEED = "speed";
-	private static final String TAG_BACKWARDS = "backwards";
-
-	private int speed = 1;
-	private boolean backwards = false;
 
 	public SpreaderTurntableBlockEntity(BlockPos pos, BlockState state) {
 		super(BotaniaBlockEntities.TURNTABLE, pos, state);
 	}
 
 	public static void commonTick(Level level, BlockPos worldPosition, BlockState state, SpreaderTurntableBlockEntity self) {
-		if (!level.hasNeighborSignal(worldPosition)) {
-			BlockEntity tile = level.getBlockEntity(worldPosition.above());
-			if (tile instanceof ManaSpreaderBlockEntity spreader) {
-				spreader.rotationX += self.speed * (self.backwards ? -1 : 1);
-				if (spreader.rotationX >= 360F) {
-					spreader.rotationX -= 360F;
-				} else if (spreader.rotationX < 0F) {
-					spreader.rotationX += 360F;
-				}
-				if (!level.isClientSide) {
-					spreader.checkForReceiver();
-				}
+		if (state.getValue(BlockStateProperties.POWERED)) {
+			return;
+		}
+		BlockEntity tile = level.getBlockEntity(worldPosition.above());
+		if (tile instanceof ManaSpreaderBlockEntity spreader) {
+			spreader.rotationX += state.getValue(BotaniaStateProperties.SPEED)
+					* (state.getValue(BotaniaStateProperties.BACKWARDS) ? -1 : 1);
+			if (spreader.rotationX >= 360F) {
+				spreader.rotationX -= 360F;
+			} else if (spreader.rotationX < 0F) {
+				spreader.rotationX += 360F;
+			}
+			if (!level.isClientSide) {
+				spreader.checkForReceiver();
 			}
 		}
 	}
 
 	@Override
-	public void writePacketNBT(CompoundTag cmp, HolderLookup.Provider registries) {
-		cmp.putInt(TAG_SPEED, speed);
-		cmp.putBoolean(TAG_BACKWARDS, backwards);
-	}
-
-	@Override
-	public void readPacketNBT(CompoundTag cmp, HolderLookup.Provider registries) {
-		speed = cmp.getInt(TAG_SPEED);
-		backwards = cmp.getBoolean(TAG_BACKWARDS);
-	}
-
-	@Override
 	public boolean onUsedByWand(@Nullable Player player, ItemStack wand, Direction side) {
-		if ((player != null && player.isShiftKeyDown()) || (player == null && side == Direction.DOWN)) {
-			backwards = !backwards;
+		if ((player != null && player.isSecondaryUseActive()) || (player == null && side == Direction.DOWN)) {
+			level.setBlockAndUpdate(getBlockPos(), getBlockState().cycle(BotaniaStateProperties.BACKWARDS));
 		} else {
-			speed = speed == 6 ? 1 : speed + 1;
+			level.setBlockAndUpdate(getBlockPos(), getBlockState().cycle(BotaniaStateProperties.SPEED));
 		}
-		VanillaPacketDispatcher.dispatchTEToNearbyPlayers(this);
 		return true;
 	}
 
@@ -93,14 +77,18 @@ public class SpreaderTurntableBlockEntity extends BotaniaBlockEntity implements 
 
 		@Override
 		public void renderHUD(GuiGraphics gui, Window window, Font font, float partialTick) {
-			String speed = ChatFormatting.BOLD + StringUtils.repeat(turntable.backwards ? '<' : '>', turntable.speed);
+			Component speed = Component
+					.literal(StringUtils.repeat(
+							turntable.getBlockState().getValue(BotaniaStateProperties.BACKWARDS) ? '<' : '>',
+							turntable.getBlockState().getValue(BotaniaStateProperties.SPEED)))
+					.withStyle(ChatFormatting.BOLD).withStyle(ChatFormatting.WHITE);
 
 			int strWidth = font.width(speed);
 			int x = (window.getGuiScaledWidth() - strWidth) / 2;
 			int y = window.getGuiScaledHeight() / 2 + 8;
 
 			RenderHelper.renderHUDBox(gui, x - 2, y, x + strWidth + 2, y + 12);
-			gui.drawString(font, speed, x, y + 2, ChatFormatting.WHITE.getColor());
+			gui.drawString(font, speed, x, y + 2, 0xFFFFFF);
 		}
 	}
 
--- Xplat/src/main/java/vazkii/botania/common/block/mana/SpreaderTurntableBlock.java
+++ Xplat/src/main/java/vazkii/botania/common/block/mana/SpreaderTurntableBlock.java
@@ -10,15 +10,21 @@ package vazkii.botania.common.block.mana;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
 import org.jetbrains.annotations.Nullable;
 
+import vazkii.botania.api.block.RedstoneSensitiveBlock;
+import vazkii.botania.api.state.BotaniaStateProperties;
 import vazkii.botania.common.block.BotaniaBlock;
 import vazkii.botania.common.block.OpenCrateBlock;
 import vazkii.botania.common.block.block_entity.BotaniaBlockEntities;
@@ -28,6 +34,26 @@ public class SpreaderTurntableBlock extends BotaniaBlock implements EntityBlock 
 
 	public SpreaderTurntableBlock(Properties builder) {
 		super(builder);
+		registerDefaultState(defaultBlockState()
+				.setValue(BlockStateProperties.POWERED, false)
+				.setValue(BotaniaStateProperties.SPEED, 1)
+				.setValue(BotaniaStateProperties.BACKWARDS, false));
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(BlockStateProperties.POWERED, BotaniaStateProperties.SPEED, BotaniaStateProperties.BACKWARDS);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return RedstoneSensitiveBlock.getPoweredStateForPlacement(super.getStateForPlacement(context), context);
+	}
+
+	@Override
+	public void neighborChanged(BlockState state, Level world, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
+		RedstoneSensitiveBlock.updateRedstonePower(state, world, pos);
 	}
 
 	@Override
@@ -43,7 +69,7 @@ public class SpreaderTurntableBlock extends BotaniaBlock implements EntityBlock 
 
 	@Override
 	public void animateTick(BlockState state, Level world, BlockPos pos, RandomSource rand) {
-		if (world.hasNeighborSignal(pos) && rand.nextDouble() < 0.2) {
+		if (state.getValue(BlockStateProperties.POWERED) && rand.nextDouble() < 0.2) {
 			OpenCrateBlock.redstoneParticlesOnFullBlock(world, pos, rand);
 		}
 	}
