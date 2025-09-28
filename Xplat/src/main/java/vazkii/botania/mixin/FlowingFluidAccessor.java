c10e1da37874befb7a673c0af25e54897f0c6f51
TheRealWormbo <github@wormbo.de>
2025-09-28 22:13:05 +0200

    Spotless apply

--- Xplat/src/main/java/vazkii/botania/mixin/FlowingFluidAccessor.java
+++ Xplat/src/main/java/vazkii/botania/mixin/FlowingFluidAccessor.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FlowingFluid;
 import net.minecraft.world.level.material.FluidState;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
