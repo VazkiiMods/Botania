4a5b2ed02bb4805dbbaed3e03bb34363a1b15301
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2026-01-31 22:52:16 +1300

    Use the correct nullable annotation

--- Xplat/src/main/java/vazkii/botania/api/item/BlockProvider.java
+++ Xplat/src/main/java/vazkii/botania/api/item/BlockProvider.java
@@ -12,7 +12,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * An Item that has this capability can provide blocks to other items that use them.
