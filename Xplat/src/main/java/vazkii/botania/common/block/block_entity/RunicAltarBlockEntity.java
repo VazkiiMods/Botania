2b956a38e9fa9a6539852a2c6a9125615d50bde4
TheRealWormbo <github@wormbo.de>
2026-06-11 06:46:10 +0200

    Fix Runic Altar accepting item entities

--- Xplat/src/main/java/vazkii/botania/common/block/block_entity/RunicAltarBlockEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/block/block_entity/RunicAltarBlockEntity.java
@@ -176,7 +176,7 @@ public class RunicAltarBlockEntity extends SimpleInventoryBlockEntity implements
 			List<ItemEntity> items = level.getEntitiesOfClass(ItemEntity.class, new AABB(worldPosition));
 			for (ItemEntity item : items) {
 				if (item.isAlive() && !item.getItem().isEmpty() && !item.getItem().is(BotaniaBlocks.livingrock.asItem())
-						&& ItemSource.HOLDER.getFor(item) == ItemSources.RUNIC_ALTAR) {
+						&& ItemSource.HOLDER.getFor(item) != ItemSources.RUNIC_ALTAR) {
 					ItemStack stack = item.getItem();
 					if (self.addItem(null, stack, null)) {
 						EntityHelper.syncItem(item);
