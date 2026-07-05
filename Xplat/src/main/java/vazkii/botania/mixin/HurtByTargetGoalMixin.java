43eca70248233ed7b0164a66e2c928a074b13abb
TheRealWormbo <github@wormbo.de>
2026-07-05 07:16:49 +0200

    Make HurtByTargetGoalMixin a bit more flexible
    It now prevents the `alertOthers` call entirely when applicable, which makes it work for e.g. bees, polar bears, and aggressive pandas as well.

--- Xplat/src/main/java/vazkii/botania/mixin/HurtByTargetGoalMixin.java
+++ Xplat/src/main/java/vazkii/botania/mixin/HurtByTargetGoalMixin.java
@@ -9,27 +9,30 @@
 
 package vazkii.botania.mixin;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HurtByTargetGoal.class)
 public class HurtByTargetGoalMixin {
 	/**
 	 * Prevent mobs from punching themselves. This can happen when either the Heisei Dream or Diva Charm brainwash
-	 * another mob and that mob alerts other mobs to attack the same target. If the target is among the alerted mobs,
-	 * it would attack itself.
-	 * TODO[MixinExtras] Preventing the call in HurtByTargetGoal::alertOthers would be a more compatible option.
+	 * another mob and that mob alerts other mobs to attack the same target. If the target is among the alerted mobs, it
+	 * would attack itself.
 	 */
-	@Inject(method = "alertOther", at = @At("HEAD"), cancellable = true)
-	private void checkSelfTargeting(Mob mobToAlert, LivingEntity targetToAttack, CallbackInfo ci) {
-		if (mobToAlert == targetToAttack) {
-			ci.cancel();
-		}
+	@WrapWithCondition(
+		method = "alertOthers",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/entity/ai/goal/target/HurtByTargetGoal;alertOther(Lnet/minecraft/world/entity/Mob;Lnet/minecraft/world/entity/LivingEntity;)V"
+		)
+	)
+	private boolean checkNotTargetingSelf(HurtByTargetGoal instance, Mob mob, LivingEntity target) {
+		return mob != target;
 	}
 }
