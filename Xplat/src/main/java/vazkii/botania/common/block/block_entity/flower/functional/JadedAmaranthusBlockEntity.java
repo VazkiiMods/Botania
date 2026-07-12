2b60e017de0e443beded31809fb0e8be91da5ab4
TheRealWormbo <github@wormbo.de>
2026-07-12 09:59:56 +0200

    Jaded Amaranthus uses small_mystical_flowers block tag

--- Xplat/src/main/java/vazkii/botania/common/block/block_entity/flower/functional/JadedAmaranthusBlockEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/block/block_entity/flower/functional/JadedAmaranthusBlockEntity.java
@@ -9,7 +9,8 @@
 package vazkii.botania.common.block.block_entity.flower.functional;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.item.DyeColor;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LevelEvent;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,9 +18,11 @@ import net.minecraft.world.level.gameevent.GameEvent;
 
 import vazkii.botania.api.block_entity.FunctionalFlowerBlockEntity;
 import vazkii.botania.api.block_entity.RadiusDescriptor;
-import vazkii.botania.common.block.BotaniaBlocks;
 import vazkii.botania.common.block.block_entity.BotaniaBlockEntities;
+import vazkii.botania.common.lib.BotaniaTags;
 import vazkii.botania.xplat.BotaniaConfig;
+
+import java.util.Optional;
 
 public class JadedAmaranthusBlockEntity extends FunctionalFlowerBlockEntity {
 	private static final int COST = 100;
@@ -36,6 +39,12 @@ public class JadedAmaranthusBlockEntity extends FunctionalFlowerBlockEntity {
 		if (getLevel().isClientSide() || isPowered() || getMana() < COST || !shouldUpdateThisTick()) {
 			return;
 		}
+		Optional<HolderSet.Named<Block>> flowersTag =
+				BuiltInRegistries.BLOCK.getTag(BotaniaTags.Blocks.SMALL_MYSTICAL_FLOWERS);
+		if (flowersTag.flatMap(holders -> holders.stream().findAny()).isEmpty()) {
+			// missing or empty flowers tag
+			return;
+		}
 
 		BlockPos pos = new BlockPos(
 				getEffectivePos().getX() - RANGE + getLevel().getRandom().nextInt(RANGE * 2 + 1),
@@ -46,9 +55,12 @@ public class JadedAmaranthusBlockEntity extends FunctionalFlowerBlockEntity {
 		BlockPos up = pos.above();
 
 		for (int i = 0; i < RANGE * 2; i++) {
-			DyeColor color = DyeColor.byId(getLevel().getRandom().nextInt(16));
-			BlockState flower = BotaniaBlocks.getMysticalFlower(color).defaultBlockState();
-
+			var randomFlower = flowersTag.get().getRandomElement(getLevel().getRandom());
+			if (randomFlower.isEmpty()) {
+				// shouldn't be possible, but just in case
+				continue;
+			}
+			BlockState flower = randomFlower.get().value().defaultBlockState();
 			if (getLevel().isEmptyBlock(up) && flower.canSurvive(getLevel(), up)) {
 				if (BotaniaConfig.common().blockBreakParticles()) {
 					getLevel().levelEvent(LevelEvent.PARTICLES_DESTROY_BLOCK, up, Block.getId(flower));
